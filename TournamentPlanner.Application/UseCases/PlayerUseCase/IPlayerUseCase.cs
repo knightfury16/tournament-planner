5f8ddf2276df52b8227a72c49b4766798d703a32
suhaib <suhaib.ahmmad@orbitax.com>
2024-05-02 16:21:36 +0600

    Player use case template added

---
+++ TournamentPlanner.Application/UseCases/PlayerUseCase/IPlayerUseCase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Application.UseCases.AddPlayer
+{
+    //Lets not separate the concern for now. 
+    // TODO: Separate command and query
+    public interface IPlayerUseCase
+    {
+        Task<Player> AddPlayerAsync(PlayerDto player);
+        Task<IEnumerable<Player>> GetAllPlayerAsync();
+        Task<IEnumerable<Player>> GetAllPlayerWhoseMatchNotStillPlayedAsync();
+        Task<IEnumerable<Player>> GetPlayersAsync(string? playerName);
+    }
+}
---
+++ TournamentPlanner.Application/UseCases/PlayerUseCase/PlayerUseCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Application.UseCases.AddPlayer;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Application.UseCases.PlayerUseCase
+{
+    public class PlayerUseCase : IPlayerUseCase
+    {
+        public IRepository<PlayerDto, Player> _playerRepository { get; }
+
+        public PlayerUseCase(IRepository<PlayerDto, Player> playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        public Task<Player> AddPlayerAsync(PlayerDto player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Player>> GetAllPlayerAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Player>> GetAllPlayerWhoseMatchNotStillPlayedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Player>> GetPlayersAsync(string? playerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
