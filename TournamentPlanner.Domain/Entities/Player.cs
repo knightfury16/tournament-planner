6253a7ba4823ae4ec1f50b9f9e62a62846733f2c
suhaib <suhaib.ahmmad@orbitax.com>
2024-05-02 16:38:20 +0600

    Domain change on player property modifier

--- TournamentPlanner.Domain/Entities/Player.cs
+++ TournamentPlanner.Domain/Entities/Player.cs
@@ -9,9 +9,9 @@ namespace TournamentPlanner.Domain.Entities
     public class Player: BaseEntity
     {
         public string Name { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
 
-        public string Email { get; set; } = string.Empty;
-        
+        public string? Email { get; set; } = string.Empty;
+
     }
 }
