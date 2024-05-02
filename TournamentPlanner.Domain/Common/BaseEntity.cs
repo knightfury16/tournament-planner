50fa500138ee7fb7f48dd0590ba341597950de45
knightfury16 <suhaibknight@gmail.com>
2024-05-02 09:08:46 +0600

    Made base entity abstract

--- TournamentPlanner.Domain/Common/BaseEntity.cs
+++ TournamentPlanner.Domain/Common/BaseEntity.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace TournamentPlanner.Domain.Common
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         public int Id { get; set; }
     }
