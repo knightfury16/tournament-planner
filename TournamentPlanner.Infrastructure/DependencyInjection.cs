7b56aaa3594095727b60af260542ac94be1da8e7
knightfury16 <suhaibknight@gmail.com>
2024-05-03 16:53:47 +0600

    implemented IPlayerRepository and injected it to service

--- TournamentPlanner.Infrastructure/DependencyInjection.cs
+++ TournamentPlanner.Infrastructure/DependencyInjection.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
 using TournamentPlanner.Infrastructure.DataContext;
 
 namespace TournamentPlanner.Infrastructure
@@ -13,6 +16,7 @@ namespace TournamentPlanner.Infrastructure
     {
        public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration){
             services.AddDbContext<TournamentPlannerDataContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IRepository<PlayerDto,Player>,PlayerRepository>();
        } 
     }
 }
---
+++ TournamentPlanner.Infrastructure/PlayerRepository.cs
@@ -0,0 +1,95 @@
+using Microsoft.EntityFrameworkCore;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Infrastructure.DataContext;
+
+namespace TournamentPlanner.Infrastructure
+{
+    public class PlayerRepository : IRepository<PlayerDto, Player>
+    {
+        public TournamentPlannerDataContext _context { get; }
+
+        public PlayerRepository(TournamentPlannerDataContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<Player> AddAsync(PlayerDto obj)
+        {
+            var player = FromDto(obj);
+            _context.Players.Add(player);
+            await _context.SaveChangesAsync();
+            return player;
+        }
+
+
+        public async Task<Player> GetByIdAsync(int id)
+        {
+            var player = await _context.Players.FirstOrDefaultAsync(p => p.Id == id);
+
+            if(player is null){
+                throw new ArgumentException("Can not find Player with specified Id");
+            }
+            return  player;
+        }
+
+        public Task<Player> UpdateAsync(PlayerDto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Player> UpdateByIdAsync(int id, PlayerDto obj)
+        {
+            var player = await _context.Players.FindAsync(id);
+
+            if(player is null){
+                throw new ArgumentException("Player not found");
+            }
+
+            player.Name = obj.Name;
+            player.PhoneNumber = obj.PhoneNumber;
+            player.Email = obj.Email;
+            
+            await _context.SaveChangesAsync();
+
+            return player;
+        }
+
+        public PlayerDto ToDto(Player player)
+        {
+            return new PlayerDto
+            {
+                Name = player.Name,
+                PhoneNumber = player.PhoneNumber,
+                Email = player.Email
+            };
+        }
+
+        public Player FromDto(PlayerDto dto)
+        {
+            return new Player
+            {
+                Name = dto.Name,
+                PhoneNumber = dto.PhoneNumber,
+                Email = dto.Email
+            };
+        }
+
+        public async Task<IEnumerable<Player>> GetAllAsync()
+        {
+            return await _context.Players.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Player>> GetByNameAsync(string? name)
+        {
+            if(string.IsNullOrEmpty(name)){
+                return await _context.Players.AsNoTracking().ToListAsync();
+            }
+
+            return await _context.Players.AsNoTracking().Where(p => p.Name.ToLower().Contains(name.ToLower())).ToListAsync();
+        }
+
+    }
+}
