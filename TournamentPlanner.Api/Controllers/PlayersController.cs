caf4fde8f1086e218d45c0436f595ff4bd2629fa
knightfury16 <suhaibknight@gmail.com>
2024-05-03 17:28:25 +0600

    added players controller

---
+++ TournamentPlanner.Api/Controllers/PlayersController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Application.UseCases.AddPlayer;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class PlayersController : ControllerBase
+    {
+        public IPlayerUseCase _playerUseCase { get; }
+
+        public PlayersController(IPlayerUseCase playerUseCase)
+        {
+            _playerUseCase = playerUseCase;
+        }
+
+        [HttpGet]
+        [Route("players")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Player>))]
+        public async Task<IActionResult> GetAllPlayer()
+        {
+            var players = await _playerUseCase.GetPlayersAsync(null);
+            return Ok(players);
+        }
+
+
+        [HttpPost]
+        [Route("players")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Player))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddPlayer([FromBody] PlayerDto playerDto){
+            if(playerDto == null){
+                return BadRequest("Player information needed");
+            }
+            var player = await _playerUseCase.AddPlayerAsync(playerDto);
+            //TODO: make return type createdAt
+            return Ok(player);
+        }
+
+    }
+}
