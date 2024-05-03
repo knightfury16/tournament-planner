1e4dec0f43fd088811b4e2c1352c6f753684508e
knightfury16 <suhaibknight@gmail.com>
2024-05-03 17:27:38 +0600

    dont use string empty on optional parameter

--- TournamentPlanner.Application/DTOs/PlayerDto.cs
+++ TournamentPlanner.Application/DTOs/PlayerDto.cs
@@ -3,7 +3,7 @@
     public class PlayerDto
     {
         public string Name { get; set; } = string.Empty;
-        public string? PhoneNumber { get; set; } = string.Empty;
-        public string? Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
     }
 }
