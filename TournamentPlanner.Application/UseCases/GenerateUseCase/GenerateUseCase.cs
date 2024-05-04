600a9b7af32ef0cbfb47e626958c82d76df219a7
knightfury16 <suhaibknight@gmail.com>
2024-05-04 17:05:13 +0600

    Implementing GenerateUseCase, implemented get allplayer method

---
+++ TournamentPlanner.Application/UseCases/GenerateUseCase/GenerateUseCase.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Application.UseCases.GenerateUseCase
+{
+    public class GenerateUseCase : IGenerate
+    {
+        private readonly IRepository<PlayerDto, Player> _playerRepository;
+        private readonly IRepository<Match, Match> _matchRepository;
+        private readonly IConfiguration _configuration;
+
+        public GenerateUseCase(IRepository<PlayerDto, Player> playerRepository, IRepository<Match, Match> matchRepository, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _playerRepository = playerRepository;
+            _matchRepository = matchRepository;
+        }
+        public async Task<List<Player>> AddPlayerAutoToTournament(string TournamentName)
+        {
+            List<Player> playerList = await GetAllPlayers();
+            throw new NotImplementedException();
+        }
+
+        private async Task<List<Player>> GetAllPlayers()
+        {
+            var playerDataPath = _configuration["playerData"];
+
+            if(playerDataPath == null){
+                throw new Exception("Player Data could not be found");
+            }
+            var playerJsonContent = await File.ReadAllTextAsync(playerDataPath);
+
+            if(playerJsonContent == null){
+                throw new Exception("Player Content could not be read");
+            }
+
+            var playerList = JsonSerializer.Deserialize<List<Player>>(playerJsonContent);
+
+            if(playerList == null){
+                throw new Exception("Player could not be deserialized");
+            }
+
+            return playerList;
+
+        }
+
+        public Task<List<Match>> MakeRoaster<T>(T TournamentIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Match>> SimulateMatches<T>(T TournamentIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TournamentPlanner.Application/UseCases/GenerateUseCase/IGenerate.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Application.UseCases.GenerateUseCase
+{
+    public interface IGenerate
+    {
+        Task<List<Player>> AddPlayerAutoToTournament(string TournamentName);
+        Task<List<Match>> MakeRoaster<T>(T TournamentIdentifier);
+        Task<List<Match>> SimulateMatches<T>(T TournamentIdentifier);
+    }
+}
