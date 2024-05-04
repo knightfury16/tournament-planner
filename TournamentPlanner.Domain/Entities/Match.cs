17d2d74dcbd2e23fb98714fa6ec5941e37c22f45
knightfury16 <suhaibknight@gmail.com>
2024-05-04 11:19:50 +0600

    Added json ignore attribute to some entity(should i?) to avoid object cycle

--- TournamentPlanner.Domain/Entities/Match.cs
+++ TournamentPlanner.Domain/Entities/Match.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using TournamentPlanner.Domain.Common;
 
@@ -20,6 +21,7 @@ namespace TournamentPlanner.Domain.Entities
         public DateOnly? GamePlayed { get; set; }
 
         public Round Round { get; set; } = new();
+
         
     }
 }
--- TournamentPlanner.Domain/Entities/Round.cs
+++ TournamentPlanner.Domain/Entities/Round.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using TournamentPlanner.Domain.Common;
 
@@ -9,6 +10,8 @@ namespace TournamentPlanner.Domain.Entities
     public class Round: BaseEntity
     {
         public int RoundNumber { get; set; }
+
+        [JsonIgnore]
         public List<Match> Matches { get; set; } = new();
         public Tournament? Tournament { get; set; } = new();
         
