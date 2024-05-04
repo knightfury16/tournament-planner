ed2d78df0913fe3752172186f221ca4d9cb7ec05
knightfury16 <suhaibknight@gmail.com>
2024-05-04 18:54:23 +0600

    generate controller added

---
+++ TournamentPlanner.Api/Controllers/GenerateController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TournamentPlanner.Application.UseCases.GenerateUseCase;
+
+namespace TournamentPlanner.Api.Controllers
+{
+    [ApiController, Route("/generate")]
+    public class GenerateController: ControllerBase
+    {
+        private readonly IGenerate _generate;
+
+        public GenerateController(IGenerate generate)
+        {
+            _generate = generate;
+        }
+
+
+        [HttpPost]
+        [Route("{tournamentName}")]
+        public async Task<IActionResult> AddPlayerToTournament(string tournamentName){
+            var players = await _generate.AddPlayerAutoToTournament(tournamentName);
+            return Ok(players);
+        }
+
+        
+    }
+}
