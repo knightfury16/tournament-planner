3106acb1aca6d495c33eb57b3787e9c810f8eaed
knightfury16 <suhaibknight@gmail.com>
2024-05-05 23:39:18 +0600

    configured services with Repository

--- TournamentPlanner.Infrastructure/DependencyInjection.cs
+++ TournamentPlanner.Infrastructure/DependencyInjection.cs
@@ -17,10 +17,10 @@ namespace TournamentPlanner.Infrastructure
         public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<TournamentPlannerDataContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IRepository<PlayerDto, Player>, PlayerRepository>();
-            services.AddScoped<IRepository<Match, Match>, MatchRepository>();
-            services.AddScoped<IRepository<Tournament, Tournament>, TournamentRepository>();
-            services.AddScoped<IRepository<Round, Round>, RoundRepository>();
+            services.AddScoped<IRepository<Player, Player>, Repository<Player, Player>>();
+            services.AddScoped<IRepository<Match, Match>, Repository<Match, Match>>();
+            services.AddScoped<IRepository<Tournament, Tournament>, Repository<Tournament,Tournament>>();
+            services.AddScoped<IRepository<Round, Round>, Repository<Round, Round>>();
         }
     }
 }
