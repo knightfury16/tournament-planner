cf68bb12c685543eb108f5ef91e30fdbbf48b6ca
knightfury16 <suhaibknight@gmail.com>
2024-05-08 21:53:59 +0600

    Initial functional

--- TournamentPlanner.Api/Controllers/GenerateController.cs
+++ TournamentPlanner.Api/Controllers/GenerateController.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TournamentPlanner.Application.UseCases.GenerateUseCase;
+using TournamentPlanner.Domain.Entities;
 
 namespace TournamentPlanner.Api.Controllers
 {
-    [ApiController, Route("/generate")]
+    [ApiController, Route("api/generate")]
     public class GenerateController: ControllerBase
     {
         private readonly IGenerate _generate;
@@ -20,6 +21,7 @@ namespace TournamentPlanner.Api.Controllers
 
         [HttpPost]
         [Route("{tournamentName}")]
+        [ProducesResponseType(StatusCodes.Status200OK,Type = typeof(IEnumerable<Player>))]
         public async Task<IActionResult> AddPlayerToTournament(string tournamentName){
             var players = await _generate.AddPlayerAutoToTournament(tournamentName);
             return Ok(players);
@@ -27,27 +29,30 @@ namespace TournamentPlanner.Api.Controllers
 
         [HttpPost]
         [Route("/roaster")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Match>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> MakeRoaster([FromQuery] string tournamentName){
             var matches = await _generate.MakeRoaster(tournamentName);
             if(matches is null)
             {
-                return Ok("Can not make roaster, previous round not finish");
+                return BadRequest("Can not make roaster, previous round not finish");
             }
             return Ok(matches);
         }
 
         [HttpPost]
         [Route("/simulate")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Match>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> SimulateMatch([FromQuery]string tournamentName, bool allMatch)
         {
             var matches = await _generate.SimulateMatches(tournamentName, allMatch);
             if(matches is null)
             {
-                return Ok("No match is scheduled yet.Please schedule some match first");
+                return BadRequest("No match is scheduled yet.Please schedule some match first");
             }
             return Ok(matches);
         }
-
         
     }
 }
