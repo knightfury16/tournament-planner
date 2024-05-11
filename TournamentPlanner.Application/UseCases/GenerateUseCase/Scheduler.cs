ed80eab5078edcd6ba7b119537d80243bc5832a8
knightfury16 <suhaibknight@gmail.com>
2024-05-11 23:11:56 +0600

    Scheduler for first round logic added

--- TournamentPlanner.Application/UseCases/GenerateUseCase/Scheduler.cs
+++ TournamentPlanner.Application/UseCases/GenerateUseCase/Scheduler.cs
@@ -10,28 +10,42 @@ namespace TournamentPlanner.Application.UseCases.GenerateUseCase
     {
         public static int MAX_MATCH_EACH_DAY { get; set; } = 4;
         public static List<Match> Schedule(List<Match> matches, Round round){
-            var roundStartTime = round.StartTime;
             
-            if(roundStartTime is null){
+            if(round.StartTime is null){
                 throw new ArgumentException("Cant not schedule match. Round start time is not specified");
             }
 
+            var roundStartTimeUtc = round.StartTime.Value.ToUniversalTime();
+
             //staring match after two hour of round start
-            var matchStartTime = roundStartTime.Value.Date.AddHours(10); //from 10 in the morning
+            var matchStartTimeUtc = roundStartTimeUtc.Date.AddHours(10); //from 10 in the morning
+
+            // Adjust match start time if round start time is after 10am UTC
+            if(roundStartTimeUtc.TimeOfDay > new TimeSpan(10,0,0)){
+
+                //start match one hour after round start time if its before 5pm
+                if(roundStartTimeUtc.TimeOfDay < new TimeSpan(17,0,0)){
+                    matchStartTimeUtc = roundStartTimeUtc.AddHours(1).ToUniversalTime();
+                }
+                else{
+                    // start the next day
+                    matchStartTimeUtc = roundStartTimeUtc.AddDays(1).AddHours(10).ToUniversalTime();
+                }
+            }
 
             var matchScheduleOnThisDate = 0;
 
             foreach (var match in matches)
             {
-                if(matchScheduleOnThisDate == MAX_MATCH_EACH_DAY){
-                    matchStartTime = matchStartTime.Date.AddDays(1).AddHours(10);
+                if(matchScheduleOnThisDate == MAX_MATCH_EACH_DAY || matchStartTimeUtc.TimeOfDay > new TimeSpan(17,0,0)){
+                    matchStartTimeUtc = matchStartTimeUtc.Date.AddDays(1).AddHours(10).ToUniversalTime();
                     matchScheduleOnThisDate = 0;
                 }
-                match.GameScheduled = matchStartTime;
+                match.GameScheduled = matchStartTimeUtc;
                 matchScheduleOnThisDate++;
-                matchStartTime = matchStartTime.AddMinutes(150); // 1.5 hour gap
+                //TODO: Configure break time dynamically
+                matchStartTimeUtc = matchStartTimeUtc.AddHours(2).ToUniversalTime(); // match duration 1 hour + 1 hour break after each match
             }
-
             return matches;
         }
     }
