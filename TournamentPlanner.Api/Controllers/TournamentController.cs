ee901a1ace37cfa66e621eeeef9a6a2c0098cad2
knightfury16 <suhaibknight@gmail.com>
2024-05-13 18:27:18 +0600

    tournament controller added

---
+++ TournamentPlanner.Api/Controllers/TournamentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Application.UseCases.AddPlayer;
+using TournamentPlanner.Application.UseCases.TournamentUseCase;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/tournamnet")]
+    public class TournamentController : ControllerBase
+    {
+        private readonly ITournamentUseCase tournamnetUseCase;
+
+        public TournamentController(ITournamentUseCase tournamnetUseCase)
+        {
+            this.tournamnetUseCase = tournamnetUseCase;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTournament([FromBody] TournamentDto tournamentDto)
+        {
+            var tour = await tournamnetUseCase.AddTournamnet(tournamentDto);
+            return Ok(tour);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTournament()
+        {
+            IEnumerable<Tournament> tournamets = await tournamnetUseCase.GetAll();
+            return Ok(tournamets);
+        }
+    }
+}
