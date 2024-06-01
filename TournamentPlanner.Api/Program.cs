c15a13ad12f2f0410182e1f2fee96b7534b0eee0
knightfury16 <suhaibknight@gmail.com>
2024-06-01 15:35:16 +0600

    configured serilog with console and seq sink

--- TournamentPlanner.Api/Program.cs
+++ TournamentPlanner.Api/Program.cs
@@ -1,37 +1,71 @@
 using TournamentPlanner.Infrastructure;
 using TournamentPlanner.Application;
 using TournamentPlanner.Api.Middleware;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-// TODO: configure error handling middleware
-// Add services to the container.
-
-builder.Services.AddControllers();
-var configuration = builder.Configuration;
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddInfrastructureServices(configuration);
-builder.Services.AddApplicationServices();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Configuring serilog service
+    builder.Services.AddSerilog((services, config) => config
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    var configuration = builder.Configuration;
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddInfrastructureServices(configuration);
+    builder.Services.AddApplicationServices();
+
+    var app = builder.Build();
+
+    //serilog config
+    app.UseSerilogRequestLogging(option =>
+    {
+        option.MessageTemplate = "HTTP {RequestMethod} {RequestPath} ({UserId}) responded {StatusCode} in {Elapsed:0.0000}ms";
+    });
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+    app.Run();
 }
+catch (System.Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-app.Run();
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+return 0;
