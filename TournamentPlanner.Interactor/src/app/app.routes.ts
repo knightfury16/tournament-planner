07564cbeec39f6d5e2ae10ca394aa3c8800e4e9a
knightfury16 <suhaibknight@gmail.com>
2024-06-05 17:44:20 +0600

    added some route to test

--- TournamentPlanner.Interactor/src/app/app.routes.ts
+++ TournamentPlanner.Interactor/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting.component';
+import { SayingGoodbyeComponent } from './saying-goodbye/saying-goodbye.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {path:'', redirectTo:'/greeting', pathMatch:'full'},
+    {path: 'greeting', component: GreetingComponent},
+    {path:'saying-goodbye', component: SayingGoodbyeComponent}
+];
---
+++ TournamentPlanner.Interactor/src/app/saying-goodbye/saying-goodbye.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saying-goodbye',
+  standalone: true,
+  imports: [],
+  templateUrl: './saying-goodbye.component.html',
+  styleUrl: './saying-goodbye.component.scss'
+})
+export class SayingGoodbyeComponent {
+
+}
