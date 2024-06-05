bc1c46f6a72249229f53f4a4660caeb4e1ebd8f6
knightfury16 <suhaibknight@gmail.com>
2024-06-05 19:01:55 +0600

    number number display added

--- TournamentPlanner.Interactor/src/app/regular-number/regular-number.component.ts
+++ TournamentPlanner.Interactor/src/app/regular-number/regular-number.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, computed, input, signal } from '@angular/core';
 import { RegularDigitComponent } from '../regular-digit/regular-digit.component';
 
 @Component({
@@ -9,5 +9,20 @@ import { RegularDigitComponent } from '../regular-digit/regular-digit.component'
   styleUrl: './regular-number.component.scss'
 })
 export class RegularNumberComponent {
+  _number = signal(0);
+  @Input() set number(value: number) {
+    this._number.set(value);
+  }
 
+  private getDigit = (position: number):number => {
+    if(this._number() / Math.pow(10, position) < 1){
+      return -1
+    }
+    return Math.floor(this._number() / Math.pow(10, position)) % 10;
+  };
+
+  first = computed(() => this._number() % 10);
+  second = computed(() => this.getDigit(1));
+  third = computed(() => this.getDigit(2));
+  fourth = computed(() => this.getDigit(3));
 }
