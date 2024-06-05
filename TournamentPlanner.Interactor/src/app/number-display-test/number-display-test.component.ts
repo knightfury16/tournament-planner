2180129897ad59c6f5f9dc4a5566275d53397fdf
knightfury16 <suhaibknight@gmail.com>
2024-06-05 22:52:07 +0600

    a simple timer added to show seven segment

--- TournamentPlanner.Interactor/src/app/number-display-test/number-display-test.component.ts
+++ TournamentPlanner.Interactor/src/app/number-display-test/number-display-test.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RegularNumberComponent } from '../regular-number/regular-number.component';
 import { SevenSegmentNumberComponent } from '../seven-segment-number/seven-segment-number.component';
 
 @Component({
   selector: 'app-number-display-test',
   standalone: true,
-  imports: [RegularNumberComponent,SevenSegmentNumberComponent],
+  imports: [RegularNumberComponent, SevenSegmentNumberComponent],
   templateUrl: './number-display-test.component.html',
-  styleUrl: './number-display-test.component.scss'
+  styleUrl: './number-display-test.component.scss',
 })
 export class NumberDisplayTestComponent {
+  _number = signal(0);
+  private intervalId: any;
 
+  //update the number base on time  ngOnInit() {
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this._number.update((n) => n + 1);
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
--- TournamentPlanner.Interactor/src/app/seven-segment-number/seven-segment-number.component.ts
+++ TournamentPlanner.Interactor/src/app/seven-segment-number/seven-segment-number.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 import { SevenSegmentDigitComponent } from '../seven-segment-digit/seven-segment-digit.component';
 
 @Component({
@@ -6,8 +6,23 @@ import { SevenSegmentDigitComponent } from '../seven-segment-digit/seven-segment
   standalone: true,
   imports: [SevenSegmentDigitComponent],
   templateUrl: './seven-segment-number.component.html',
-  styleUrl: './seven-segment-number.component.scss'
+  styleUrl: './seven-segment-number.component.scss',
 })
 export class SevenSegmentNumberComponent {
+  _number = signal(0);
+  @Input() set number(value: number) {
+    this._number.set(value);
+  }
 
+  private getDigit = (position: number): number => {
+    if (this._number() / Math.pow(10, position) < 1) {
+      return -1;
+    }
+    return Math.floor(this._number() / Math.pow(10, position)) % 10;
+  };
+
+  first = computed(() => this._number() % 10);
+  second = computed(() => this.getDigit(1));
+  third = computed(() => this.getDigit(2));
+  fourth = computed(() => this.getDigit(3));
 }
