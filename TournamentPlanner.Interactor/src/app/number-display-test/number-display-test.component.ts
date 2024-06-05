4d2d8b76feac370ede9d2607ac3a6bfa3c461d19
knightfury16 <suhaibknight@gmail.com>
2024-06-05 23:14:55 +0600

    input field added

--- TournamentPlanner.Interactor/src/app/number-display-test/number-display-test.component.ts
+++ TournamentPlanner.Interactor/src/app/number-display-test/number-display-test.component.ts
@@ -1,28 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RegularNumberComponent } from '../regular-number/regular-number.component';
 import { SevenSegmentNumberComponent } from '../seven-segment-number/seven-segment-number.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-number-display-test',
   standalone: true,
-  imports: [RegularNumberComponent, SevenSegmentNumberComponent],
+  imports: [RegularNumberComponent, SevenSegmentNumberComponent, ReactiveFormsModule,CommonModule],
   templateUrl: './number-display-test.component.html',
   styleUrl: './number-display-test.component.scss',
 })
 export class NumberDisplayTestComponent {
-  _number = signal(0);
-  private intervalId: any;
-
-  //update the number base on time  ngOnInit() {
-  ngOnInit() {
-    this.intervalId = setInterval(() => {
-      this._number.update((n) => n + 1);
-    }, 1000);
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
+  numberInput = new FormControl(0);
 }
