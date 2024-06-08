829bdd505b83330004f5b483fb5e2480d84a1285
knightfury16 <suhaibknight@gmail.com>
2024-06-08 22:39:21 +0600

    converted digits to signal array

--- TournamentPlanner.Interactor/src/app/seven-segment-number/seven-segment-number.component.ts
+++ TournamentPlanner.Interactor/src/app/seven-segment-number/seven-segment-number.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   Input,
+  Signal,
   WritableSignal,
   computed,
   effect,
@@ -17,30 +18,57 @@ import { CommonModule } from '@angular/common';
   styleUrl: './seven-segment-number.component.scss',
 })
 export class SevenSegmentNumberComponent {
-  private _numberOfDigit: WritableSignal<number> = signal<number>(0);
-  private _number = signal(0);
-  public _digits: number[] = [];
+  _number = signal(0);
+  _digits = signal<unknown[]>([0, 0, 0, 0]);
 
   @Input() set number(value: number) {
     this._number.set(value);
-    this.computeAndSetDigits();
   }
+
   @Input() set numberOfDigit(value: number) {
-    this._numberOfDigit.set(value);
-  }
-
-  private computeAndSetDigits() {
-    const numberOfDigits: number = this._numberOfDigit();
-    this._digits = [];
-
-    for (let i = 0; i < numberOfDigits ; i++) {
-      this._digits[i] = this.getDigit(i);
+    let newDigits: unknown[] = [];
+    for (let i = 0; i < value; i++) {
+      newDigits.push(0);
     }
-    this._digits.reverse();
+
+    // Set the signal
+    this._digits.set(newDigits);
   }
 
-  private getDigit = (position: number): number => {
-    const divisor = Math.pow(10, position);
-    return Math.floor(this._number() / divisor) % 10;
-  };
+  _numberOfDigits = computed(() => this._digits().length);
+
+  constructor() {
+    // This is just for demonstration purposes. Sometime, you
+    // need debug output whenever a signal changes. This is how
+    // you implement that.
+    effect(() => console.log(this._digits()));
+  }
+
+  public getDigit(index: number): Signal<number> {
+    return computed(() => {
+      // We have to compute the digit on the given number.
+      let digit = this._number() / Math.pow(10, index);
+      if (index > 0 && digit < 1) {
+        // We display a zero only if we were asked for the
+        // first digit. This is necessary to avoid printing "0001"
+        // when the value is 1.
+        return -1;
+      }
+      return Math.floor(digit) % 10;
+    });
+  }
+  // private computeAndSetDigits() {
+  //   const numberOfDigits: number = this._numberOfDigit();
+  //   this._digits = [];
+
+  //   for (let i = 0; i < numberOfDigits; i++) {
+  //     this._digits[i] = this.getDigit(i);
+  //   }
+  //   this._digits.reverse();
+  // }
+
+  // private getDigit = (position: number): number => {
+  //   const divisor = Math.pow(10, position);
+  //   return Math.floor(this._number() / divisor) % 10;
+  // };
 }
