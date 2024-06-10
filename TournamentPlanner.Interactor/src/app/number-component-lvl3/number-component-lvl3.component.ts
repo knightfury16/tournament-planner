e746c3eca909b3c7a2643ab1ed646706727c418d
knightfury16 <suhaibknight@gmail.com>
2024-06-10 23:23:21 +0600

    number of digit and number bounded to component

--- TournamentPlanner.Interactor/src/app/number-component-lvl3/number-component-lvl3.component.ts
+++ TournamentPlanner.Interactor/src/app/number-component-lvl3/number-component-lvl3.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-component-lvl3',
   standalone: true,
   imports: [],
   templateUrl: './number-component-lvl3.component.html',
-  styleUrl: './number-component-lvl3.component.scss'
+  styleUrl: './number-component-lvl3.component.scss',
 })
 export class NumberComponentLvl3Component {
+  _number = signal<number>(0);
+  _digits = signal<unknown[]>([0, 0, 0, 0]);
 
+  _numberOfdigits = computed(() => this._digits().length);
+
+  @Input() set nubmerOfDigit(value: number) {
+    let digits = [];
+
+    for (let i = 0; i < value; i++) {
+      digits.push(0);
+    }
+    this._digits.set(digits);
+  }
+
+  @Input() set number(value: number) {
+    this._number.set(value);
+  }
 }
