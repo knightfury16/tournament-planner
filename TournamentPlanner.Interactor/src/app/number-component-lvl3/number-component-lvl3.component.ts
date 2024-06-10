14d23fe31f8efaa858cc62655317513672cb3609
knightfury16 <suhaibknight@gmail.com>
2024-06-10 23:07:15 +0600

    Another component for lvl3 created

---
+++ TournamentPlanner.Interactor/src/app/number-component-lvl3/number-component-lvl3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-number-component-lvl3',
+  standalone: true,
+  imports: [],
+  templateUrl: './number-component-lvl3.component.html',
+  styleUrl: './number-component-lvl3.component.scss'
+})
+export class NumberComponentLvl3Component {
+
+}
--- TournamentPlanner.Interactor/src/app/number-display-test/number-display-test.component.ts
+++ TournamentPlanner.Interactor/src/app/number-display-test/number-display-test.component.ts
@@ -3,11 +3,12 @@ import { RegularNumberComponent } from '../regular-number/regular-number.compone
 import { SevenSegmentNumberComponent } from '../seven-segment-number/seven-segment-number.component';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NumberComponentLvl3Component } from '../number-component-lvl3/number-component-lvl3.component';
 
 @Component({
   selector: 'app-number-display-test',
   standalone: true,
-  imports: [RegularNumberComponent, SevenSegmentNumberComponent, ReactiveFormsModule,CommonModule],
+  imports: [RegularNumberComponent, SevenSegmentNumberComponent, ReactiveFormsModule,CommonModule, NumberComponentLvl3Component],
   templateUrl: './number-display-test.component.html',
   styleUrl: './number-display-test.component.scss',
 })
