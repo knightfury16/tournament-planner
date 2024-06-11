0b471c53b46cf80afb642237d26b4cc47c32c073
knightfury16 <suhaibknight@gmail.com>
2024-06-12 00:00:01 +0600

    Added a signal to compute total price of the cart

--- TournamentPlanner.Interactor/src/app/shopping-cart.service.ts
+++ TournamentPlanner.Interactor/src/app/shopping-cart.service.ts
@@ -1,43 +1,50 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, computed, signal } from '@angular/core';
 import { Offering } from './shopping.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingCartService {
-
   public shoppingCart = signal<Offering[]>([]);
 
-  public addToCart(offering: Offering){
-    this.shoppingCart.update(cart => {
-
+  public addToCart(offering: Offering) {
+    this.shoppingCart.update((cart) => {
       //* check if the item already exists in the cart
-      const itemIndex = cart.findIndex(item => item.name === offering.name);
-
+      const itemIndex = cart.findIndex((item) => item.name === offering.name);
 
       //*if already in cart then just add the price of the item to the existing cart item
       //* item already exists, update its price
-      if(itemIndex !== -1){
+      if (itemIndex !== -1) {
         let updatedCart = [...cart];
+        const updatedPrice = parseFloat(
+          (updatedCart[itemIndex].priceEur + offering.priceEur).toFixed(2)
+        );
 
         //* update the price
         updatedCart[itemIndex] = {
           ...updatedCart[itemIndex],
-          priceEur: updatedCart[itemIndex].priceEur + offering.priceEur
+          priceEur: updatedPrice,
         };
 
         return updatedCart;
       }
 
       //* if item dont exists on the cart just add the item to the cart
-      //! test if spread is necessary, that is if i change value of this offering later, will it impact in the cart
-      cart = [...cart, offering];
-      offering.priceEur = 100;
+      cart = [...cart, { ...offering }];
 
       //* return the updated cart
       return cart;
-
     });
   }
+
+  public totalPrice = computed(() => {
+    let totalPrice = 0;
+
+    this.shoppingCart().forEach((item) => {
+      totalPrice += item.priceEur;
+    });
+
+    return parseFloat(totalPrice.toFixed(2));
+  });
 
 }
