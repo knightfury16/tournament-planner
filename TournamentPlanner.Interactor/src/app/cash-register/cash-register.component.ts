096be898a6e0274bd16d5657db69b64ad4fe4d21
knightfury16 <suhaibknight@gmail.com>
2024-06-12 00:00:46 +0600

    Integrated app number component lvl3 with cash register

--- TournamentPlanner.Interactor/src/app/cash-register/cash-register.component.ts
+++ TournamentPlanner.Interactor/src/app/cash-register/cash-register.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Offering, ShoppingService } from '../shopping.service';
 import { CommonModule } from '@angular/common';
 import { ShoppingCartService } from '../shopping-cart.service';
+import { NumberComponentLvl3Component } from '../number-component-lvl3/number-component-lvl3.component';
 
 @Component({
   selector: 'app-cash-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NumberComponentLvl3Component],
   templateUrl: './cash-register.component.html',
   styleUrl: './cash-register.component.scss'
 })
@@ -19,6 +20,13 @@ export class CashRegisterComponent {
   }
 
 
-
+  public getTotalPriceDigit(): number{
+    let numberString = this.cart.totalPrice().toString();
+    if(numberString.indexOf('.') == -1){
+      return numberString.length;
+    }
+    return numberString.length - 1;
+    
+  }
 }
  
