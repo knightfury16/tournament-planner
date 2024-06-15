821f3c439c98bdddf6b7cc04770adbd90fb2ddfd
knightfury16 <suhaibknight@gmail.com>
2024-06-15 20:19:56 +0600

    inline type declaration

--- TournamentPlanner.Interactor/src/app/trippin.service.ts
+++ TournamentPlanner.Interactor/src/app/trippin.service.ts
@@ -1,15 +1,18 @@
 import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TRIPPIN_BASE_URL } from './app.config';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrippinService {
-  constructor(@Inject(TRIPPIN_BASE_URL) private baseUrl: string) {}
+  constructor(
+    private httpClient: HttpClient,
+    @Inject(TRIPPIN_BASE_URL) private baseUrl: string
+  ) {}
 
-  public getPeople(name: string): Observable<any[]> {
-    // request the web and fetch data here
-    return of([1, 2, 3, 4, 5]);
+  public getPeople(name: string): Observable<{ value: any[] }> {
+    return this.httpClient.get<{ value: any[] }>(`${this.baseUrl}/People`);
   }
 }
