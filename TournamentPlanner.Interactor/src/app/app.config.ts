411d1d5dd4f40c5f19d7cfc683de49f3c33900aa
knightfury16 <suhaibknight@gmail.com>
2024-06-15 19:49:48 +0600

    base url set up as injectable token in app.Config.ts

--- TournamentPlanner.Interactor/src/app/app.config.ts
+++ TournamentPlanner.Interactor/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+export const TRIPPIN_BASE_URL = new InjectionToken<string>('TRIPPIN_BASE_URL');
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: TRIPPIN_BASE_URL,
+      useValue:
+        'https://services.odata.org/TripPinRESTierService/(S(dwkum2ychcndvz35fd2eivpa))',
+    },
+  ],
 };
--- TournamentPlanner.Interactor/src/app/trippin.service.ts
+++ TournamentPlanner.Interactor/src/app/trippin.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { TRIPPIN_BASE_URL } from './app.config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TrippinService {
+  constructor(@Inject(TRIPPIN_BASE_URL) private baseUrl: string) {}
 
-  constructor() { }
-
-  public getPeople(name: string): Observable<any[]>{
+  public getPeople(name: string): Observable<any[]> {
     // request the web and fetch data here
-    return of([1,2,3,4,5]);
+    return of([1, 2, 3, 4, 5]);
   }
 }
