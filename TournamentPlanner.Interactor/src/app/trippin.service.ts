792a49cf75d4ae2660d74cac5ffce43d07e20768
knightfury16 <suhaibknight@gmail.com>
2024-06-16 21:57:23 +0600

    applied generated type

--- TournamentPlanner.Interactor/src/app/trippin.service.ts
+++ TournamentPlanner.Interactor/src/app/trippin.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TRIPPIN_BASE_URL } from './app.config';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Person } from '../../models/trippin/TrippinModel';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class TrippinService {
     @Inject(TRIPPIN_BASE_URL) private baseUrl: string
   ) {}
 
-  public getPeople(nameFilter: string): Observable<{ value: any[] }> {
+  public getPeople(nameFilter: string): Observable<{ value: Person[] }> {
     let params = new HttpParams();
     if (nameFilter) {
       params = params.set('$filter', `contains(UserName,'${nameFilter}')`);
@@ -21,7 +22,7 @@ export class TrippinService {
     params = params.set("$select",'UserName,FirstName,MiddleName,LastName,Age');
     params = params.set("$orderby", 'LastName,FirstName');
 
-    return this.httpClient.get<{ value: any[] }>(`${this.baseUrl}/People`, {
+    return this.httpClient.get<{ value: Person[] }>(`${this.baseUrl}/People`, {
       params,
     });
   }
