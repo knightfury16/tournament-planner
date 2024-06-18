6ff9c87c1db76add00daf8010d10dce8a4bb3ef4
knightfury16 <suhaibknight@gmail.com>
2024-06-18 22:45:48 +0600

    added trip list component and route

--- TournamentPlanner.Interactor/src/app/app.routes.ts
+++ TournamentPlanner.Interactor/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { NumberDisplayTestComponent } from './number-display-test/number-display-test.component';
 import { CashRegisterComponent } from './cash-register/cash-register.component';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { TripListComponent } from './trip-list/trip-list.component';
 
 export const routes: Routes = [ 
     {path:'', redirectTo:'people', pathMatch:'full'},
     {path:'digit', component:NumberDisplayTestComponent, pathMatch:'full'},
     {path:'cash-register', component:CashRegisterComponent},
-    {path:'people', component:PeopleListComponent}
+    {path:'people', component:PeopleListComponent},
+    {path:'people/:userName/trips', component:TripListComponent},
 ];
