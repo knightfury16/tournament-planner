7ef8f4ca137024c8ddd41e0798fc1ec10ae043d3
knightfury16 <suhaibknight@gmail.com>
2024-06-19 23:14:34 +0600

    get trip servce added

--- TournamentPlanner.Interactor/src/app/trippin.service.ts
+++ TournamentPlanner.Interactor/src/app/trippin.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TRIPPIN_BASE_URL } from './app.config';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Person } from '../../models/trippin/TrippinModel';
+import { Person, Trip } from '../../models/trippin/TrippinModel';
 
 @Injectable({
   providedIn: 'root',
@@ -19,11 +19,23 @@ export class TrippinService {
       params = params.set('$filter', `contains(UserName,'${nameFilter}')`);
     }
 
-    params = params.set("$select",'UserName,FirstName,MiddleName,LastName,Age');
-    params = params.set("$orderby", 'LastName,FirstName');
+    params = params.set(
+      '$select',
+      'UserName,FirstName,MiddleName,LastName,Age'
+    );
+    params = params.set('$orderby', 'LastName,FirstName');
 
     return this.httpClient.get<{ value: Person[] }>(`${this.baseUrl}/People`, {
       params,
     });
+  }
+
+  public getTrips(userName: string): Observable<{ value: Trip[] }> {
+    let params = new HttpParams()
+      .set('$select', 'TripId,Name,Budget,Description,Tags,StartsAt,EndsAt')
+      .set('$orderby', 'EndsAt desc');
+
+    const url = `${this.baseUrl}/People('${userName}')/Trips`;
+    return this.httpClient.get<{ value: Trip[] }>(url, { params });
   }
 }
