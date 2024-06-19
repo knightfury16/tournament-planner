05da134e9afbff7ae97a15755b7e6bdd105d7576
knightfury16 <suhaibknight@gmail.com>
2024-06-19 23:15:05 +0600

    trip list component finish

--- TournamentPlanner.Interactor/src/app/trip-list/trip-list.component.ts
+++ TournamentPlanner.Interactor/src/app/trip-list/trip-list.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { TrippinService } from '../trippin.service';
+import { Observable } from 'rxjs';
+import { Trip } from '../../../models/trippin/TrippinModel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trip-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './trip-list.component.html',
   styleUrl: './trip-list.component.scss',
 })
-export class TripListComponent {
+export class TripListComponent implements AfterViewInit {
   @Input() public userName?: string;
+  public data$?: Observable<{ value: Trip[] }>;
+
+  constructor(private trip: TrippinService) {}
+  ngAfterViewInit(): void {
+    if (this.userName) {
+      this.data$ = this.trip.getTrips(this.userName);
+    }
+  }
 }
