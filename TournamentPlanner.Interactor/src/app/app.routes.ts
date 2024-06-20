b29aa57b2cc048997e407205437f45bdee62e327
knightfury16 <suhaibknight@gmail.com>
2024-06-20 22:31:22 +0600

    airport route added

--- TournamentPlanner.Interactor/src/app/app.routes.ts
+++ TournamentPlanner.Interactor/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NumberDisplayTestComponent } from './number-display-test/number-display
 import { CashRegisterComponent } from './cash-register/cash-register.component';
 import { PeopleListComponent } from './people-list/people-list.component';
 import { TripListComponent } from './trip-list/trip-list.component';
+import { AirportsListComponent } from './airports-list/airports-list.component';
 
 export const routes: Routes = [ 
     {path:'', redirectTo:'people', pathMatch:'full'},
     {path:'digit', component:NumberDisplayTestComponent, pathMatch:'full'},
     {path:'cash-register', component:CashRegisterComponent},
     {path:'people', component:PeopleListComponent},
+    {path:'airports', component:AirportsListComponent},
     {path:'people/:userName/trips', component:TripListComponent},
 ];
