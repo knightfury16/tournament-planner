b0ac40b7057de1d9eaeb6948a48ee5d329569c48
knightfury16 <suhaibknight@gmail.com>
2024-06-21 21:35:22 +0600

    add trip functional, observable made to promise by firstValueFrom

--- TournamentPlanner.Interactor/src/app/trip-list/trip-list.component.ts
+++ TournamentPlanner.Interactor/src/app/trip-list/trip-list.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { TrippinService } from '../trippin.service';
-import { Observable } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
 import { Trip } from '../../../models/trippin/TrippinModel';
 import { CommonModule } from '@angular/common';
 import {
@@ -9,6 +9,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Guid } from 'guid-typescript';
 
 @Component({
   selector: 'app-trip-list',
@@ -23,7 +24,7 @@ export class TripListComponent implements AfterViewInit {
 
   public tripForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
-    budget: new FormControl('1000', [Validators.required]),
+    budget: new FormControl(1000, [Validators.required]),
     description: new FormControl('', [Validators.required]),
     tags: new FormControl(''),
     startsAt: new FormControl('', [Validators.required]),
@@ -37,10 +38,19 @@ export class TripListComponent implements AfterViewInit {
     }
   }
 
-  addTrip(){
-
+  async addTrip(){
+    const newTrip: Trip = {
+      TripId: Math.floor(Math.random() * 10000),
+      ShareId: Guid.create().toString(),
+      Name: this.tripForm.value.name ?? '',
+      Budget: this.tripForm.value.budget ?? 0,
+      Description: this.tripForm.value.description ?? '',
+      Tags: (this.tripForm.value.tags ?? '').split(','),
+      StartsAt: new Date(this.tripForm.value.startsAt ?? '').toISOString(),
+      EndsAt: new Date(this.tripForm.value.endsAt ?? '').toISOString(),
+    }
+    await firstValueFrom(this.trip.addTrip(this.userName!, newTrip));
+    this.ngAfterViewInit();
+    this.tripForm.reset();
   }
-
-
-
 }
