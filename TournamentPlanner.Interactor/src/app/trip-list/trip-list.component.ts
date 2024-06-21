8c9b0204fa0eabc22dc766a61a00fb3463b69b64
knightfury16 <suhaibknight@gmail.com>
2024-06-21 20:49:20 +0600

    added a formGroup to add trip

--- TournamentPlanner.Interactor/src/app/trip-list/trip-list.component.ts
+++ TournamentPlanner.Interactor/src/app/trip-list/trip-list.component.ts
@@ -3,11 +3,17 @@ import { TrippinService } from '../trippin.service';
 import { Observable } from 'rxjs';
 import { Trip } from '../../../models/trippin/TrippinModel';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-trip-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './trip-list.component.html',
   styleUrl: './trip-list.component.scss',
 })
@@ -15,10 +21,26 @@ export class TripListComponent implements AfterViewInit {
   @Input() public userName?: string;
   public data$?: Observable<{ value: Trip[] }>;
 
+  public tripForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    budget: new FormControl('1000', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    tags: new FormControl(''),
+    startsAt: new FormControl('', [Validators.required]),
+    endsAt: new FormControl('', [Validators.required]),
+  });
+
   constructor(private trip: TrippinService) {}
   ngAfterViewInit(): void {
     if (this.userName) {
       this.data$ = this.trip.getTrips(this.userName);
     }
   }
+
+  addTrip(){
+
+  }
+
+
+
 }
