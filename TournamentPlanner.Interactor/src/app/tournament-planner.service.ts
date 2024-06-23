a3706028d4fc0a18a0f4a7dd54c28c7699ce35f4
knightfury16 <suhaibknight@gmail.com>
2024-06-23 22:22:00 +0600

    tp service modified for testing purpose

--- TournamentPlanner.Interactor/src/app/tournament-planner.service.ts
+++ TournamentPlanner.Interactor/src/app/tournament-planner.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { TP_BASE_URL } from './app.config';
-import { Observable } from 'rxjs';
+import { Observable, of, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,13 @@ export class TournamentPlannerService {
   constructor(private httpClient: HttpClient,@Inject(TP_BASE_URL) private baseUrl: string) { }
 
 
-  public getTournament(name:string): Observable<any>{
+  public getTournament(name:string): Observable<any[]>{
 
     let params = new HttpParams();
     if(name){
       params = params.set("name", name);
     }
-    return this.httpClient.get<Observable<any>>(`${this.baseUrl}/tournament`,{params});
+    return of([1,2,3]);
+    // return this.httpClient.get<Observable<any>>(`${this.baseUrl}/tournament`,{params});
   }
 }
