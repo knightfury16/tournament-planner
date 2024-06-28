13764058e3bec929ffa236bddac8952ad52c83a5
knightfury16 <suhaibknight@gmail.com>
2024-06-28 08:58:00 +0600

    add navbar component

--- TournamentPlanner.Interactor/src/app/app.component.ts
+++ TournamentPlanner.Interactor/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [NavBarComponent, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ TournamentPlanner.Interactor/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+
+}
