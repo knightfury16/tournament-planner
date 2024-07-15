e6845bd7b0022689a6fd6bf674f8a5670ad39dfd
knightfury16 <suhaibknight@gmail.com>
2024-07-15 22:06:31 +0600

    change date type to datetime-local in add tournament form

--- TournamentPlanner.Interactor/src/app/add-tournament/add-tournament.component.ts
+++ TournamentPlanner.Interactor/src/app/add-tournament/add-tournament.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { TournamentDto } from '../tp-model/TpModel';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-tournament',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-tournament.component.html',
-  styleUrl: './add-tournament.component.scss'
+  styleUrl: './add-tournament.component.scss',
 })
 export class AddTournamentComponent {
 
+  public tournamentDto: TournamentDto;
+
+  constructor(){
+    this.tournamentDto = {
+      name: ""
+    }
+  }
+
+  onClickCreate() {
+    throw new Error('Method not implemented.');
+  }
 }
