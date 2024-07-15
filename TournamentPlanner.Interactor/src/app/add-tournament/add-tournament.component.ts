2e5a2c40b22a01898a03b517cfdd965ea371e0f1
knightfury16 <suhaibknight@gmail.com>
2024-07-15 22:39:54 +0600

    converted local time to utc in tournament form

--- TournamentPlanner.Interactor/src/app/add-tournament/add-tournament.component.ts
+++ TournamentPlanner.Interactor/src/app/add-tournament/add-tournament.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TournamentDto } from '../tp-model/TpModel';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TimeService } from '../time.service';
 
 @Component({
   selector: 'app-add-tournament',
@@ -11,16 +12,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-tournament.component.scss',
 })
 export class AddTournamentComponent {
-
   public tournamentDto: TournamentDto;
 
-  constructor(){
+  constructor(private timeService: TimeService) {
     this.tournamentDto = {
-      name: ""
-    }
+      name: '',
+    };
   }
 
   onClickCreate() {
     throw new Error('Method not implemented.');
+  }
+
+  onStartDateChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const localDate = input.value;
+    this.tournamentDto.startDate = this.timeService.convertToUTC(localDate);
+  }
+  onEndDateChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const localDate = input.value;
+    this.tournamentDto.endDate = this.timeService.convertToUTC(localDate);
   }
 }
