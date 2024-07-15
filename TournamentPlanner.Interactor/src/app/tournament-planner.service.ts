fef60727e90960be905dc4eea97d6533eb925dd6
knightfury16 <suhaibknight@gmail.com>
2024-07-15 22:32:19 +0600

    created add tournament service method

--- TournamentPlanner.Interactor/src/app/tournament-planner.service.ts
+++ TournamentPlanner.Interactor/src/app/tournament-planner.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { TP_BASE_URL } from './app.config';
 import { Observable, of, retry } from 'rxjs';
+import { TournamentDto } from './tp-model/TpModel';
 
 @Injectable({
   providedIn: 'root',
@@ -27,5 +28,12 @@ export class TournamentPlannerService {
       params = params.set('tournamentId', tournamentId);
     }
     return this.httpClient.get<any[]>(`${this.baseUrl}/matches`, { params });
+  }
+
+  public addTournament(tournamentDto: TournamentDto): Observable<any[]> {
+    return this.httpClient.post<any[]>(
+      `${this.baseUrl}/tournament`,
+      tournamentDto
+    );
   }
 }
