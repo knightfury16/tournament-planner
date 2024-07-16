15005e01e3373399fe598a2788b9937f40264416
knightfury16 <suhaibknight@gmail.com>
2024-07-16 23:35:12 +0600

    tournament matches component functional

--- TournamentPlanner.Interactor/src/app/tournament-matches/tournament-matches.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-matches/tournament-matches.component.ts
@@ -1,18 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TournamentPlannerService } from '../tournament-planner.service';
+import { CommonModule } from '@angular/common';
+import { map, Observable } from 'rxjs';
+import { Match } from '../tp-model/TpModel';
 
 @Component({
   selector: 'app-tournament-matches',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CommonModule],
   templateUrl: './tournament-matches.component.html',
-  styleUrl: './tournament-matches.component.scss'
+  styleUrl: './tournament-matches.component.scss',
 })
-export class TournamentMatchesComponent {
+export class TournamentMatchesComponent implements OnInit {
+  @Input() public tournamentId?: number;
 
-  @Input() public tournamentId?: string;
+  displayedColumns: string[] = [
+    'firstPlayerName',
+    'secondPlayerName',
+    'winner',
+    'gameScheduled',
+  ];
+  dataSource = new MatTableDataSource<Match>();
 
-  constructor(){
-    console.log("From matches component", this.tournamentId)
+  constructor(private tp: TournamentPlannerService) {}
+  ngOnInit(): void {
+    console.log('From matches component', this.tournamentId);
+    // asserting not null here
+    this.tp
+      .getMatches(this.tournamentId!)
+      .pipe(
+        map((matches) => matches ?? []) // Ensure it always returns an array
+      )
+      .subscribe((matches) => {
+        this.dataSource.data = matches;
+      });
   }
-
 }
