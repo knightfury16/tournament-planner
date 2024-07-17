9ae217d4ddbc433034e899fbe9415a6c1da6d30e
knightfury16 <suhaibknight@gmail.com>
2024-07-17 12:24:32 +0600

    Domain change on BaseEntity, added createdAt and updateAt property

--- TournamentPlanner.Domain/Common/BaseEntity.cs
+++ TournamentPlanner.Domain/Common/BaseEntity.cs
@@ -8,5 +8,7 @@ namespace TournamentPlanner.Domain.Common
     public abstract class BaseEntity
     {
         public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
