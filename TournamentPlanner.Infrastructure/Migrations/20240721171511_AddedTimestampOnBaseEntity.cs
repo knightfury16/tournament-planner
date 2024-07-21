56270f7cd321a916db8085df2e974fedb80e68ed
knightfury16 <suhaibknight@gmail.com>
2024-07-21 23:47:10 +0600

    migration update

---
+++ TournamentPlanner.Infrastructure/Migrations/20240721171511_AddedTimestampOnBaseEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TournamentPlanner.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTimestampOnBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Tournaments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Rounds",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Players",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Matches",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Rounds");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Matches");
+        }
+    }
+}
