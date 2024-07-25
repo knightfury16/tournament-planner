8c4b171b1a282d3c8e0446fde3f95845ce3c2d28
knightfury16 <suhaibknight@gmail.com>
2024-07-25 23:16:15 +0600

    Bug Fix: Adding to DataContext if Tournament was empty even if tournamentId was given it was createing a new empty tournament.

--- TournamentPlanner.Domain/Entities/Round.cs
+++ TournamentPlanner.Domain/Entities/Round.cs
@@ -16,7 +16,7 @@ namespace TournamentPlanner.Domain.Entities
         // TODO: do this through fluent api
         [JsonIgnore]
         public List<Match> Matches { get; set; } = new();
-        public Tournament? Tournament { get; set; } = new();
+        public Tournament? Tournament { get; set; }
         public int? TournamentId { get; set; }
         
     }
