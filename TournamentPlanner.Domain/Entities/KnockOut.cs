ffc696d3d8b0edb532178b9a2e3c28c7e8f96583
knightfury16 <suhaibknight@gmail.com>
2024-07-29 16:59:57 +0600

    modification. ongoing...

--- TournamentPlanner.Domain/Entities/KnockOut.cs
+++ TournamentPlanner.Domain/Entities/KnockOut.cs
@@ -3,10 +3,8 @@ using TournamentPlanner.Domain.Interface;
 
 namespace TournamentPlanner.Domain.Entities
 {
-    public class KnockOut<TScore>: BaseEntity where TScore : IScore
+    public class KnockOut<TScore> : MatchType<TScore> where TScore : IScore
     {
-        public  required string Name { get; set; }
-        public List<Player> Players { get; set; } = new List<Player>();
-        public List<Match<TScore>> Matches { get; set; } = new List<Match<TScore>>();
+        public int Round { get; set; }
     }
 }
