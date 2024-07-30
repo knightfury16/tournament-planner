a9222879230f34f9647ac8295cf994c68a0abb4c
knightfury16 <suhaibknight@gmail.com>
2024-07-30 21:06:52 +0600

    Tournament DbContext set

--- TournamentPlanner.Infrastructure/DataContext/TournamentPlannerDataContext.cs
+++ TournamentPlanner.Infrastructure/DataContext/TournamentPlannerDataContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TournamentPlanner.Domain.Common;
 using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Enum;
 
 namespace TournamentPlanner.Infrastructure.DataContext
 {
@@ -82,6 +83,39 @@ namespace TournamentPlanner.Infrastructure.DataContext
                 entity.Property(p => p.Name).IsRequired();
             });
 
+            modelBuilder.Entity<Tournament>(entity =>
+            {
+                entity.Property(p => p.Name)
+                      .HasMaxLength(250)
+                      .IsRequired();
+                entity.Property(p => p.StartDate)
+                      .IsRequired();
+                entity.Property(p => p.MaxParticipant)
+                      .HasDefaultValue<int>(104); // 26 Group (A-Z) * 4 per group = 104
+                entity.Property(p => p.RegistrationFee)
+                      .HasColumnType("decimal(18,2)");
+                entity.Property(p => p.MinimumAgeOfRegistration)
+                      .HasDefaultValue<int>(18);
+                entity.Property(p => p.WinnerPerGroup)
+                      .HasDefaultValue(2); // Default value = 2
+                entity.Property(p => p.KnockOutStartNumber)
+                      .HasDefaultValue(16); // Default value = 16
+                entity.Property(p => p.ParticipantResolutionStrategy)
+                      .HasDefaultValue(ResolutionStrategy.StatBased)
+                      .HasConversion<string>();
+                entity.Property(p => p.TournamentType)
+                      .HasDefaultValue(TournamentType.GroupStage)
+                      .HasConversion<string>();
+                entity.Property(p => p.GameType)
+                      .IsRequired();
+                entity.Property(p => p.Status)
+                      .HasDefaultValue(TournamentStatus.Draft)
+                      .HasConversion<string>();
+
+            });
+
+            //making all CreatedAt and UpdatedAt filed of all entity that inherit from BaseEntity
+            //TODO: Can do similar thing for player and admin on name and email field
             foreach (var entityType in modelBuilder.Model.GetEntityTypes())
             {
                 if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
