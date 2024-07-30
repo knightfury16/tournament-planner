7662556f7ca868840b3ea78bdacb3f9dfe7b9df6
knightfury16 <suhaibknight@gmail.com>
2024-07-30 20:33:17 +0600

    Player and Admin DbContext set

--- TournamentPlanner.Infrastructure/DataContext/TournamentPlannerDataContext.cs
+++ TournamentPlanner.Infrastructure/DataContext/TournamentPlannerDataContext.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TournamentPlanner.Domain.Common;
 using TournamentPlanner.Domain.Entities;
@@ -11,17 +7,36 @@ namespace TournamentPlanner.Infrastructure.DataContext
     public class TournamentPlannerDataContext : DbContext
     {
         public DbSet<Player> Players { get; set; }
+        public DbSet<Admin> Admins { get; set; }
         public DbSet<Match> Matches { get; set; }
         public DbSet<Round> Rounds { get; set; }
         public DbSet<Tournament> Tournaments { get; set; }
         public TournamentPlannerDataContext(DbContextOptions<TournamentPlannerDataContext> options) : base(options)
-        {}
-        override protected void OnModelCreating(ModelBuilder modelBuilder){
+        { }
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
-            modelBuilder.Entity<Player>(entity => {
+            modelBuilder.Entity<Player>(entity =>
+            {
                 entity.Property(p => p.Name)
                 .IsRequired()
                 .HasMaxLength(250);
+
+                entity.Property(p => p.Email)
+                .IsRequired()
+                .HasAnnotation("RegularExpression", @"^[^@\s]+@[^@\s]+\.[^@\s]+$"); //Basic regex for email
+            });
+
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.Property(p => p.Name)
+                      .IsRequired()
+                      .HasMaxLength(250);
+                entity.Property(p => p.PhoneNumber)
+                      .IsRequired();
+                entity.Property(p => p.Email)
+                .IsRequired()
+                .HasAnnotation("RegularExpression", @"^[^@\s]+@[^@\s]+\.[^@\s]+$"); //Basic regex for email
             });
 
             //* Database provider dont support Player type exception
@@ -30,17 +45,19 @@ namespace TournamentPlanner.Infrastructure.DataContext
             //     entity.Property(p => p.SecondPlayer).IsRequired();
             // });
 
-            modelBuilder.Entity<Round>(entity => {
+            modelBuilder.Entity<Round>(entity =>
+            {
                 entity.Property(p => p.RoundNumber).IsRequired();
             });
 
-            modelBuilder.Entity<Tournament>(entity => {
+            modelBuilder.Entity<Tournament>(entity =>
+            {
                 entity.Property(p => p.Name).IsRequired();
             });
 
-            foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
             {
-                if(typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+                if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
                 {
                     modelBuilder.Entity(entityType.ClrType).Property<DateTime>("CreatedAt").IsRequired();
                     modelBuilder.Entity(entityType.ClrType).Property<DateTime>("UpdatedAt").IsRequired();
