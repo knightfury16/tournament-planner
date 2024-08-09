1dda4eb38cae2633feb03799bba21fa84bf9b3d2
knightfury16 <suhaibknight@gmail.com>
2024-08-09 16:53:45 +0600

    initial migration

---
+++ TournamentPlanner.DataModeling/Migrations/20240809105136_Initial.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TournamentPlanner.DataModeling.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    Weight = table.Column<int>(type: "integer", nullable: false),
+                    GamePlayed = table.Column<int>(type: "integer", nullable: false),
+                    GameWon = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tournaments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    AdminId = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RegistrationLastDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    MaxParticipant = table.Column<int>(type: "integer", nullable: false, defaultValue: 104),
+                    Venue = table.Column<string>(type: "text", nullable: true),
+                    RegistrationFee = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    MinimumAgeOfRegistration = table.Column<int>(type: "integer", nullable: false, defaultValue: 18),
+                    WinnerPerGroup = table.Column<int>(type: "integer", nullable: false, defaultValue: 2),
+                    KnockOutStartNumber = table.Column<int>(type: "integer", nullable: false, defaultValue: 16),
+                    ParticipantResolutionStrategy = table.Column<string>(type: "text", nullable: true, defaultValue: "StatBased"),
+                    TournamentType = table.Column<string>(type: "text", nullable: true, defaultValue: "GroupStage"),
+                    GameTypeId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: true, defaultValue: "Draft"),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tournaments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tournaments_Admins_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "Admins",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tournaments_GameTypes_GameTypeId",
+                        column: x => x.GameTypeId,
+                        principalTable: "GameTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    TournamentId = table.Column<int>(type: "integer", nullable: false),
+                    MatchType = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    Round = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MatchTypes_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TournamentParticipant",
+                columns: table => new
+                {
+                    ParticipantsId = table.Column<int>(type: "integer", nullable: false),
+                    TournamentId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TournamentParticipant", x => new { x.ParticipantsId, x.TournamentId });
+                    table.ForeignKey(
+                        name: "FK_TournamentParticipant_Players_ParticipantsId",
+                        column: x => x.ParticipantsId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TournamentParticipant_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Player1Id = table.Column<int>(type: "integer", nullable: false),
+                    Player2Id = table.Column<int>(type: "integer", nullable: false),
+                    WinnerId = table.Column<int>(type: "integer", nullable: true),
+                    TournamentId = table.Column<int>(type: "integer", nullable: false),
+                    MatchTypeId = table.Column<int>(type: "integer", nullable: false),
+                    GameScheduled = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    GamePlayed = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ScoreJson = table.Column<string>(type: "jsonb", nullable: true),
+                    IsRescheduled = table.Column<bool>(type: "boolean", nullable: false),
+                    RescheduleReason = table.Column<string>(type: "text", nullable: true),
+                    RescheduledById = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Admins_RescheduledById",
+                        column: x => x.RescheduledById,
+                        principalTable: "Admins",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Matches_MatchTypes_MatchTypeId",
+                        column: x => x.MatchTypeId,
+                        principalTable: "MatchTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Players_Player1Id",
+                        column: x => x.Player1Id,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Players_Player2Id",
+                        column: x => x.Player2Id,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Players_WinnerId",
+                        column: x => x.WinnerId,
+                        principalTable: "Players",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Matches_Tournaments_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "Tournaments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchTypeParticipants",
+                columns: table => new
+                {
+                    MatchTypesId = table.Column<int>(type: "integer", nullable: false),
+                    PlayersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchTypeParticipants", x => new { x.MatchTypesId, x.PlayersId });
+                    table.ForeignKey(
+                        name: "FK_MatchTypeParticipants_MatchTypes_MatchTypesId",
+                        column: x => x.MatchTypesId,
+                        principalTable: "MatchTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MatchTypeParticipants_Players_PlayersId",
+                        column: x => x.PlayersId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "GameTypes",
+                columns: new[] { "Id", "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 8, 9, 10, 51, 36, 647, DateTimeKind.Utc).AddTicks(6236), "TableTennis", new DateTime(2024, 8, 9, 10, 51, 36, 647, DateTimeKind.Utc).AddTicks(6237) },
+                    { 2, new DateTime(2024, 8, 9, 10, 51, 36, 647, DateTimeKind.Utc).AddTicks(6243), "Chess", new DateTime(2024, 8, 9, 10, 51, 36, 647, DateTimeKind.Utc).AddTicks(6244) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_MatchTypeId",
+                table: "Matches",
+                column: "MatchTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_Player1Id",
+                table: "Matches",
+                column: "Player1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_Player2Id",
+                table: "Matches",
+                column: "Player2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_RescheduledById",
+                table: "Matches",
+                column: "RescheduledById",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_TournamentId",
+                table: "Matches",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_WinnerId",
+                table: "Matches",
+                column: "WinnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchTypeParticipants_PlayersId",
+                table: "MatchTypeParticipants",
+                column: "PlayersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchTypes_TournamentId",
+                table: "MatchTypes",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TournamentParticipant_TournamentId",
+                table: "TournamentParticipant",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tournaments_AdminId",
+                table: "Tournaments",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tournaments_GameTypeId",
+                table: "Tournaments",
+                column: "GameTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "MatchTypeParticipants");
+
+            migrationBuilder.DropTable(
+                name: "TournamentParticipant");
+
+            migrationBuilder.DropTable(
+                name: "MatchTypes");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Tournaments");
+
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "GameTypes");
+        }
+    }
+}
