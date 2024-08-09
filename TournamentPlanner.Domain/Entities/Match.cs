770a15935004f396524dfa05837e1c592c4e56e8
knightfury16 <suhaibknight@gmail.com>
2024-08-09 16:15:13 +0600

    Match entity updated

--- TournamentPlanner.Domain/Entities/Match.cs
+++ TournamentPlanner.Domain/Entities/Match.cs
@@ -1,29 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+namespace TournamentPlanner.Domain.Entities;
+
 using TournamentPlanner.Domain.Common;
-using TournamentPlanner.Domain.Interface;
 
-namespace TournamentPlanner.Domain.Entities
+
+public class Match : BaseEntity
 {
-    public class Match: BaseEntity 
-    {
-        public required Player FirstPlayer { get; set; }
-        public required Player SecondPlayer { get; set; }
+    public required Player FirstPlayer { get; set; }
+    public int Player1Id { get; set; }
 
-        public Player? Winner { get; set; }
+    public required Player SecondPlayer { get; set; }
+    public int Player2Id { get; set; }
 
-        public DateTime? GameScheduled { get; set; }
+    public Player? Winner { get; set; }
+    public int? WinnerId { get; set; }
 
-        public DateTime? GamePlayed { get; set; }
+    public required Tournament Tournament { get; set; }
+    public int TournamentId { get; set; }
 
-        public IScore? Score { get; set; }
-        // Added to support match rescheduling
-        public bool IsRescheduled { get; set; }
-        public string? RescheduleReason { get; set; }
-        public Admin? RescheduledBy { get; set; }
+    public required MatchType MatchType { get; set; }
+    public int MatchTypeId { get; set; }
 
-    }
+    public DateTime? GameScheduled { get; set; }
+
+    public DateTime? GamePlayed { get; set; }
+
+    public string? ScoreJson { get; set; }
+    // Added to support match rescheduling
+    public bool IsRescheduled { get; set; }
+    public string? RescheduleReason { get; set; }
+    public Admin? RescheduledBy { get; set; }
+    public int? RescheduledById { get; set; }
+
+    // public required GameFormat<Score> GameFormat { get; set; } // we handle the game format in runtime, no need to save
+
 }
