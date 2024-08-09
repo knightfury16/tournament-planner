bfc12f7837c1bf9c6242770996aa0af68f6780ef
knightfury16 <suhaibknight@gmail.com>
2024-08-09 16:20:50 +0600

    Round depricated

--- TournamentPlanner.Domain/Entities/Round.cs
+++ TournamentPlanner.Domain/Entities/Round.cs
@@ -2,6 +2,8 @@ using TournamentPlanner.Domain.Common;
 
 namespace TournamentPlanner.Domain.Entities
 {
+
+    //@depricated
     public class Round: BaseEntity
     {
         public int RoundNumber { get; set; }
