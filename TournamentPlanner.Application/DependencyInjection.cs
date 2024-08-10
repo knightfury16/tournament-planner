e9c408d13ff0a5d0845e284530ef9c76427bdb00
knightfury16 <suhaibknight@gmail.com>
2024-08-10 16:03:51 +0600

    Refactoring Application Layer

--- TournamentPlanner.Application/DependencyInjection.cs
+++ TournamentPlanner.Application/DependencyInjection.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using TournamentPlanner.Application.UseCases.AddPlayer;
-using TournamentPlanner.Application.UseCases.GenerateUseCase;
-using TournamentPlanner.Application.UseCases.MatchUseCase;
-using TournamentPlanner.Application.UseCases.PlayerUseCase;
-using TournamentPlanner.Application.UseCases.TournamentUseCase;
+
 
 namespace TournamentPlanner.Application
 {
@@ -15,10 +7,7 @@ namespace TournamentPlanner.Application
     {
         public static void AddApplicationServices (this IServiceCollection services){
             //TODO: Make a aggregate use cases
-            services.AddScoped<IPlayerUseCase, PlayerUseCase> ();
-            services.AddScoped<IMatchUseCase, MatchUseCase> ();
-            services.AddScoped<IGenerate, GenerateUseCase> ();
-            services.AddScoped<ITournamentUseCase, TournamentUseCase> ();
+    
         }
         
     }
