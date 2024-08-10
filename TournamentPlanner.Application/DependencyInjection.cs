91bbe7c89feb6233e1d6080ceb2f569806e92b5c
knightfury16 <suhaibknight@gmail.com>
2024-08-10 18:16:15 +0600

    Added getAllPlayerHandler to service container

--- TournamentPlanner.Application/DependencyInjection.cs
+++ TournamentPlanner.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
+using TournamentPlanner.Application.Request;
+using TournamentPlanner.Application.RequestHandler;
+using TournamentPlanner.Domain.Entities;
 using TournamentPlanner.Mediator;
 
 
@@ -6,11 +9,13 @@ namespace TournamentPlanner.Application
 {
     public static class DependencyInjection
     {
-        public static void AddApplicationServices (this IServiceCollection services){
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
             //TODO: Make a aggregate use cases
             services.AddScoped<IMediator, Mediator.Mediator>();
-    
+            services.AddScoped<IRequestHandler<GetAllPlayerRequest, IEnumerable<Player>>, GetAllPlayerRequestHandler>();
+
         }
-        
+
     }
 }
