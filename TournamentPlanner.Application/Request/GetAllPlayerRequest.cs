3ac190460ed4ec438710370bbbd78fde318825eb
knightfury16 <suhaibknight@gmail.com>
2024-08-11 18:11:33 +0600

    GetAllPlayer filter included part 2

--- TournamentPlanner.Application/Request/GetAllPlayerRequest.cs
+++ TournamentPlanner.Application/Request/GetAllPlayerRequest.cs
@@ -6,6 +6,10 @@ namespace TournamentPlanner.Application.Request
     public class GetAllPlayerRequest : IRequest<IEnumerable<Player>>
     {
         public string? name { get; set; }
-        
+
+        public GetAllPlayerRequest(string? name)
+        {
+            this.name = name;
+        }
     }
 }
