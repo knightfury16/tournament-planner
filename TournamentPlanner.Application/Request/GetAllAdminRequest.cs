9d96eded018a821902363bac5c4b93125de79d02
knightfury16 <suhaibknight@gmail.com>
2024-08-14 23:49:02 +0600

    GetAllAdmin request and handler added

---
+++ TournamentPlanner.Application/Request/GetAllAdminRequest.cs
@@ -0,0 +1,14 @@
+﻿using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetAllAdminRequest : IRequest<IEnumerable<AdminDto>>
+{
+    public string? Name { get; set; }
+    public GetAllAdminRequest(string? name)
+    {
+        Name = name;
+    }
+
+}
---
+++ TournamentPlanner.Application/RequestHandler/GetAllAdminRequestHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetAllAdminRequestHandler : IRequestHandler<GetAllAdminRequest, IEnumerable<AdminDto>>
+{
+
+    private readonly IRepository<Admin, Admin> _adminRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllAdminRequestHandler(IRepository<Admin, Admin> adminRepository, IMapper mapper)
+    {
+        _adminRepository = adminRepository;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<AdminDto>?> Handle(GetAllAdminRequest request, CancellationToken cancellationToken1 = default)
+    {
+        IEnumerable<Admin> admins;
+        if (request.Name != null)
+        {
+            admins = await _adminRepository.GetAllAsync(admin => admin.Name.ToLowerInvariant().Contains(request.Name));
+        }
+        else
+        {
+            admins = await _adminRepository.GetAllAsync();
+        }
+
+        var adminsDtos = _mapper.Map<IEnumerable<AdminDto>>(admins);
+
+        return adminsDtos;
+    }
+}
+
+
