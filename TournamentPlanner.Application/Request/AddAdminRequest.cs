6116770186f481c2ab146ad969298d1966143b6d
knightfury16 <suhaibknight@gmail.com>
2024-08-14 23:48:34 +0600

    Add admin request and handler added

---
+++ TournamentPlanner.Application/Request/AddAdminRequest.cs
@@ -0,0 +1,15 @@
+﻿using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class AddAdminRequest : IRequest<AdminDto>
+{
+    public AddAdminDto AddAdminDto { get; set; }
+
+    public AddAdminRequest(AddAdminDto addAdminDto)
+    {
+        AddAdminDto = addAdminDto;
+    }
+
+}
---
+++ TournamentPlanner.Application/RequestHandler/AddAdminRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class AddAdminRequestHandler : IRequestHandler<AddAdminRequest, AdminDto>
+{
+    public IRepository<Admin, Admin> _adminRepository { get; set; }
+    public IMapper _mapper { get; set; }
+
+    public AddAdminRequestHandler(IMapper mapper, IRepository<Admin, Admin> adminRepository)
+    {
+        _mapper = mapper;
+        _adminRepository = adminRepository;
+    }
+    public async Task<AdminDto?> Handle(AddAdminRequest request, CancellationToken cancellationToken1 = default)
+    {
+        var admin = _mapper.Map<Admin>(request.AddAdminDto);
+        await _adminRepository.AddAsync(admin);
+        await _adminRepository.SaveAsync();
+
+        return _mapper.Map<AdminDto>(admin);
+    }
+}
