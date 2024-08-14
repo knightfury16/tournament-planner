16a1a0c0327c3ce7f5cc2570118711a3a7aebb82
knightfury16 <suhaibknight@gmail.com>
2024-08-14 23:49:24 +0600

    GetAdminById request and handler added

---
+++ TournamentPlanner.Application/Request/GetAdminByIdRequest.cs
@@ -0,0 +1,15 @@
+﻿using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetAdminByIdRequest : IRequest<AdminDto>
+{
+
+    public readonly int id;
+
+    public GetAdminByIdRequest(int id)
+    {
+        this.id = id;
+    }
+}
---
+++ TournamentPlanner.Application/RequestHandler/GetAdminByIdRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetAdminByIdRequestHandler: IRequestHandler<GetAdminByIdRequest, AdminDto>
+{
+
+    private readonly IRepository<Admin, Admin> _adminRepository;
+    private readonly IMapper _mapper;
+
+    public GetAdminByIdRequestHandler(IRepository<Admin, Admin> adminRepository, IMapper mapper)
+    {
+        _adminRepository = adminRepository;
+        _mapper = mapper;
+    }
+    public async Task<AdminDto?> Handle(GetAdminByIdRequest request, CancellationToken cancellationToken1 = default)
+    {
+
+        var admin = await _adminRepository.GetAllAsync(a => a.Id == request.id, ["CreatedTournament"]);
+
+        if (admin == null) return null;
+
+        return _mapper.Map<AdminDto>(admin.First());
+
+    }
+}
