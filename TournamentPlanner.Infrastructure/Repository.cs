bc56f70b9bb0168f22b4306b1a8249d5551e57cd
knightfury16 <suhaibknight@gmail.com>
2024-08-22 15:13:40 +0600

    Refactored the way we were fetching the property via include in EF

--- TournamentPlanner.Infrastructure/Repository.cs
+++ TournamentPlanner.Infrastructure/Repository.cs
@@ -55,13 +55,7 @@ namespace TournamentPlanner.Infrastructure
 
             foreach (var property in includeProperties)
             {
-                var propertyInfo = typeof(T).GetProperty(property);
-                if (propertyInfo != null)
-                {
-                    Expression<Func<T, object>> lambdaExpression = BuildTheIncludeExpression(property);
-
-                    query = query.Include(lambdaExpression);
-                }
+                query = query.Include(property);
             }
             return await query.ToListAsync();
         }
@@ -74,18 +68,17 @@ namespace TournamentPlanner.Infrastructure
             {
                 foreach (var property in includeProperties)
                 {
-                    Expression<Func<T, object>> includeLambdaExpression = BuildTheIncludeExpression(property);
-
-                    query = query.Include(includeLambdaExpression);
+                    query = query.Include(property);
                 }
+
             }
 
             if (filter != null)
             {
                 query = query.Where(filter);
             }
-
             return await query.ToListAsync();
+
         }
 
         public async Task<IEnumerable<T>> GetAllAsync(IEnumerable<Expression<Func<T, bool>>> filters, string[] includeProperties)
@@ -96,9 +89,7 @@ namespace TournamentPlanner.Infrastructure
             {
                 foreach (var property in includeProperties)
                 {
-                    Expression<Func<T, object>> lambdaExpression = BuildTheIncludeExpression(property);
-
-                    query = query.Include(lambdaExpression);
+                    query = query.Include(property);
                 }
             }
 
@@ -135,63 +126,9 @@ namespace TournamentPlanner.Infrastructure
             return await _dataContext.Set<T>().FindAsync(id);
         }
 
-        public async Task<IEnumerable<T>> GetByNameAsync(string? name)
+        public Task<IEnumerable<T>> GetByNameAsync(string? name)
         {
-            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
-            {
-                return await GetAllAsync();
-            }
-
-            Expression<Func<T, bool>> lamdaPredicateExpression = BuildNameMatchingExpression(name);
-
-            var query = _dataContext.Set<T>().Where(lamdaPredicateExpression);
-
-            return await query.ToListAsync();
-        }
-
-        public static Expression<Func<T, bool>> BuildNameMatchingExpression(string name)
-        {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentException("Name cannot be null or empty.");
-            }
-
-            //TODO: Add case insensitivity
-            // var nameLowered = Expression.Call(nameProperty, typeof(string).GetMethod("ToLowerInvariant", BindingFlags.Static | BindingFlags.Public)); // Safer method call
-            var parameter = Expression.Parameter(typeof(T), "x");
-            var nameProperty = Expression.Property(parameter, "Name");
-
-            if (nameProperty == null)
-            {
-                throw new ArgumentException("No Name property to filter by on this object");
-            }
-
-            // Use Contains method (assuming Name is a string)
-            var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) })!; // a string will always have Contains method
-            var nameConstant = Expression.Constant(name); // No case-insensitive conversion
-
-            var containsCall = Expression.Call(nameProperty, containsMethod, nameConstant);
-
-            // var comparison = Expression.Equal(nameProperty, nameConstant);
-
-            return Expression.Lambda<Func<T, bool>>(containsCall, parameter);
-        }
-
-        //Naive I am
-        private bool MatchNameProperty(T e, string name)
-        {
-            var nameProperty = e.GetType().GetProperty("Name");
-
-            if (nameProperty != null)
-            {
-
-                var nameValue = nameProperty.GetValue(e)?.ToString();
-                if (nameValue != null)
-                {
-                    return nameValue.ToLower().Equals(name.ToLower());
-                }
-            }
-            return false;
+            throw new NotImplementedException();
         }
 
         public async Task SaveAsync()
@@ -224,31 +161,5 @@ namespace TournamentPlanner.Infrastructure
             throw new NotImplementedException();
         }
 
-        private static Expression<Func<T, object>> BuildTheIncludeExpression(string property)
-        {
-            // Construct an expression for the include property
-            // it select parameter as x of Object Player
-            var parameter = Expression.Parameter(typeof(T), "x"); // x here is just a random name
-
-            // Split the property string by dots to handle nested properties
-            var propertyNames = property.Split('.');
-
-            // for example x => x.Tournament
-            //propertyExpression type Tournament
-            //var propertyExpression = Expression.Property(parameter, property);
-
-            Expression propertyExpression = parameter;
-
-            // Iterate through each property name to build the nested property expression
-            foreach (var propertyName in propertyNames)
-            {
-                propertyExpression = Expression.Property(propertyExpression, propertyName);
-            }
-
-
-            //TODO:: I dont understand this, need no know more about Expression
-            Expression<Func<T, object>> lambdaExpression = Expression.Lambda<Func<T, object>>(propertyExpression, parameter);
-            return lambdaExpression;
-        }
     }
 }
