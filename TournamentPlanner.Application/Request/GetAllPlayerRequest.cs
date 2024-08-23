98006b872bd6b45c1a5719e13eb0344be7bba5e5
knightfury16 <suhaibknight@gmail.com>
2024-08-23 21:47:05 +0600

    GetAllPlayerRequest setter set convert the name to lower.

--- TournamentPlanner.Application/Request/GetAllPlayerRequest.cs
+++ TournamentPlanner.Application/Request/GetAllPlayerRequest.cs
@@ -6,7 +6,11 @@ namespace TournamentPlanner.Application.Request
 {
     public class GetAllPlayerRequest : IRequest<IEnumerable<PlayerDto>>
     {
-        public string? Name { get; set; }
+        private string? _name;
+        public string? Name { 
+            get {return _name;}
+            set { _name = value?.ToLower(); }
+         }
 
         public GetAllPlayerRequest(string? name)
         {
