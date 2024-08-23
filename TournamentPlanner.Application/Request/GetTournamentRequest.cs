648827cd2a5f023ed956bc0e5899861b8f2ae20e
knightfury16 <suhaibknight@gmail.com>
2024-08-23 21:48:29 +0600

    get tournament request created

---
+++ TournamentPlanner.Application/Request/GetTournamentRequest.cs
@@ -0,0 +1,24 @@
+﻿using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Application.Enums;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Enum;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetTournamentRequest : IRequest<IEnumerable<TournamentDto>>
+{
+    private string? _name;
+
+    public string? Name
+    {
+        get => _name;
+        set => _name = value?.ToLower();
+    }
+    public TournamentSearchCategory SearchCategory { get; set; } = TournamentSearchCategory.ThisWeek;
+    public TournamentStatus? Status { get; set; }
+    public GameTypeSupported? GameTypeSupported { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+
+}
