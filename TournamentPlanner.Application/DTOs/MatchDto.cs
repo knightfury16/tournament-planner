85a01e3d4f9c6e7f491ef23facbc98f314f981bd
knightfury16 <suhaibknight@gmail.com>
2024-08-31 22:46:40 +0600

    matchDto added

---
+++ TournamentPlanner.Application/DTOs/MatchDto.cs
@@ -0,0 +1,20 @@
+namespace TournamentPlanner.Application.DTOs;
+public class MatchDto
+{
+    public required PlayerDto FirstPlayer { get; set; }
+
+    public required PlayerDto SecondPlayer { get; set; }
+
+    public PlayerDto? Winner { get; set; }
+    public int MatchTypeId { get; set; }
+
+    public DateTime? GameScheduled { get; set; }
+
+    public DateTime? GamePlayed { get; set; }
+
+    public string? ScoreJson { get; set; }
+    // Added to support match rescheduling
+    public bool IsRescheduled { get; set; }
+    public string? RescheduleReason { get; set; }
+
+}
