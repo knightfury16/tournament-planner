e708fdec9f34c7c6c03bd0ed58a022692c7458ef
knightfury16 <suhaibknight@gmail.com>
2024-09-04 22:58:14 +0600

    get tournament match types request and handler added

---
+++ TournamentPlanner.Application/Request/GetTournamentMatchTypesRequest.cs
@@ -0,0 +1,13 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetTournamentMatchTypesRequest : IRequest<IEnumerable<MatchTypeDto>>
+{
+    public int Id { get; set; }
+    public GetTournamentMatchTypesRequest(int Id)
+    {
+        this.Id = Id;
+    }
+
+}
---
+++ TournamentPlanner.Application/RequestHandler/GetTournamentMatchesTypesRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetTournamentMatchesTypesRequestHandler : IRequestHandler<GetTournamentMatchTypesRequest, IEnumerable<MatchTypeDto>>
+{
+    private readonly IRepository<Tournament> _tournamentRepository;
+    private readonly IMapper _mapper;
+
+    public GetTournamentMatchesTypesRequestHandler(IRepository<Tournament> tournamentRepository, IMapper mapper)
+    {
+        _tournamentRepository = tournamentRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<MatchTypeDto>?> Handle(GetTournamentMatchTypesRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+
+        var matchTypes = (await _tournamentRepository.GetAllAsync(t => t.Id == request.Id, [nameof(Tournament.MatchTypes)]))
+                    .SelectMany(t => t.MatchTypes);
+
+        return _mapper.Map<IEnumerable<MatchTypeDto>>(matchTypes);
+
+    }
+}
