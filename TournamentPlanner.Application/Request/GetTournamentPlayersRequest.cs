b3a5daaadbf7059dad598753336ff57c889c79b4
knightfury16 <suhaibknight@gmail.com>
2024-09-04 22:02:42 +0600

    get tournament players request and handler added

---
+++ TournamentPlanner.Application/Request/GetTournamentPlayersRequest.cs
@@ -0,0 +1,14 @@
+﻿using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetTournamentPlayersRequest : IRequest<IEnumerable<PlayerDto>>
+{
+    public int Id { get; set; }
+    public GetTournamentPlayersRequest(int Id)
+    {
+        this.Id = Id;
+    }
+
+}
---
+++ TournamentPlanner.Application/RequestHandler/GetTournamentPlayersRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetTournamentPlayersRequestHandler : IRequestHandler<GetTournamentPlayersRequest, IEnumerable<PlayerDto>>
+{
+    private readonly IRepository<Tournament> _tournamentRepository;
+    private readonly IMapper _mapper;
+
+    public GetTournamentPlayersRequestHandler(IRepository<Tournament> tournamentRepository, IMapper mapper)
+    {
+        _tournamentRepository = tournamentRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<PlayerDto>?> Handle(GetTournamentPlayersRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+
+        var players = (await _tournamentRepository.GetAllAsync(t => t.Id == request.Id, [nameof(Tournament.Participants)]))
+                    .SelectMany(t => t.Participants);
+
+        return _mapper.Map<IEnumerable<PlayerDto>>(players);
+
+    }
+}
