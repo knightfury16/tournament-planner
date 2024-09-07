20bf5a19a7b7b8ebd812924244f697dcef9bea26
knightfury16 <suhaibknight@gmail.com>
2024-09-07 20:28:30 +0600

    Added round property to match type

--- TournamentPlanner.Domain/Entities/KnockOut.cs
+++ TournamentPlanner.Domain/Entities/KnockOut.cs
@@ -2,5 +2,4 @@ namespace TournamentPlanner.Domain.Entities;
 
 public class KnockOut : MatchType
 {
-    public int Round { get; set; }
 }
--- TournamentPlanner.Domain/Entities/MatchType.cs
+++ TournamentPlanner.Domain/Entities/MatchType.cs
@@ -5,6 +5,7 @@ using TournamentPlanner.Domain.Common;
 public abstract class MatchType : BaseEntity
 {
     public required string Name { get; set; } //group name, eg. GroupA or Elimination1
+    public int Round { get; set; }
     public List<Player> Players { get; set; } = new List<Player>();
     public List<Match> Matches { get; set; } = new();
     public required Tournament Tournament { get; set; }
