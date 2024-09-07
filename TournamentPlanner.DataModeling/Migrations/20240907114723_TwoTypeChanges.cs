b17d3946ac15a2df6e528ffa13ffd7f0009f3455
knightfury16 <suhaibknight@gmail.com>
2024-09-07 20:40:07 +0600

    Changing jsonScore to object and gametype name to enum migration

---
+++ TournamentPlanner.DataModeling/Migrations/20240907114723_TwoTypeChanges.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TournamentPlanner.DataModeling.Migrations
+{
+    /// <inheritdoc />
+    public partial class TwoTypeChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 9, 7, 11, 47, 23, 558, DateTimeKind.Utc).AddTicks(7163), new DateTime(2024, 9, 7, 11, 47, 23, 558, DateTimeKind.Utc).AddTicks(7165) });
+
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 9, 7, 11, 47, 23, 558, DateTimeKind.Utc).AddTicks(7170), new DateTime(2024, 9, 7, 11, 47, 23, 558, DateTimeKind.Utc).AddTicks(7170) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 8, 13, 11, 46, 31, 383, DateTimeKind.Utc).AddTicks(6314), new DateTime(2024, 8, 13, 11, 46, 31, 383, DateTimeKind.Utc).AddTicks(6316) });
+
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 8, 13, 11, 46, 31, 383, DateTimeKind.Utc).AddTicks(6324), new DateTime(2024, 8, 13, 11, 46, 31, 383, DateTimeKind.Utc).AddTicks(6324) });
+        }
+    }
+}
