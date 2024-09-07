7662522c77ac2431307ab915fe21f8fe96f0a2b4
knightfury16 <suhaibknight@gmail.com>
2024-09-07 20:29:30 +0600

    Made GameType Name to GameTypeSupported enum

--- TournamentPlanner.Domain/Entities/GameType.cs
+++ TournamentPlanner.Domain/Entities/GameType.cs
@@ -1,13 +1,13 @@
 namespace TournamentPlanner.Domain.Entities;
 
 using TournamentPlanner.Domain.Common;
+using TournamentPlanner.Domain.Enum;
 
 //Figure out GameFormat which will be predefined, from GameType
 // Changed from abstract class to regular class to support the InterestedGameTypes property in User
 public class GameType : BaseEntity
 {
-    //TODO: Make a enum of the supported GameType
-    public required string Name { get; set; }
+    public required GameTypeSupported Name { get; set; }
     public List<Tournament>? Tournaments { get; set; }
 }
 
