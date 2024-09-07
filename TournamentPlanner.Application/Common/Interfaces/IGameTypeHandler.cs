fe70d41e2fc403c2108107375032c8e5965c9554
knightfury16 <suhaibknight@gmail.com>
2024-09-07 20:44:42 +0600

    Refactoring GameTypeHandler and Factory

--- TournamentPlanner.Application/Common/Interfaces/IGameTypeHandler.cs
+++
@@ -1,9 +0,0 @@
-using TournamentPlanner.Application.GameTypeHandler;
-
-namespace TournamentPlanner.Application.Common.Interfaces;
-public interface IGameTypeHandler
-{
-    bool ValidateGameSpecificData(string gameSpecificScore);
-    IScore DeserializeScore(string scoreData);
-    string SerializeScore(IScore score);
-}
