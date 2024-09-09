d56db50f84774a1f79cf56468639391a97905f57
knightfury16 <suhaibknight@gmail.com>
2024-09-09 22:44:44 +0600

    added ICreateMatchType interface

---
+++ TournamentPlanner.Application/Common/Interfaces/ICreateMatchType.cs
@@ -0,0 +1,9 @@
+using TournamentPlanner.Domain.Entities;
+using MatchType = TournamentPlanner.Domain.Entities.MatchType;
+
+namespace TournamentPlanner.Application.Common.Interfaces;
+
+public interface ICreateMatchType
+{
+    public Task<IEnumerable<MatchType>?> CreateMatchType(Tournament tournament, string? prefix = "Untitled");
+}
