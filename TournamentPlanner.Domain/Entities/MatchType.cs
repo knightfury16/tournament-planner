9fcf8db73308ae43bb11320e0cde545e5c40131f
knightfury16 <suhaibknight@gmail.com>
2024-09-10 22:28:19 +0600

    Todo in Match Entity

--- TournamentPlanner.Domain/Entities/MatchType.cs
+++ TournamentPlanner.Domain/Entities/MatchType.cs
@@ -5,6 +5,7 @@ using TournamentPlanner.Domain.Common;
 public abstract class MatchType : BaseEntity
 {
     public required string Name { get; set; } //group name, eg. GroupA or Elimination1
+    //Todo: need to make this a Round class which will hold the name and matches
     public int Round { get; set; }
     public List<Player> Players { get; set; } = new List<Player>();
     public List<Match> Matches { get; set; } = new();
