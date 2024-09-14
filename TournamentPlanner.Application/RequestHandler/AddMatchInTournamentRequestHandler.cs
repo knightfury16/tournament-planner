015b0ea62ba6892bd5d2d80cf586fd8b55fc18d9
knightfury16 <suhaibknight@gmail.com>
2024-09-14 22:40:52 +0600

    Add Match in Tournament handler on maintenance

--- TournamentPlanner.Application/RequestHandler/AddMatchInTournamentRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/AddMatchInTournamentRequestHandler.cs
@@ -14,77 +14,80 @@ using MatchType = TournamentPlanner.Domain.Entities.MatchType;
 namespace TournamentPlanner.Application;
 
 //TODO: refactor this handler
-public class AddMatchInTournamentRequestHandler : IRequestHandler<AddMatchInTournamentRequest, MatchDto>
-{
-    private readonly IRepository<Tournament> _tournamentRepository;
-    private readonly IGameFormatFactory gameFormatFactory;
-    private readonly IMapper _mapper;
-    private readonly IRepository<MatchType> _matchTypeRepository;
+//!! I Can not add match in a tournament. I can only update the score of a match. Match will be
+//!! created automatically
+// !! Lets comment out this handler for now
+// public class AddMatchInTournamentRequestHandler : IRequestHandler<AddMatchInTournamentRequest, MatchDto>
+// {
+//     private readonly IRepository<Tournament> _tournamentRepository;
+//     private readonly IGameFormatFactory gameFormatFactory;
+//     private readonly IMapper _mapper;
+//     private readonly IRepository<MatchType> _matchTypeRepository;
 
-    public AddMatchInTournamentRequestHandler(IRepository<Tournament> tournamentRepository, IGameFormatFactory gameTypeFactory
-    , IMapper mapper, IRepository<MatchType> matchTypeRepository)
-    {
-        this._matchTypeRepository = matchTypeRepository;
-        this._tournamentRepository = tournamentRepository;
-        this.gameFormatFactory = gameTypeFactory;
-        this._mapper = mapper;
-    }
-    public async Task<MatchDto?> Handle(AddMatchInTournamentRequest request, CancellationToken cancellationToken = default)
-    {
-        //does the tournament of the given id exists
-        var tournament = (await _tournamentRepository
-                         .GetAllAsync(t => t.Id == request.TournamentId, [nameof(Tournament.GameType), nameof(Tournament.Participants)]))
-                         .FirstOrDefault();
-        if (tournament == null)
-        {
-            throw new InvalidOperationException("Tournament not found");
-        }
-        //are the players of the match part of this tournament
-        var player1 = tournament.Participants.Find(p => p.Id == request.AddMatchDto.Player1Id);
-        var player2 = tournament.Participants.Find(p => p.Id == request.AddMatchDto.Player2Id);
-        if (player1 == null || player2 == null)
-        {
-            throw new InvalidOperationException("One or both player are not registered to this tournament");
-        }
+//     public AddMatchInTournamentRequestHandler(IRepository<Tournament> tournamentRepository, IGameFormatFactory gameTypeFactory
+//     , IMapper mapper, IRepository<MatchType> matchTypeRepository)
+//     {
+//         this._matchTypeRepository = matchTypeRepository;
+//         this._tournamentRepository = tournamentRepository;
+//         this.gameFormatFactory = gameTypeFactory;
+//         this._mapper = mapper;
+//     }
+//     public async Task<MatchDto?> Handle(AddMatchInTournamentRequest request, CancellationToken cancellationToken = default)
+//     {
+//         //does the tournament of the given id exists
+//         var tournament = (await _tournamentRepository
+//                          .GetAllAsync(t => t.Id == request.TournamentId, [nameof(Tournament.GameType), nameof(Tournament.Participants)]))
+//                          .FirstOrDefault();
+//         if (tournament == null)
+//         {
+//             throw new InvalidOperationException("Tournament not found");
+//         }
+//         //are the players of the match part of this tournament
+//         var player1 = tournament.Participants.Find(p => p.Id == request.AddMatchDto.Player1Id);
+//         var player2 = tournament.Participants.Find(p => p.Id == request.AddMatchDto.Player2Id);
+//         if (player1 == null || player2 == null)
+//         {
+//             throw new InvalidOperationException("One or both player are not registered to this tournament");
+//         }
 
-        //get the gametype handler from the factory
-        var gameTypeHandler = gameFormatFactory.GetGameFormat(tournament.GameType.Name);
+//         //get the gametype handler from the factory
+//         var gameTypeHandler = gameFormatFactory.GetGameFormat(tournament.GameType.Name);
 
-        if (gameTypeHandler == null)
-        {
-            throw new InvalidOperationException("Game type hanlder could not be resolved");
-        }
-        //use the handler to validate the score
-        IScore gameScore = gameTypeHandler.DeserializeScore(request.AddMatchDto.GameSpecificScore);
-        if (!gameTypeHandler.IsValidScore(gameScore))
-        {
-            throw new ValidationException("Invalid game-specific data for the given game type.");
-        }
-        //add the match to the db
-        MatchType? matchType = await _matchTypeRepository.GetByIdAsync(1);
-        if (matchType == null)
-        {
-            throw new InvalidOperationException("Could not find match Type");
-        }
+//         if (gameTypeHandler == null)
+//         {
+//             throw new InvalidOperationException("Game type hanlder could not be resolved");
+//         }
+//         //use the handler to validate the score
+//         IScore gameScore = gameTypeHandler.DeserializeScore(request.AddMatchDto.GameSpecificScore);
+//         if (!gameTypeHandler.IsValidScore(gameScore))
+//         {
+//             throw new ValidationException("Invalid game-specific data for the given game type.");
+//         }
+//         //add the match to the db
+//         MatchType? matchType = await _matchTypeRepository.GetByIdAsync(1);
+//         if (matchType == null)
+//         {
+//             throw new InvalidOperationException("Could not find match Type");
+//         }
 
-        var winner = gameTypeHandler.DetermineWinner(player1, player2, gameScore);
+//         var winner = gameTypeHandler.DetermineWinner(player1, player2, gameScore);
 
-        var match = new Match
-        {
-            FirstPlayer = player1,
-            SecondPlayer = player2,
-            Tournament = tournament,
-            MatchType = matchType,
-            ScoreJson = gameScore,
-            GamePlayed = DateTime.UtcNow,
-            Winner = winner
-        };
+//         var match = new Match
+//         {
+//             FirstPlayer = player1,
+//             SecondPlayer = player2,
+//             Tournament = tournament,
+//             MatchType = matchType,
+//             ScoreJson = gameScore,
+//             GamePlayed = DateTime.UtcNow,
+//             Winner = winner
+//         };
 
-        tournament.Matches.Add(match);
-        //save the db context
-        await _tournamentRepository.SaveAsync(); 
-        //map the match and return
-        return _mapper.Map<MatchDto>(match);
-    }
+//         tournament.Matches.Add(match);
+//         //save the db context
+//         await _tournamentRepository.SaveAsync(); 
+//         //map the match and return
+//         return _mapper.Map<MatchDto>(match);
+//     }
 
-}
+// }
