d49995f0ac083ac02917c2f0cf98c4bf68b74eb8
knightfury16 <suhaibknight@gmail.com>
2024-09-15 23:44:31 +0600

    Added Draw service

---
+++ TournamentPlanner.Application/Services/DrawService.cs
@@ -0,0 +1,30 @@
+﻿using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Application;
+
+public interface IDrawService
+{
+    public Task<bool> IsTheDrawComplete(Tournament tournament);
+}
+public class DrawService : IDrawService
+{
+    private readonly IRepository<Draw> _drawRepository;
+
+    public DrawService(IRepository<Draw> drawRepository)
+    {
+        _drawRepository = drawRepository;
+    }
+
+    public async Task<bool> IsTheDrawComplete(Tournament tournament)
+    {
+        var draws = await _drawRepository.GetAllAsync(d => d.TournamentId == tournament.Id, [nameof(Draw.MatchType)]);
+
+        foreach (var draw in draws)
+        {
+            if (draw.MatchType.IsCompleted == false) return false;
+        }
+        return true;
+    }
+
+}
