d1474a40bc2bf758dfdf81786f148c4986c1db7d
knightfury16 <suhaibknight@gmail.com>
2024-09-15 23:46:05 +0600

    Create Tournament Draw request and handler  work in progress

---
+++ TournamentPlanner.Application/Request/CreateTournamentDrawRequest.cs
@@ -0,0 +1,17 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class CreateTournamentDrawRequest : IRequest<IEnumerable<DrawDto>>
+{
+    public int TournamentId { get; set; }
+    public List<int>? SeedersId { get; set; }// Seeders players Id
+    public string? MatchTypePrefix { get; set; } = null;
+    public CreateTournamentDrawRequest(int tournamentId, List<int>? seeders = null, string? matchTypePrefix = null)
+    {
+        TournamentId = tournamentId;
+        SeedersId = seeders;
+        MatchTypePrefix = matchTypePrefix;
+    }
+
+}
---
+++ TournamentPlanner.Application/RequestHandler/CreateTournamentDrawRequestHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class CreateTournamentDrawRequestHandler : IRequestHandler<CreateTournamentDrawRequest, IEnumerable<DrawDto>>
+{
+    private readonly IRepository<Tournament> _tournamentRepository;
+    private readonly IMapper _mapper;
+    private readonly ITournamentService _tournamentService;
+
+    public CreateTournamentDrawRequestHandler(IRepository<Tournament> tournamentRepository, IMapper mapper, ITournamentService tournamentService)
+    {
+        _tournamentRepository = tournamentRepository;
+        _mapper = mapper;
+        _tournamentService = tournamentService;
+    }
+
+
+    public async Task<IEnumerable<DrawDto>?> Handle(CreateTournamentDrawRequest request, CancellationToken cancellationToken = default)
+    {
+        //does the tournament exists?
+        var tournament = (await _tournamentRepository.GetAllAsync(t => t.Id == request.TournamentId, [nameof(Tournament.Participants),
+                            nameof(Tournament.Draw)]))
+                            .FirstOrDefault();
+
+        if (tournament == null)
+        {
+            throw new Exception($"Could not find Tournament with Id {request.TournamentId}.");
+        }
+        //can i make draw? 
+        var canIDarw = await _tournamentService.CanIMakeDraw(tournament);
+
+        if (!canIDarw) throw new Exception("Previous draws are not completed.Can not make new draw");
+        //make draw -> make matchType
+        var draws = await _tournamentService.MakeDraws(tournament, request.MatchTypePrefix, request.SeedersId);
+
+        return _mapper.Map<IEnumerable<DrawDto>>(draws);
+    }
+}
