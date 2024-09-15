122bb224870aeee54902b8ad3950106846ec450d
knightfury16 <suhaibknight@gmail.com>
2024-09-15 23:44:07 +0600

    Added matchTypeService

---
+++ TournamentPlanner.Application/Services/MatchTypeService.cs
@@ -0,0 +1,27 @@
+﻿using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Enum;
+using MatchType = TournamentPlanner.Domain.Entities.MatchType;
+
+namespace TournamentPlanner.Application;
+public interface IMatchTypeService
+{
+    public Task<IEnumerable<MatchType>> CreateMatchType(Tournament tournament, string? matchTypePrefix = null, List<int>? seeders = null);
+}
+public class MatchTypeService : IMatchTypeService
+{
+    private readonly ICreateMatchTypeFactory createMatchTypeFactory;
+
+    public MatchTypeService(ICreateMatchTypeFactory createMatchTypeFactory)
+    {
+        this.createMatchTypeFactory = createMatchTypeFactory;
+    }
+
+    public async Task<IEnumerable<MatchType>> CreateMatchType(Tournament tournament, string? matchTypePrefix = null, List<int>? seeders = null)
+    {
+        var matchTypeCreator = createMatchTypeFactory.GetMatchTypeCreator(tournament.TournamentType ?? TournamentType.GroupStage);
+        var matchTypes = await matchTypeCreator.CreateMatchType(tournament, matchTypePrefix, seeders);
+        if (matchTypes == null) throw new Exception("Could not make match types from tournament service");
+        return matchTypes;
+    }
+}
