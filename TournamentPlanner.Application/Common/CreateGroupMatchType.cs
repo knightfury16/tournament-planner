782618ef87a11487d42b2df0dc7d44f70d74641f
knightfury16 <suhaibknight@gmail.com>
2024-09-17 23:02:15 +0600

    Added custom player sorter

--- TournamentPlanner.Application/Common/CreateGroupMatchType.cs
+++ TournamentPlanner.Application/Common/CreateGroupMatchType.cs
@@ -14,8 +14,8 @@ public class CreateGroupMatchType : ICreateMatchType
         prefix ??= "Group";
         var numberOfGroup = DetermineNumberOfGroup(tournament.Participants.Count);
         List<MatchType> groups = GenerateGroups(tournament, numberOfGroup, prefix);
-        var distributedPlayers = DetermineDistributedPlayers(tournament.Participants);
-        DistributePlayersAmongGroups(distributedPlayers, ref groups);
+        var sortedPlayers = GetSortedPlayers(tournament.Participants, seederPlayerIds);
+        DistributePlayersAmongGroups(sortedPlayers, ref groups);
         return Task.FromResult((IEnumerable<MatchType>?)groups);
     }
 
@@ -42,11 +42,49 @@ public class CreateGroupMatchType : ICreateMatchType
 
     }
 
-    private List<Player> DetermineDistributedPlayers(List<Player> participants)
+    private List<Player> GetSortedPlayers(List<Player> participants, List<int>? seederPlayerIds = null)
     {
+        //getting a list of player by seeder, power, then by randomly to distribute similar level player evenly among groups
         Random random = new Random();
-        //getting a list of player by power, then by randomly to distribute similar level player evenly among groups
-        return participants.OrderBy(p => p.WinRatio).ThenBy(p => random.Next()).Reverse().ToList();
+
+        //*custom sorting for learning purpose
+        if (seederPlayerIds != null)
+        {
+            participants.Sort((leftPlayer, rightPlayer) =>
+           {
+               //First level of sorting: Seeder Player
+               bool isLeftPlayerSeeder = seederPlayerIds.Contains(leftPlayer.Id);
+               bool isRightPlayerSeeder = seederPlayerIds.Contains(rightPlayer.Id);
+
+               if (isLeftPlayerSeeder && !isRightPlayerSeeder) return -1;
+               if (!isLeftPlayerSeeder && isRightPlayerSeeder) return 1;
+
+               //Second level of sorting: Win Ratio
+               int winRatioComparison = leftPlayer.WinRatio.CompareTo(rightPlayer.WinRatio);
+               if (winRatioComparison != 0) return winRatioComparison * -1; //coz i need it in descending order
+
+               //Third level of sorting: Random
+               return random.Next().CompareTo(random.Next());
+
+           });
+
+        }
+        else
+        {
+
+            participants.Sort((leftPlayer, rightPlayer) =>
+            {
+                //First level of sorting: Win Ratio
+                int winRatioComparison = leftPlayer.WinRatio.CompareTo(rightPlayer.WinRatio);
+                if (winRatioComparison != 0) return winRatioComparison * -1; //coz i need it in descending order
+
+                //Second level of sorting: Random
+                return random.Next().CompareTo(random.Next());
+            });
+        }
+        return participants;
+        //*Previous implementation
+        // return participants.OrderBy(p => p.WinRatio).ThenBy(p => random.Next()).Reverse().ToList();
     }
 
     private List<MatchType> GenerateGroups(Tournament tournament, int numberOfGroup, string? prefix)
