c797c5c6902cecc8c2157ee99fd4e05e579c9bd5
knightfury16 <suhaibknight@gmail.com>
2024-09-21 16:19:43 +0600

    Tournament Property draw pluralization and migration

---
+++ TournamentPlanner.DataModeling/Migrations/20240921101304_TournamentDrawsPlural.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TournamentPlanner.DataModeling.Migrations
+{
+    /// <inheritdoc />
+    public partial class TournamentDrawsPlural : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 9, 21, 10, 13, 4, 221, DateTimeKind.Utc).AddTicks(1147), new DateTime(2024, 9, 21, 10, 13, 4, 221, DateTimeKind.Utc).AddTicks(1149) });
+
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 9, 21, 10, 13, 4, 221, DateTimeKind.Utc).AddTicks(1154), new DateTime(2024, 9, 21, 10, 13, 4, 221, DateTimeKind.Utc).AddTicks(1155) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 9, 13, 12, 38, 39, 345, DateTimeKind.Utc).AddTicks(9569), new DateTime(2024, 9, 13, 12, 38, 39, 345, DateTimeKind.Utc).AddTicks(9571) });
+
+            migrationBuilder.UpdateData(
+                table: "GameTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 9, 13, 12, 38, 39, 345, DateTimeKind.Utc).AddTicks(9576), new DateTime(2024, 9, 13, 12, 38, 39, 345, DateTimeKind.Utc).AddTicks(9576) });
+        }
+    }
+}
