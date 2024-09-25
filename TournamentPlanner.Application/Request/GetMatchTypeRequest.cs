e9a29e939cbe6fb09416df5223b4c8f8292a7c49
knightfury16 <suhaibknight@gmail.com>
2024-09-25 21:54:57 +0600

    GetMatchType request and handler added

---
+++ TournamentPlanner.Application/Request/GetMatchTypeRequest.cs
@@ -0,0 +1,13 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetMatchTypeRequest : IRequest<MatchTypeDto>
+{
+    public int MatchTypeId { get; }
+
+    public GetMatchTypeRequest(int matchTypeId)
+    {
+        MatchTypeId = matchTypeId;
+    }
+}
---
+++ TournamentPlanner.Application/RequestHandler/GetMatchTypeRequestHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.Helpers;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Exceptions;
+using TournamentPlanner.Mediator;
+using MatchType = TournamentPlanner.Domain.Entities.MatchType;
+
+namespace TournamentPlanner.Application;
+
+public class GetMatchTypeRequestHandler : IRequestHandler<GetMatchTypeRequest, MatchTypeDto>
+{
+
+    private readonly IRepository<MatchType> _matchTypeRepository;
+    private readonly IRepository<Round> _roundRepository;
+    private readonly IMapper _mapper;
+
+    public GetMatchTypeRequestHandler(IRepository<MatchType> matchTypeRepository, IMapper mapper, IRepository<Round> roundRepository)
+    {
+        _matchTypeRepository = matchTypeRepository;
+        _mapper = mapper;
+        _roundRepository = roundRepository;
+    }
+
+
+
+    public async Task<MatchTypeDto?> Handle(GetMatchTypeRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+
+        // get the matchtype and populate the round and matches
+        var roundNavigationProop = Utility.NavigationPrpertyCreator(nameof(MatchType.Rounds), nameof(Round.Matches));
+        var matchType = await _matchTypeRepository.GetByIdAsync(request.MatchTypeId, [roundNavigationProop]);
+
+        if (matchType == null) throw new NotFoundException(nameof(matchType));
+
+        return _mapper.Map<MatchTypeDto>(matchType);
+
+
+    }
+}
