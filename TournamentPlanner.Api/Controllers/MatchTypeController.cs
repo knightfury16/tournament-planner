2746dad022fac18971ca5869aa11bd4aaff5c972
knightfury16 <suhaibknight@gmail.com>
2024-09-25 21:55:23 +0600

    Get match type by id controller added

---
+++ TournamentPlanner.Api/Controllers/MatchTypeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using TournamentPlanner.Application;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace MyApp.Namespace
+{
+    [ApiController]
+    [Route("api/match-type")]
+    public class MatchTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MatchTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
+
+        //That is the get the Group-A for example
+        [HttpGet("{matchTypeId}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<MatchTypeDto>))]
+        public async Task<IActionResult> GetMatchType(int matchTypeId)
+        {
+            if (matchTypeId < 0) return BadRequest("Invalid Id");
+
+            var getMatchTypeRequest = new GetMatchTypeRequest(matchTypeId);
+            var matchTypeDto = await _mediator.Send(getMatchTypeRequest);
+
+            return Ok(matchTypeDto);
+        }
+
+    }
+}
