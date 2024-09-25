f9b8da0d1a5460f98e4926de84f75a0310c6f01b
knightfury16 <suhaibknight@gmail.com>
2024-09-25 21:30:40 +0600

    Added AddMatchScoreDto

---
+++ TournamentPlanner.Application/DTOs/AddMatchScoreDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TournamentPlanner.Application;
+
+public class AddMatchScoreDto
+{
+    public DateTime? GamePlayed { get; set; }
+    public required object GameSpecificScore { get; set; }
+    //TODO: can i pass the tournament game type here to double validate the in handler? or will it be redundent?
+
+}
