1ac36383657a35c940a88b1ab8bbb7f6e654416f
knightfury16 <suhaibknight@gmail.com>
2024-09-26 22:54:56 +0600

    Added round service and implemented UpdateRoundCompletion method

---
+++ TournamentPlanner.Application/Services/RoundService.cs
@@ -0,0 +1,46 @@
+﻿using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.Services;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Exceptions;
+
+namespace TournamentPlanner.Application;
+
+public interface IRoundService
+{
+    public Task UpdateRoundCompletion(Round round);
+}
+public class RoundService : IRoundService
+{
+    private readonly IRepository<Round> _roundRepository;
+    private readonly IMatchService _matchService;
+
+    public RoundService(IRepository<Round> roundRepository, IMatchService matchService)
+    {
+        _roundRepository = roundRepository;
+        _matchService = matchService;
+    }
+
+    public async Task UpdateRoundCompletion(Round round)
+    {
+        //get all the matches of the round
+        var roundWithPopulatedMatches = await _roundRepository.GetByIdAsync(round.Id, [nameof(Round.Matches), nameof(Round.MatchType)]);
+
+        if (roundWithPopulatedMatches == null)throw new NotFoundException(nameof(roundWithPopulatedMatches));
+
+        var matches = roundWithPopulatedMatches.Matches;
+
+        foreach (var match in matches)
+        {
+            //if any match is not complete round is not complete
+            if(!_matchService.IsMatchComplete(match)){
+                return;
+            }
+        }
+
+        //if here then all matches are complete\
+        round.IsCompleted = true;
+        await _roundRepository.SaveAsync();
+        //call the update matchtype
+
+    }
+}
