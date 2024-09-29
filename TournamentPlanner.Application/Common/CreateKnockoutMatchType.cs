937260006bff2d0bf92c658681dea95fa07b6336
knightfury16 <suhaibknight@gmail.com>
2024-09-29 18:58:01 +0600

    CreateKnockoutMatchType implemented

--- TournamentPlanner.Application/Common/CreateKnockoutMatchType.cs
+++ TournamentPlanner.Application/Common/CreateKnockoutMatchType.cs
@@ -1,5 +1,6 @@
 using TournamentPlanner.Application.Common.Interfaces;
 using TournamentPlanner.Domain.Entities;
+using MatchType = TournamentPlanner.Domain.Entities.MatchType;
 
 namespace TournamentPlanner.Application.Common;
 
@@ -7,9 +8,19 @@ public class CreateKnockoutMatchType : ICreateMatchType
 {
 
 
-    public Task<IEnumerable<Domain.Entities.MatchType>?> CreateMatchType(Tournament tournament,
+    public Task<IEnumerable<MatchType>?> CreateMatchType(Tournament tournament,
     List<Player> players, string? prefix = null, List<int>? seederPlayerIds = null)
     {
-        throw new NotImplementedException();
+        //im here, and i will be here only once in my tp life. according to Axiom.
+        //there is no work here all the work will be done by scheduler of knockout match type
+        prefix ??= "Knockout";
+        var matchTypes = new List<MatchType>(){
+            new KnockOut {
+                Name = prefix,
+                Players = players,
+            }
+        };
+
+        return Task.FromResult((IEnumerable<MatchType>?)matchTypes);
     }
 }
