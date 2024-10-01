93e69d34a5d32851bcfea5e0c8d8387efacb3261
knightfury16 <suhaibknight@gmail.com>
2024-10-01 23:29:52 +0600

    Implemted default GroupStanding based on game win and match point in GameFormat

--- TournamentPlanner.Domain/Entities/GameFormat.cs
+++ TournamentPlanner.Domain/Entities/GameFormat.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using TournamentPlanner.Domain.Exceptions;
 using TournamentPlanner.Domain.Interface;
 
 namespace TournamentPlanner.Domain.Entities
@@ -18,6 +19,32 @@ namespace TournamentPlanner.Domain.Entities
         public virtual string SerializeScore(object score)
         {
             return JsonSerializer.Serialize(score);
+        }
+
+        //Deafult group standing calculation based on only number of game wins, then by match points
+        public virtual List<PlayerStanding> GetGroupStanding(Tournament tournament, MatchType matchType)
+        {
+            if (tournament == null || matchType == null) throw new ArgumentNullException(nameof(GetGroupStanding));
+
+            var winnerPerGroup = tournament.WinnerPerGroup;
+            var playerStandings = matchType.Players.Select(gp => new PlayerStanding { Player = gp });
+
+            foreach (var round in matchType.Rounds)
+            {
+                foreach (var match in round.Matches)
+                {
+                    if (match.Winner == null) throw new Exception("Winner not found");
+                    var winnerStanding = playerStandings.First(ps => ps.Player.Id == match.Winner.Id);
+                    var loserStanding = playerStandings.First(ps => ps.Player.Id == (match.Winner.Id == match.FirstPlayer.Id ? match.SecondPlayer.Id : match.FirstPlayer.Id));
+
+                    winnerStanding.Wins++;
+                    winnerStanding.MatchPoints += 2;
+                    loserStanding.Losses++;
+                    loserStanding.MatchPoints += 1;
+                }
+            }
+
+            return playerStandings.OrderByDescending(ps => ps.Wins).ThenBy(ps => ps.MatchPoints).Take(winnerPerGroup).ToList();
         }
 
         public abstract IScore DeserializeScore(object scoreData);
