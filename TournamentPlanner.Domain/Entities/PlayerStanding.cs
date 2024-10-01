8a21fa182cdf963f6bbbc0a963e9e0b3eaf740d0
knightfury16 <suhaibknight@gmail.com>
2024-10-01 23:29:09 +0600

    Created PlayerStanding model entity

---
+++ TournamentPlanner.Domain/Entities/PlayerStanding.cs
@@ -0,0 +1,19 @@
+﻿using TournamentPlanner.Domain.Entities;
+
+namespace TournamentPlanner.Domain;
+
+//A Basic model, complex calculation can inherit and modify it
+public class PlayerStanding
+{
+    public required Player Player { get; set; }
+    public int MatchPoints { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int GamesWon { get; set; }
+    public int GamesLost { get; set; }
+    public int GameDifference { get; set; }
+    public int PointsWon { get; set; }
+    public int PointsLost { get; set; }
+    public int PointDifference { get; set; }
+    public int Ranking { get; set; }
+}
