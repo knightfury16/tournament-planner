795491c468b2c16c93add6d9b26a8dc1cb6dcf43
knightfury16 <suhaibknight@gmail.com>
2024-10-05 21:28:11 +0600

    power of two method added

--- TournamentPlanner.Application/Common/CreateKnockOutMatches.cs
+++ TournamentPlanner.Application/Common/CreateKnockOutMatches.cs
@@ -11,4 +11,15 @@ public class CreateKnockOutMatches : ICreateMatch
         //Todo: figure out a way to keep seeder player
         throw new NotImplementedException();
     }
+
+    static int highestPowerof2(int N)
+    {
+        // if N is a power of two simply return it
+        if ((N & (N - 1)) == 0)
+            return N;
+        var num = Convert.ToString(N, 2);
+        var num2 = num.Length + 1;
+        // else set only the most significant bit
+        return (1 << ((Convert.ToString(N, 2).Length)));
+    }
 }
