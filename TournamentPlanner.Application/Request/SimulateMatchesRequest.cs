4773c5b217d7bd397ea9c8bf5a0e88acafb4abcb
knightfury16 <suhaibknight@gmail.com>
2024-10-15 23:32:51 +0600

    SimulateMatchRequest added

---
+++ TournamentPlanner.Application/Request/SimulateMatchesRequest.cs
@@ -0,0 +1,13 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class SimulateMatchesRequest : IRequest<bool>
+{
+    public int TournamentId { get; }
+    public SimulateMatchesRequest(int TournamentId)
+    {
+        this.TournamentId = TournamentId;
+    }
+
+}
