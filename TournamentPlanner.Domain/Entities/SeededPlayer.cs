0fa06de97751a5b3ca2489f7f3a654121f84e6e7
knightfury16 <suhaibknight@gmail.com>
2024-10-21 17:39:11 +0600

    Added SeededPlayer entity

---
+++ TournamentPlanner.Domain/Entities/SeededPlayer.cs
@@ -0,0 +1,12 @@
+﻿using TournamentPlanner.Domain.Common;
+
+namespace TournamentPlanner.Domain.Entities;
+
+public class SeededPlayer : BaseEntity
+{
+    public Player? Player { get; set; }
+    public int PlayerId { get; set; }
+    public MatchType? MatchType { get; set; }
+    public int MatchTypeId { get; set; }
+
+}
