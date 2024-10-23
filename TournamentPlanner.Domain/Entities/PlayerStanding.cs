6fc561eb8ee2d763e8e99e7e4a1c8d676fd367bf
knightfury16 <suhaibknight@gmail.com>
2024-10-23 21:10:13 +0600

    player standing modified

--- TournamentPlanner.Domain/Entities/PlayerStanding.cs
+++ TournamentPlanner.Domain/Entities/PlayerStanding.cs
@@ -7,13 +7,14 @@ public class PlayerStanding
 {
     public required Player Player { get; set; }
     public int MatchPoints { get; set; }
+    public int GamePoints { get; set; }
     public int Wins { get; set; }
     public int Losses { get; set; }
     public int GamesWon { get; set; }
     public int GamesLost { get; set; }
-    public int GameDifference { get; set; }
+    public int GameDifference { get => GamesWon - GamesLost; }
     public int PointsWon { get; set; }
     public int PointsLost { get; set; }
-    public int PointDifference { get; set; }
+    public int PointsDifference { get => PointsWon - PointsLost; }
     public int Ranking { get; set; }
 }
