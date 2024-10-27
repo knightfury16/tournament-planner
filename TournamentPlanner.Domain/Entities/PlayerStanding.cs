a217b8898f816b2f4dc4a1b7480189152f4ddb7f
knightfury16 <suhaibknight@gmail.com>
2024-10-27 22:08:20 +0600

    update player standing model

--- TournamentPlanner.Domain/Entities/PlayerStanding.cs
+++ TournamentPlanner.Domain/Entities/PlayerStanding.cs
@@ -7,7 +7,6 @@ public class PlayerStanding
 {
     public required Player Player { get; set; }
     public int MatchPoints { get; set; }
-    public int GamePoints { get; set; }
     public int Wins { get; set; }
     public int Losses { get; set; }
     public int GamesWon { get; set; }
