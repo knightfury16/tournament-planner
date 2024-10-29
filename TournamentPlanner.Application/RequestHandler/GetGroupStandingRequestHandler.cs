64aeaf28864ea2b5de9ae0b052d632d766e15866
knightfury16 <suhaibknight@gmail.com>
2024-10-29 17:57:06 +0600

    Get group standing implemeted

--- TournamentPlanner.Application/RequestHandler/GetGroupStandingRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/GetGroupStandingRequestHandler.cs
@@ -1,12 +1,49 @@
-﻿using TournamentPlanner.Domain;
+﻿using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.Helpers;
+using TournamentPlanner.Domain;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Exceptions;
 using TournamentPlanner.Mediator;
+using MatchType = TournamentPlanner.Domain.Entities.MatchType;
 
 namespace TournamentPlanner.Application;
 
 public class GetGroupStandingRequestHandler : IRequestHandler<GetGroupStandingRequest, IEnumerable<PlayerStanding>>
 {
-    public Task<IEnumerable<PlayerStanding>?> Handle(GetGroupStandingRequest request, CancellationToken cancellationToken = default)
+    private readonly IRepository<Tournament> _tournamentRepository;
+    private readonly IRepository<MatchType> _matchTypeRepository;
+    private readonly IGameFormatFactory _gameFormatFactory;
+
+    public GetGroupStandingRequestHandler(IRepository<Tournament> tournamentRepository, IGameFormatFactory gameFormatFactory, IRepository<MatchType> matchTypeRepository)
     {
-        throw new NotImplementedException();
+        _tournamentRepository = tournamentRepository;
+        _gameFormatFactory = gameFormatFactory;
+        _matchTypeRepository = matchTypeRepository;
+    }
+
+    public async Task<IEnumerable<PlayerStanding>?> Handle(GetGroupStandingRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+
+        //populating tournament
+        var includePropOfTournament = nameof(Tournament.GameType);
+        var tournament = await _tournamentRepository.GetByIdAsync(request.TournamentId, [includePropOfTournament]);
+        if (tournament == null) throw new NotFoundException(nameof(tournament));
+
+        //populating matches of match type
+        var includePropOfMatchType = Utility.NavigationPrpertyCreator(nameof(MatchType.Rounds), nameof(Round.Matches));
+        var matchType = await _matchTypeRepository.GetByIdAsync(request.GroupId, [includePropOfMatchType]);
+
+        if (matchType == null) throw new NotFoundException(nameof(matchType));
+        if (matchType.Rounds == null || matchType.Rounds.Count == 0) throw new BadRequestException("No rounds founds to get group standing");
+
+
+        var gameTypeHandler = _gameFormatFactory.GetGameFormat(tournament.GameType.Name);
+
+        if (gameTypeHandler == null) throw new NotFoundException($"Game type handler could not be resolved");
+
+        var groupStanding = gameTypeHandler.GetGroupStanding(tournament, matchType, true);
+
+        return groupStanding;
     }
 }
