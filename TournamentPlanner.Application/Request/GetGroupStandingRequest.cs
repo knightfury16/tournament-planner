a8536b4f56bd6a4898c32fcf753ca6d6a1604398
knightfury16 <suhaibknight@gmail.com>
2024-10-29 17:09:38 +0600

    Get Group standing request added

---
+++ TournamentPlanner.Application/Request/GetGroupStandingRequest.cs
@@ -0,0 +1,18 @@
+﻿using TournamentPlanner.Domain;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetGroupStandingRequest: IRequest<IEnumerable<PlayerStanding>>
+{
+    public int TournamentId { get; set; }
+    public int GroupId { get; set; }
+
+    public GetGroupStandingRequest(int tournamentId, int groupId)
+    {
+        TournamentId = tournamentId;
+        GroupId = groupId;
+    }
+
+
+}
