ac8cb8eb5c189650f436c598633d327be794620c
knightfury16 <suhaibknight@gmail.com>
2024-10-29 17:10:14 +0600

    Get group standing request handler boilerplate

---
+++ TournamentPlanner.Application/RequestHandler/GetGroupStandingRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using TournamentPlanner.Domain;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetGroupStandingRequestHandler : IRequestHandler<GetGroupStandingRequest, IEnumerable<PlayerStanding>>
+{
+    public Task<IEnumerable<PlayerStanding>?> Handle(GetGroupStandingRequest request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
