0e9a0c2d419c4daa44991d6228c3e4be381df875
knightfury16 <suhaibknight@gmail.com>
2024-10-30 21:06:09 +0600

    Added player standing dto

---
+++ TournamentPlanner.Application/DTOs/PlayerStandingDto.cs
@@ -0,0 +1,19 @@
+﻿using TournamentPlanner.Application.DTOs;
+
+namespace TournamentPlanner.Application;
+
+public class PlayerStandingDto
+{
+
+    public required PlayerDto Player { get; set; }
+    public int MatchPoints { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int GamesWon { get; set; }
+    public int GamesLost { get; set; }
+    public int GameDifference { get => GamesWon - GamesLost; }
+    public int PointsWon { get; set; }
+    public int PointsLost { get; set; }
+    public int PointsDifference { get => PointsWon - PointsLost; }
+    public int Ranking { get; set; }
+}
