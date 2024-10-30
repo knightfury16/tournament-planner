f2b7c09de8518ee90d3c22ae57931c5ae1bba3a6
knightfury16 <suhaibknight@gmail.com>
2024-10-30 16:21:28 +0600

    get rounds of match type request handler implemented

---
+++ TournamentPlanner.Application/RequestHandler/GetRoundsOfMatchTypeRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Exceptions;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetRoundsOfMatchTypeRequestHandler : IRequestHandler<GetRoundsOfMatchTypeRequest, IEnumerable<RoundDto>>
+{
+    private readonly IRepository<Round> _roundRepository;
+    private readonly IMapper _mapper;
+
+
+    public GetRoundsOfMatchTypeRequestHandler(IRepository<Round> roundRepository, IMapper mapper)
+    {
+        _roundRepository = roundRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<RoundDto>?> Handle(GetRoundsOfMatchTypeRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+
+        //get round condition on mathctype id
+        var rounds = await _roundRepository.GetAllAsync(r => r.MatchTypeId == request.MatchTypeId, [nameof(Round.Matches)]);
+
+        if (rounds == null) throw new NotFoundException("Could not find any round of the ");
+
+        return (IEnumerable<RoundDto>?)_mapper.Map<RoundDto>(rounds);
+
+    }
+}
