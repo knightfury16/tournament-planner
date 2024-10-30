12e9ebf9f970573b2517336bcb3d89745f8936a2
knightfury16 <suhaibknight@gmail.com>
2024-10-30 16:21:00 +0600

    Get rounds of match types added

---
+++ TournamentPlanner.Application/Request/GetRoundsOfMatchTypeRequest.cs
@@ -0,0 +1,16 @@
+﻿using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class GetRoundsOfMatchTypeRequest : IRequest<IEnumerable<RoundDto>>
+{
+
+    public int MatchTypeId { get; }
+
+    public GetRoundsOfMatchTypeRequest(int matchTypeId)
+    {
+        MatchTypeId = matchTypeId;
+    }
+
+}
