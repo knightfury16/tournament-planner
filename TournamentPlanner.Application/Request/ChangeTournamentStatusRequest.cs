a7297fae97eaedcc3467b68ff30dfc9517b241a0
knightfury16 <suhaibknight@gmail.com>
2024-11-13 22:35:37 +0600

    Added change tournament status request

---
+++ TournamentPlanner.Application/Request/ChangeTournamentStatusRequest.cs
@@ -0,0 +1,17 @@
+﻿using TournamentPlanner.Domain.Enum;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class ChangeTournamentStatusRequest : IRequest<bool>
+{
+    public TournamentStatus TournamentStatus { get; set; }
+    public int TournamentId { get; set; }
+
+    public ChangeTournamentStatusRequest(TournamentStatus tournamentStatus, int tournamentId)
+    {
+        TournamentStatus = tournamentStatus;
+        TournamentId = tournamentId;
+    }
+
+}
