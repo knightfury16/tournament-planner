ba4540ab0096b8e152a9f2ab34c58c5412355e64
knightfury16 <suhaibknight@gmail.com>
2024-11-25 13:19:57 +0600

    config for seeding default domain roles

--- TournamentPlanner.Identity/TpIdentityDbContext.cs
+++ TournamentPlanner.Identity/TpIdentityDbContext.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TournamentPlanner.Domain.Enum;
 
 namespace TournamentPlanner.Identity;
 
@@ -10,4 +12,14 @@ public class TpIdentityDbContex : IdentityDbContext<ApplicationUser>
 
     }
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //seeding default domain role in db
+        var defaultRoles = Enum.GetValues(typeof(DomainRole)).Cast<DomainRole>()
+                           .Select((role, index) => new IdentityRole { Name = role.ToString(), NormalizedName = role.ToString().ToUpper() });
+        modelBuilder.Entity<IdentityRole>().HasData(defaultRoles);
+    }
+
+
 }
