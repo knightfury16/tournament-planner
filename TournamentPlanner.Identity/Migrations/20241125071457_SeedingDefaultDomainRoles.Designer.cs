ec692b4b0b3c4c5a4e3ab1ab8182afdc01b4bfa5
knightfury16 <suhaibknight@gmail.com>
2024-11-25 13:20:25 +0600

    Default Domain role migration

--- TournamentPlanner.Identity/Migrations/20241125071457_SeedingDefaultDomainRoles.Designer.cs
+++ TournamentPlanner.Identity/Migrations/20241125071457_SeedingDefaultDomainRoles.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TournamentPlanner.Identity;
@@ -11,9 +12,11 @@ using TournamentPlanner.Identity;
 namespace TournamentPlanner.Identity.Migrations
 {
     [DbContext(typeof(TpIdentityDbContex))]
-    partial class TpIdentityDbContexModelSnapshot : ModelSnapshot
+    [Migration("20241125071457_SeedingDefaultDomainRoles")]
+    partial class SeedingDefaultDomainRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
