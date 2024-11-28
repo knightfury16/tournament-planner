71d6ec0dbdb6faca3eaed47d8e4d0c1e124a234c
knightfury16 <suhaibknight@gmail.com>
2024-11-28 20:36:40 +0600

    Default role with policy claim migration

--- TournamentPlanner.Identity/Migrations/20241128120803_DefaultRoleWithClaims.Designer.cs
+++ TournamentPlanner.Identity/Migrations/20241128120803_DefaultRoleWithClaims.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TournamentPlanner.Identity;
@@ -11,9 +12,11 @@ using TournamentPlanner.Identity;
 namespace TournamentPlanner.Identity.Migrations
 {
     [DbContext(typeof(TpIdentityDbContex))]
-    partial class TpIdentityDbContexModelSnapshot : ModelSnapshot
+    [Migration("20241128120803_DefaultRoleWithClaims")]
+    partial class DefaultRoleWithClaims
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
