996598e1cb5834facaee73a3d60dd2ab8c730d8d
knightfury16 <suhaibknight@gmail.com>
2024-11-28 20:35:14 +0600

    Domain Claim types class added

---
+++ TournamentPlanner.Domain/Constant/DomainClaim.cs
@@ -0,0 +1,7 @@
+namespace TournamentPlanner.Domain.Constant;
+
+public static class DomainClaim
+{
+    public const string PermissionClaimType = "Permission";
+    public const string DomainUserIdClaimType = "DomainUserId";
+}
