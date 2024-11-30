13a01822d144a7aec32fc1b1523c0c720041091d
knightfury16 <suhaibknight@gmail.com>
2024-11-30 12:29:53 +0600

    integrated add admin with identity

--- TournamentPlanner.Application/RequestHandler/AddAdminRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/AddAdminRequestHandler.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using TournamentPlanner.Application.Common.Interfaces;
 using TournamentPlanner.Application.DTOs;
+using TournamentPlanner.Domain.Constant;
 using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Exceptions;
 using TournamentPlanner.Mediator;
 
 namespace TournamentPlanner.Application;
@@ -9,22 +11,48 @@ namespace TournamentPlanner.Application;
 public class AddAdminRequestHandler : IRequestHandler<AddAdminRequest, AdminDto>
 {
     public IRepository<Admin> _adminRepository { get; set; }
+    private readonly IIdentityService _identityService;
     public IMapper _mapper { get; set; }
 
-    public AddAdminRequestHandler(IMapper mapper, IRepository<Admin> adminRepository)
+    public AddAdminRequestHandler(IMapper mapper, IRepository<Admin> adminRepository, IIdentityService identityService)
     {
         _mapper = mapper;
         _adminRepository = adminRepository;
+        _identityService = identityService;
     }
     public async Task<AdminDto?> Handle(AddAdminRequest request, CancellationToken cancellationToken = default)
     {
-        if(request == null){
-            throw new ArgumentNullException(nameof(request));
-        }
+        ArgumentNullException.ThrowIfNull(request);
+        //tp admin
+        Admin admin = await makeTpApplicationAdmin(request);
+
+        //make identity application user
+        await makeIdentityApplicationUser(admin, request.AddAdminDto.Password);
+
+        return _mapper.Map<AdminDto>(admin);
+    }
+
+    private async Task makeIdentityApplicationUser(Admin admin, string password)
+    {
+        var applicationUserDto = new ApplicationUserDto
+        {
+            Email = admin.Email,
+            Password = password,
+            UserName = admin.Name,
+            DomainUserId = admin.Id
+        };
+        var result = await _identityService.RegisterApplicationUserAndSigninAsync(applicationUserDto, true);
+        if (!result) throw new InternalServerErrorException("Could not create identity user");
+
+        //adding the Admin role
+        await _identityService.AddRoleToApplicationUserAsync(admin.Email, Role.Admin);
+    }
+
+    private async Task<Admin> makeTpApplicationAdmin(AddAdminRequest request)
+    {
         var admin = _mapper.Map<Admin>(request.AddAdminDto);
         await _adminRepository.AddAsync(admin);
         await _adminRepository.SaveAsync();
-
-        return _mapper.Map<AdminDto>(admin);
+        return admin;
     }
 }
