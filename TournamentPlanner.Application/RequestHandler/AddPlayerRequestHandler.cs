1bb635c3a86a41b7a8a462f8961820a82e4f6606
knightfury16 <suhaibknight@gmail.com>
2024-11-30 12:47:05 +0600

    integrated app player with identity

--- TournamentPlanner.Application/RequestHandler/AddPlayerRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/AddPlayerRequestHandler.cs
@@ -2,7 +2,9 @@ using AutoMapper;
 using TournamentPlanner.Application.Common.Interfaces;
 using TournamentPlanner.Application.DTOs;
 using TournamentPlanner.Application.Request;
+using TournamentPlanner.Domain.Constant;
 using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Exceptions;
 using TournamentPlanner.Mediator;
 
 namespace TournamentPlanner.Application.RequestHandler
@@ -10,24 +12,47 @@ namespace TournamentPlanner.Application.RequestHandler
     public class AddPlayerRequestHandler : IRequestHandler<AddPlayerRequest, PlayerDto>
     {
         public IRepository<Player> _playerRepository { get; set; }
+        private readonly IIdentityService _identityService;
         public IMapper _mapper { get; set; }
 
-        public AddPlayerRequestHandler(IRepository<Player> playerRepository, IMapper mapper)
+        public AddPlayerRequestHandler(IRepository<Player> playerRepository, IMapper mapper, IIdentityService identityService)
         {
             _playerRepository = playerRepository;
             _mapper = mapper;
+            _identityService = identityService;
         }
         public async Task<PlayerDto?> Handle(AddPlayerRequest request, CancellationToken cancellationToken = default)
         {
-            if(request == null){
-                throw new ArgumentNullException(nameof(request));
-            }
+            ArgumentNullException.ThrowIfNull(request);
+
+            Player player = await makeTpApplicationPlayer(request);
+            await makeIdentityApplicationUser(player, request.AddPlayerDto.Password);
+
+            return _mapper.Map<PlayerDto>(player);
+        }
+
+        private async Task makeIdentityApplicationUser(Player player, string password)
+        {
+            var applicationUserDto = new ApplicationUserDto
+            {
+                Email = player.Email,
+                Password = password,
+                UserName = player.Name,
+                DomainUserId = player.Id
+            };
+            var result = await _identityService.RegisterApplicationUserAndSigninAsync(applicationUserDto, true);
+            if (!result) throw new InternalServerErrorException("Could not create identity user.");
+
+            await _identityService.AddRoleToApplicationUserAsync(player.Email, Role.Player);
+        }
+
+        private async Task<Player> makeTpApplicationPlayer(AddPlayerRequest request)
+        {
             var player = _mapper.Map<Player>(request.AddPlayerDto);
 
             await _playerRepository.AddAsync(player);
             await _playerRepository.SaveAsync();
-
-            return _mapper.Map<PlayerDto>(player);
+            return player;
         }
     }
 }
