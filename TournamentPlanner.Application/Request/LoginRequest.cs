1bcfdd12452f9500216f1c7680d46c3aba372a56
knightfury16 <suhaibknight@gmail.com>
2024-11-30 16:13:06 +0600

    Login request and handler created

---
+++ TournamentPlanner.Application/Request/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class LoginRequest : IRequest<bool>
+{
+  public LoginDto LoginRequestDto { get; set; }
+  public bool IsPersistent = true;
+
+  public LoginRequest(LoginDto loginRequestDto)
+  {
+    LoginRequestDto = loginRequestDto;
+  }
+}
