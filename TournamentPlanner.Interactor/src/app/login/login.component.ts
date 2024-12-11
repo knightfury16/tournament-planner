9e37d9d69a70e0bab053ed5dea5ff180eb65688c
knightfury16 <suhaibknight@gmail.com>
2024-12-11 21:03:43 +0600

    created the login form

--- TournamentPlanner.Interactor/src/app/login/login.component.ts
+++ TournamentPlanner.Interactor/src/app/login/login.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule, CommonModule, 
+    MatFormFieldModule,FormsModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+    // Any initialization logic can go here
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      // Handle login request here
+      console.log(this.loginForm.value)
+      // Example: this.authService.login(this.loginForm.value);
+    }
+  }
 }
