468d905919d92def648ccc1edfb7a744b62d8455
knightfury16 <suhaibknight@gmail.com>
2024-12-11 20:25:15 +0600

    nav bar updated

--- TournamentPlanner.Interactor/src/app/nav-bar/nav-bar.component.ts
+++ TournamentPlanner.Interactor/src/app/nav-bar/nav-bar.component.ts
@@ -3,15 +3,24 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-  public ApplicationTitle: string = "TP"
+  public ApplicationTitle: string = "Tournament Planner"
+  public userRole: string | null = null; // Define userRole variable
+
+  constructor() {
+    // Example: Set userRole based on some logic
+    // this.userRole = 'admin'; // Uncomment to simulate an admin user
+    // this.userRole = 'player'; // Uncomment to simulate a player user
+    // this.userRole = null; // Uncomment to simulate a guest user
+  }
 
 }
