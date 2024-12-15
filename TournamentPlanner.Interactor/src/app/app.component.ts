c9d7848ff762bd3ffc4af4f9f47757185b27f3dd
knightfury16 <suhaibknight@gmail.com>
2024-12-15 21:11:56 +0600

    added footer to app root

--- TournamentPlanner.Interactor/src/app/app.component.ts
+++ TournamentPlanner.Interactor/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, RouterOutlet, RouterModule],
+  imports: [NavBarComponent, RouterOutlet, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
