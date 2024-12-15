db7bbb657e95aaaded23bce8284b16627053f82a
knightfury16 <suhaibknight@gmail.com>
2024-12-15 12:51:51 +0600

    nav bar modified to show navigation based on user role

--- TournamentPlanner.Interactor/src/app/nav-bar/nav-bar.component.ts
+++ TournamentPlanner.Interactor/src/app/nav-bar/nav-bar.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService, UserInfo } from '../../Shared/auth.service';
+import { DomainRole } from '../tp-model/TpModel';
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,11 +18,30 @@ export class NavBarComponent {
   public ApplicationTitle: string = "Tournament Planner"
   public userRole: string | null = null; // Define userRole variable
 
-  constructor() {
+  constructor(private authService: AuthService) {
     // Example: Set userRole based on some logic
     // this.userRole = 'admin'; // Uncomment to simulate an admin user
     // this.userRole = 'player'; // Uncomment to simulate a player user
     // this.userRole = null; // Uncomment to simulate a guest user
+  }
+
+  public getCurrentUser(): UserInfo | null {
+    return this.authService.currentUser();
+  }
+
+  public getCurrentUserRole(): string | undefined {
+    return this.authService.currentUser()?.role;
+  }
+
+  public isCurrentUserAdmin(): boolean {
+    return this.getCurrentUserRole() === DomainRole.Admin.toString();;
+  }
+
+  public isCurrentUserPlayer(): boolean {
+    return this.getCurrentUserRole() === DomainRole.Player.toString();
+  }
+  public signOut() {
+    this.authService.singOut();
   }
 
 }
