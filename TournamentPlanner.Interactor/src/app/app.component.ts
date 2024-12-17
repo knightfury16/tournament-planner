c14bccc3ba3e0e83a25ed464bff7b2287c173741
knightfury16 <suhaibknight@gmail.com>
2024-12-17 17:41:29 +0600

    added app spinner in app component

--- TournamentPlanner.Interactor/src/app/app.component.ts
+++ TournamentPlanner.Interactor/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from "./footer/footer.component";
+import { SpinnerComponent } from "./spinner/spinner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent, RouterOutlet, RouterModule, FooterComponent],
+  imports: [NavBarComponent, RouterOutlet, RouterModule, FooterComponent, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
