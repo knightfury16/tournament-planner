d0492139842086dec3ced3d4adbcddd624d7116b
knightfury16 <suhaibknight@gmail.com>
2024-12-18 17:25:25 +0600

    route fix

--- TournamentPlanner.Interactor/src/Shared/auth.service.ts
+++ TournamentPlanner.Interactor/src/Shared/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Inject, Injectable, signal } from '@angular/core';
 import { TP_BASE_URL } from '../app/app.config';
 import { firstValueFrom, Observable } from 'rxjs';
-import { AddAdminDto, AddPlayerDto, LoginDto } from '../app/tp-model/TpModel';
+import { AddAdminDto, AddPlayerDto, LoginDto, PlayerDto } from '../app/tp-model/TpModel';
 import { LoadingService } from './loading.service';
 
 export interface UserInfo {
@@ -40,28 +40,28 @@ export class AuthService {
     return this.httpClient.post<UserInfo>(`${this.accountBaseUrl}/login`, loginDto, { headers: { 'Content-Type': 'application/json' }, withCredentials: true });
   }
 
-  public registerPlayer(addPlayerDto: AddPlayerDto): Observable<UserInfo> {
-    return this.httpClient.post<UserInfo>(`${this.accountBaseUrl}/player`, { addPlayerDto });
+  public registerPlayer(addPlayerDto: AddPlayerDto): Observable<PlayerDto> {
+    return this.httpClient.post<PlayerDto>(`${this.baseUrl}/players`, addPlayerDto, { headers: { 'Content-Type': 'application/json' }, withCredentials: true });
   }
 
   public registerAdmin(addAdminDto: AddAdminDto): Observable<UserInfo> {
-    return this.httpClient.post<UserInfo>(`${this.accountBaseUrl}/admin`, { addAdminDto });
+    return this.httpClient.post<UserInfo>(`${this.baseUrl}/admins`, addAdminDto, { headers: { 'Content-Type': 'application/json' }, withCredentials: true });
   }
 
   public async singOut(): Promise<string> {
-    var logoutResult = await firstValueFrom(this.httpClient.post<string>(`${this.accountBaseUrl}/logout`,null,{withCredentials: true}));
+    var logoutResult = await firstValueFrom(this.httpClient.post<string>(`${this.accountBaseUrl}/logout`, null, { withCredentials: true }));
     console.log("LOGOUR:: ", logoutResult);
     this.currentUser.set(null);
     return logoutResult;
   }
 
 
-  private async initializeUserInfo(): Promise<void> { 
+  private async initializeUserInfo(): Promise<void> {
     const user = await this.getUserInfo();
-    if (user) { this.currentUser.set(user); } 
+    if (user) { this.currentUser.set(user); }
   }
 
   public async getUserInfo(): Promise<UserInfo | undefined> {
-    return await firstValueFrom(this.httpClient.get<UserInfo | undefined>(`${this.accountBaseUrl}/userInfo`, {withCredentials: true}));
+    return await firstValueFrom(this.httpClient.get<UserInfo | undefined>(`${this.accountBaseUrl}/userInfo`, { withCredentials: true }));
   }
 }
