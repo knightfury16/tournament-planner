fd9e33241f26028d1f713179d963bf1991b9631b
knightfury16 <suhaibknight@gmail.com>
2024-12-18 15:56:08 +0600

    register player template complete

--- TournamentPlanner.Interactor/src/app/register-player/register-player.component.ts
+++ TournamentPlanner.Interactor/src/app/register-player/register-player.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register-player',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, ReactiveFormsModule, MatFormField, MatInputModule, MatButtonModule, CommonModule],
   templateUrl: './register-player.component.html',
   styleUrl: './register-player.component.scss'
 })
 export class RegisterPlayerComponent {
+
+  public registerPlayerForm = new FormGroup({
+    name: new FormControl('', [Validators.minLength(5), Validators.required,]), 
+    age: new FormControl(),
+    weight: new FormControl(),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+
+  public register() {
+    console.log(this.registerPlayerForm);
+  }
 
 }
