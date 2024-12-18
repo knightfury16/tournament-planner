a81f20134889e8f7b5f2b682a0ee99b7bf60b19f
knightfury16 <suhaibknight@gmail.com>
2024-12-18 12:48:51 +0600

    added register player component

---
+++ TournamentPlanner.Interactor/src/app/register-player/register-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-player.component.html',
+  styleUrl: './register-player.component.scss'
+})
+export class RegisterPlayerComponent {
+
+}
