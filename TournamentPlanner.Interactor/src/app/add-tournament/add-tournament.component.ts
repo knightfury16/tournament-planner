f4beffe7a0f42454f4a0439218de720d6d3e36a0
knightfury16 <suhaibknight@gmail.com>
2024-12-19 23:16:33 +0600

    registrator validator fix

--- TournamentPlanner.Interactor/src/app/add-tournament/add-tournament.component.ts
+++ TournamentPlanner.Interactor/src/app/add-tournament/add-tournament.component.ts
@@ -79,7 +79,7 @@ export class AddTournamentComponent {
     const registrationLastDate = control.value
     var val = new Date(registrationLastDate);
     const startDate = control.parent?.get('startDate')?.value;
-    if (registrationLastDate && startDate && new Date(registrationLastDate) > new Date(startDate)) {
+    if (registrationLastDate && startDate && new Date(registrationLastDate) >= new Date() && new Date(registrationLastDate) < new Date(startDate)) {
       return null;
     }
     return { 'invalidRegistrationLastDate': true };
