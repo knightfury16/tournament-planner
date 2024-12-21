1bfc92d27235cabd4124b52e802351afc431fab6
knightfury16 <suhaibknight@gmail.com>
2024-12-21 11:51:08 +0600

    working on tournament list

--- TournamentPlanner.Interactor/src/app/tournament-list/tournament-list.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-list/tournament-list.component.ts
@@ -2,26 +2,43 @@ import { Component } from '@angular/core';
 import { TournamentPlannerService } from '../tournament-planner.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+
 import {
   debounceTime,
   distinctUntilChanged,
+  firstValueFrom,
   map,
   startWith,
   switchMap,
 } from 'rxjs';
 import { RouterModule } from '@angular/router';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { TournamentDto } from '../tp-model/TpModel';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-tournament-list',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, MatFormFieldModule,
+    MatInputModule,MatListModule, MatCardModule, MatIconModule, MatButtonModule, MatTabsModule],
   templateUrl: './tournament-list.component.html',
   styleUrl: './tournament-list.component.scss'
 })
 export class TournamentListComponent {
   nameInput = new FormControl();
+  public tournaments: TournamentDto[] = [];
 
-  constructor(private tp: TournamentPlannerService){}
+  constructor(private tp: TournamentPlannerService) {
+  }
+
+  
+
 
 
   public data$ = this.nameInput.valueChanges.pipe(
