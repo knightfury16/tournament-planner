69ee0586ec81274f6b446cf069ab91b3c2d65775
knightfury16 <suhaibknight@gmail.com>
2024-12-23 21:20:41 +0600

    RND'ing tabs

--- TournamentPlanner.Interactor/src/app/test/test.component.ts
+++ TournamentPlanner.Interactor/src/app/test/test.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { CashRegisterComponent } from "../cash-register/cash-register.component";
+import { TournamentListComponent } from "../tournament-list/tournament-list.component";
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule, RouterModule, MatTabsModule, CashRegisterComponent, TournamentListComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
 export class TestComponent {
 
+  public mytabIndex = 0;
+  public fromTabChange = 0;
+  public tabChange($event: MatTabChangeEvent) {
+    this.fromTabChange = $event.index;
+  }
+
 }
+  
