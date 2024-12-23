0822cd5071f8f1b552867ec16f16546cce30d534
knightfury16 <suhaibknight@gmail.com>
2024-12-23 21:30:27 +0600

    details homepage contained in card

--- TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TournamentDto } from '../tp-model/TpModel';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-tournament-details-homepage',
   standalone: true,
-  imports: [MatTabsModule, CommonModule],
+  imports: [MatTabsModule, CommonModule, MatCardModule],
   templateUrl: './tournament-details-homepage.component.html',
   styleUrl: './tournament-details-homepage.component.scss'
 })
