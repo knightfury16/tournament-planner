ac73f1bc16d0d9af53821ccfc9e536843ca5c7f5
knightfury16 <suhaibknight@gmail.com>
2024-12-23 18:07:10 +0600

    tournament card style modified

--- TournamentPlanner.Interactor/src/app/tournament-card/tournament-card.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-card/tournament-card.component.ts
@@ -4,11 +4,12 @@ import { transformTournamentIsoDate } from '../../Shared/Utility/dateTimeUtility
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { mapStringToEnum } from '../../Shared/Utility/stringUtility';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tournament-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule],
+  imports: [MatCardModule, MatChipsModule, RouterModule],
   templateUrl: './tournament-card.component.html',
   styleUrl: './tournament-card.component.scss'
 })
@@ -23,7 +24,6 @@ export class TournamentCardComponent {
   public getGameType(): GameTypeSupported | null {
     var gameType = this.tournament?.gameTypeDto?.name;
     var gameTypeSupport = mapStringToEnum(GameTypeSupported, gameType!);
-    console.log("SUPPPOORRTT:: ", gameTypeSupport);
     if (gameType) return gameTypeSupport;
 
     return null;
