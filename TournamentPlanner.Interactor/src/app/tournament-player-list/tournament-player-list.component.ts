b112472e8b41a0c47fbfee77014c2c0c9814445f
knightfury16 <suhaibknight@gmail.com>
2024-12-24 14:33:55 +0600

    added tournament player list

---
+++ TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-player-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tournament-player-list.component.html',
+  styleUrl: './tournament-player-list.component.scss'
+})
+export class TournamentPlayerListComponent {
+
+}
