a923d5b702300fc6616cfc207301c7e625e651d7
knightfury16 <suhaibknight@gmail.com>
2024-12-24 18:10:09 +0600

    Made player with mat expansion panel

--- TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
@@ -5,11 +5,12 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { PlayerTabViewType } from '../tournament-details-homepage/tournament-details-homepage.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-tournament-player-list',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, MatCardModule],
+  imports: [MatListModule, MatExpansionModule, MatButtonModule, MatCardModule],
   templateUrl: './tournament-player-list.component.html',
   styleUrl: './tournament-player-list.component.scss'
 })
