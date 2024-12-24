90a19605cc6f032d2584dd8934ad973ca2f0f46e
knightfury16 <suhaibknight@gmail.com>
2024-12-24 17:58:24 +0600

    tournament player list basic functional

--- TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output, signal } from '@angular/core';
+import { PlayerDto } from '../tp-model/TpModel';
+import { TournamentPlannerService } from '../tournament-planner.service';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { PlayerTabViewType } from '../tournament-details-homepage/tournament-details-homepage.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tournament-player-list',
   standalone: true,
-  imports: [],
+  imports: [MatListModule, MatButtonModule, MatCardModule],
   templateUrl: './tournament-player-list.component.html',
   styleUrl: './tournament-player-list.component.scss'
 })
-export class TournamentPlayerListComponent {
+export class TournamentPlayerListComponent implements OnInit {
+  @Input({ required: true }) tournamentId?: string;
+  @Output() playerTabChangeEvent = new EventEmitter<PlayerTabViewType>();
+
+  public players = signal<PlayerDto[] | undefined>(undefined);
+  public tpServive = inject(TournamentPlannerService);
+
+  async ngOnInit() {
+    var playerResponse = await this.tpServive.getTournamentPlayers(this.tournamentId!);
+    this.players.set(playerResponse);
+  }
+  //From the list view i will always trigger the detail view
+  public emitTabViewChangeEvent(): void {
+    this.playerTabChangeEvent.emit(PlayerTabViewType.DetailView);
+  }
+
 
 }
