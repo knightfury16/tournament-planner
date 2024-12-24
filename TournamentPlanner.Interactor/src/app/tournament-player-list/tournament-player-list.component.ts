e32af884b0e9157d954947255948266ec65e4ee8
knightfury16 <suhaibknight@gmail.com>
2024-12-24 18:29:43 +0600

    added ng template

--- TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-player-list/tournament-player-list.component.ts
@@ -6,11 +6,12 @@ import { MatCardModule } from '@angular/material/card';
 import { PlayerTabViewType } from '../tournament-details-homepage/tournament-details-homepage.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tournament-player-list',
   standalone: true,
-  imports: [MatListModule, MatExpansionModule, MatButtonModule, MatCardModule],
+  imports: [MatListModule, MatExpansionModule, MatButtonModule, MatCardModule, CommonModule],
   templateUrl: './tournament-player-list.component.html',
   styleUrl: './tournament-player-list.component.scss'
 })
@@ -28,6 +29,12 @@ export class TournamentPlayerListComponent implements OnInit {
   //From the list view i will always trigger the detail view
   public emitTabViewChangeEvent(): void {
     this.playerTabChangeEvent.emit(PlayerTabViewType.DetailView);
+  }
+  public getRoundWinRatio(playerWinRation: number | null | undefined): string {
+    if (playerWinRation) {
+      return `${Math.ceil(playerWinRation * 100)}%`
+    }
+    return "N/A";
   }
 
 
