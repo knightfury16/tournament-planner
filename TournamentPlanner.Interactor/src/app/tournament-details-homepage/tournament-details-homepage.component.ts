0d0e7feba999d4c860cb9a109383b6f737d5575c
knightfury16 <suhaibknight@gmail.com>
2024-12-24 14:34:22 +0600

    tournament details homepage design change

--- TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
@@ -3,11 +3,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { TournamentDto } from '../tp-model/TpModel';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { TournamentPlayerListComponent } from "../tournament-player-list/tournament-player-list.component";
 
 @Component({
   selector: 'app-tournament-details-homepage',
   standalone: true,
-  imports: [MatTabsModule, CommonModule, MatCardModule],
+  imports: [MatTabsModule, CommonModule, MatCardModule, TournamentPlayerListComponent],
   templateUrl: './tournament-details-homepage.component.html',
   styleUrl: './tournament-details-homepage.component.scss'
 })
