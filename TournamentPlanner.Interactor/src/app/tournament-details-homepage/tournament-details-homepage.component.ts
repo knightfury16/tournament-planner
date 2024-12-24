5bbcc7e5557bcae5cdb23e23e1b17d25a005c164
knightfury16 <suhaibknight@gmail.com>
2024-12-24 17:59:26 +0600

    tournament details homepage palyer tab view change logic added

--- TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
@@ -1,17 +1,33 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input, signal } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TournamentDto } from '../tp-model/TpModel';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { TournamentPlayerListComponent } from "../tournament-player-list/tournament-player-list.component";
+import { TournamentPlayerDetailsComponent } from '../tournament-player-details/tournament-player-details.component';
+
+export enum PlayerTabViewType {
+  ListView = "List View",
+  DetailView = "Detail View"
+}
 
 @Component({
   selector: 'app-tournament-details-homepage',
   standalone: true,
-  imports: [MatTabsModule, CommonModule, MatCardModule, TournamentPlayerListComponent],
+  imports: [MatTabsModule, CommonModule, MatCardModule, TournamentPlayerListComponent, TournamentPlayerDetailsComponent],
   templateUrl: './tournament-details-homepage.component.html',
   styleUrl: './tournament-details-homepage.component.scss'
 })
 export class TournamentDetailsHomepageComponent {
   @Input() public tournamentId?: string;
+  public playerTabViewType = PlayerTabViewType;
+
+  public playerTabView = signal<PlayerTabViewType>(PlayerTabViewType.ListView);
+
+
+  public togglePlayerTabView(view: PlayerTabViewType) {
+    this.playerTabView.set(view);
+  }
+
+
 }
