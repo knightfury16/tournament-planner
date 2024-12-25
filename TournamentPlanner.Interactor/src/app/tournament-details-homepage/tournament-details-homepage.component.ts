cddfb8937e6c93595ba399f3fa8074abc866179a
knightfury16 <suhaibknight@gmail.com>
2024-12-25 11:29:52 +0600

    tournament detaisl homepage reciving some input from detail page and passing participants players list component

--- TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-details-homepage/tournament-details-homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input, signal } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { TournamentDto } from '../tp-model/TpModel';
+import { PlayerDto, TournamentDto } from '../tp-model/TpModel';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { TournamentPlayerListComponent } from "../tournament-player-list/tournament-player-list.component";
 import { TournamentPlayerDetailsComponent } from '../tournament-player-details/tournament-player-details.component';
+import { TournamentDetailsComponent } from "../tournament-details/tournament-details.component";
 
 export enum PlayerTabViewType {
   ListView = "List View",
@@ -14,12 +15,15 @@ export enum PlayerTabViewType {
 @Component({
   selector: 'app-tournament-details-homepage',
   standalone: true,
-  imports: [MatTabsModule, CommonModule, MatCardModule, TournamentPlayerListComponent, TournamentPlayerDetailsComponent],
+  imports: [MatTabsModule, CommonModule, MatCardModule, TournamentPlayerListComponent, TournamentPlayerDetailsComponent, TournamentDetailsComponent],
   templateUrl: './tournament-details-homepage.component.html',
   styleUrl: './tournament-details-homepage.component.scss'
 })
 export class TournamentDetailsHomepageComponent {
   @Input() public tournamentId?: string;
+
+  public tournamentName = signal('');
+  public tournamentParticipants = signal<PlayerDto[] | undefined>(undefined);
   public playerTabViewType = PlayerTabViewType;
 
   public playerTabView = signal<PlayerTabViewType>(PlayerTabViewType.ListView);
@@ -27,6 +31,14 @@ export class TournamentDetailsHomepageComponent {
 
   public togglePlayerTabView(view: PlayerTabViewType) {
     this.playerTabView.set(view);
+  }
+
+  public tournamentNameEC(tournamentName: string) {
+    this.tournamentName.set(tournamentName);
+  }
+
+  public tournamentParticipantsEC(participants: PlayerDto[]) {
+    this.tournamentParticipants.set(participants);
   }
 
 
