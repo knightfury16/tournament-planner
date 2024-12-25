0d98f402f926ae725d0c859b77c7394b92ce7e56
knightfury16 <suhaibknight@gmail.com>
2024-12-25 17:54:09 +0600

    Enhance tournament card component: add manage button functionality, update styles for manage button, and include necessary Angular Material imports.

--- TournamentPlanner.Interactor/src/app/tournament-card/tournament-card.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-card/tournament-card.component.ts
@@ -5,17 +5,20 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { mapStringToEnum } from '../../Shared/Utility/stringUtility';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tournament-card',
   standalone: true,
-  imports: [MatCardModule, MatChipsModule, RouterModule],
+  imports: [MatIconModule, MatCardModule, MatButtonModule, MatChipsModule, RouterModule],
   templateUrl: './tournament-card.component.html',
   styleUrl: './tournament-card.component.scss'
 })
 export class TournamentCardComponent {
 
   @Input({ required: true, transform: transformTournamentIsoDate }) tournament: TournamentDto | null = null;
+  @Input() manage: boolean = false;
 
   public getVenue() {
     return this.tournament?.venue ?? 'N/A'
