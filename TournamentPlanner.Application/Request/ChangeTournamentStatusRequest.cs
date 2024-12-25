87bcc3391d38135a0206f9ffcd459ddfbe004ec1
knightfury16 <suhaibknight@gmail.com>
2024-12-26 00:18:55 +0600

    Refactor ChangeTournamentStatusRequest to accept string input for tournament status and implement parsing logic
    - Updated constructor to accept a string representation of tournament status.
    - Added GetTournamentStatusEnum method to convert string to TournamentStatus enum, with defaulting to Draft on failure.
    - Introduced GetTrimmedString method to clean input strings by removing spaces.

--- TournamentPlanner.Application/Request/ChangeTournamentStatusRequest.cs
+++ TournamentPlanner.Application/Request/ChangeTournamentStatusRequest.cs
@@ -8,10 +8,28 @@ public class ChangeTournamentStatusRequest : IRequest<bool>
     public TournamentStatus TournamentStatus { get; set; }
     public int TournamentId { get; set; }
 
-    public ChangeTournamentStatusRequest(int tournamentId, TournamentStatus tournamentStatus)
+    public ChangeTournamentStatusRequest(int tournamentId, string tournamentStatus)
     {
         TournamentId = tournamentId;
-        TournamentStatus = tournamentStatus;
+
+        TournamentStatus = GetTournamentStatusEnum(tournamentStatus);
     }
 
+    public TournamentStatus GetTournamentStatusEnum(string statusString)
+    {
+        var trimString = GetTrimmedString(statusString);
+
+        if (!Enum.TryParse(trimString, out TournamentStatus tournamentStatus))
+        {
+            return TournamentStatus.Draft;
+        }
+
+        return tournamentStatus;
+    }
+
+    private string GetTrimmedString(string statusString)
+    {
+        statusString = statusString.Replace(" ", "");
+        return statusString.Trim();
+    }
 }
