90cd9a681a5028953682f45bdba8f92958f06203
knightfury16 <suhaibknight@gmail.com>
2024-12-25 23:58:08 +0600

    Add tournament status management to manage tournament details component
    - Introduced a new section for displaying and changing tournament status in the HTML template.
    - Added styles for the status button and action margin in the SCSS file.
    - Updated TypeScript logic to handle tournament status selection, including methods to retrieve and set the current status.
    - Integrated Angular Material components for improved UI/UX in the status management feature.

--- TournamentPlanner.Interactor/src/app/manage-tournament-details/manage-tournament-details.component.ts
+++ TournamentPlanner.Interactor/src/app/manage-tournament-details/manage-tournament-details.component.ts
@@ -1,22 +1,40 @@
 import { Component, EventEmitter, inject, Input, OnInit, Output, signal } from '@angular/core';
-import { TournamentDto } from '../tp-model/TpModel';
+import { TournamentDto, TournamentStatus } from '../tp-model/TpModel';
 import { TournamentPlannerService } from '../tournament-planner.service';
 import { transformTournamentIsoDate } from '../../Shared/Utility/dateTimeUtility';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { mapStringToEnum } from '../../Shared/Utility/stringUtility';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-manage-tournament-details',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './manage-tournament-details.component.html',
   styleUrl: './manage-tournament-details.component.scss'
 })
 export class ManageTournamentDetailsComponent implements OnInit {
 
   @Input({ required: true }) public tournamentId?: string;
+  @Output() public tournamentNameEE = new EventEmitter<string>();
+
+  public tournamentStatus = TournamentStatus;
   private _tpService = inject(TournamentPlannerService);
 
   public tournamentDetails = signal<TournamentDto | null>(null);
-  @Output() public tournamentNameEE = new EventEmitter<string>();
+  public selectedStatus = TournamentStatus.Draft;
+  public statusFormControl = new FormControl();
 
   async ngOnInit() {
     if (this.tournamentId == undefined) { this.emitADummyName(); return }
@@ -24,6 +42,11 @@ export class ManageTournamentDetailsComponent implements OnInit {
     var transformDateTournament = transformTournamentIsoDate(tourDetail);
     this.tournamentDetails.set(transformDateTournament);
     this.emitTournamentName(tourDetail.name);
+    this.setSelectedStatus();
+  }
+
+  setSelectedStatus() {
+    this.selectedStatus = this.getStatusValue();
   }
   emitTournamentName(tournamentName: string) {
     this.tournamentNameEE.emit(tournamentName);
@@ -32,4 +55,22 @@ export class ManageTournamentDetailsComponent implements OnInit {
   emitADummyName() {
     this.tournamentNameEE.emit("Undefined Tournament");
   }
+  getStatusValue(): TournamentStatus {
+    var status = this.tournamentDetails()?.status ?? TournamentStatus.Draft.toString();
+    return mapStringToEnum(TournamentStatus, status);
+  }
+
+  getTournamentStatusString(): string[] {
+    // I have to do this hack because if there is space in enum values all hell break loose
+    //ts can not map it properly
+    var status: string[] = [];
+    Object.keys(TournamentStatus).forEach(
+      key => {
+        if (!isNaN(Number(key))) return;
+        status.push(TournamentStatus[key as keyof typeof TournamentStatus].toString());
+      }
+    )
+    return status;
+  }
+
 }
