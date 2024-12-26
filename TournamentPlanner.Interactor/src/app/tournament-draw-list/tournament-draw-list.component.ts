5471e8efc95cdc63ebd3fef42e61a753e8b0b9fb
knightfury16 <suhaibknight@gmail.com>
2024-12-26 18:41:33 +0600

    Add TournamentDrawListComponent with HTML, SCSS, and TypeScript files
    - Implemented TournamentDrawListComponent to display tournament draws.
    - Created HTML template to render match types and their completion status.
    - Added SCSS styles for chip indicators and card layout.
    - Integrated service call to fetch tournament draws and handle errors.
    - Introduced utility function for formatting creation dates of draws.

---
+++ TournamentPlanner.Interactor/src/app/tournament-draw-list/tournament-draw-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { DrawDto, MatchTypeDto } from '../tp-model/TpModel';
+import { TournamentPlannerService } from '../tournament-planner.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCommonModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { getDateStringInFormat } from '../../Shared/Utility/dateTimeUtility';
+
+@Component({
+  selector: 'app-tournament-draw-list',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatChipsModule, MatCommonModule],
+  templateUrl: './tournament-draw-list.component.html',
+  styleUrl: './tournament-draw-list.component.scss'
+})
+export class TournamentDrawListComponent implements OnInit {
+  @Input({ required: true }) public tournamentId?: string;
+  private _tpService = inject(TournamentPlannerService);
+  public draws = signal<DrawDto[] | undefined>(undefined);
+
+
+  async ngOnInit() {
+    try {
+      var reqResponse = await this._tpService.getTournamentDraws(this.tournamentId!);
+      console.log(reqResponse);
+      this.draws.set(reqResponse);
+      console.log(this.draws());
+
+    } catch (error) {
+      console.log(error);
+      console.log((error as any).error ?? (error as any).error?.Error ?? "An unknown error occurred.");
+    }
+  }
+
+  getClassName(matchType: MatchTypeDto): string {
+    if (matchType.isCompleted) return "chip-completed"
+    return "chip-incompleted"
+  }
+
+  getCreatedAtDate(draw: DrawDto): string {
+    return getDateStringInFormat(new Date(draw.createdAt!));
+  }
+
+
+
+
+}
