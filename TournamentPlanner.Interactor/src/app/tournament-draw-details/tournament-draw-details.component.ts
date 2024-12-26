ef961347169903a5d49d20487de48788c455b192
knightfury16 <suhaibknight@gmail.com>
2024-12-27 01:05:14 +0600

    Enhance tournament draw details component with Material Design elements and improve back navigation button

--- TournamentPlanner.Interactor/src/app/tournament-draw-details/tournament-draw-details.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-draw-details/tournament-draw-details.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 import { DrawTabViewType } from '../tournament-details-homepage/tournament-details-homepage.component';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-tournament-draw-details',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatCardModule, MatButtonModule],
   templateUrl: './tournament-draw-details.component.html',
   styleUrl: './tournament-draw-details.component.scss',
 })
