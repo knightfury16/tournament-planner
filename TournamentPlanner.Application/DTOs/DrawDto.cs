135d99d15b547de937d08b025cb38b298a12b4e8
knightfury16 <suhaibknight@gmail.com>
2024-12-26 18:39:38 +0600

    Add CreatedAt and UpdatedAt properties to DrawDto for tracking timestamps

--- TournamentPlanner.Application/DTOs/DrawDto.cs
+++ TournamentPlanner.Application/DTOs/DrawDto.cs
@@ -3,6 +3,8 @@
 namespace TournamentPlanner.Application;
 public class DrawDto
 {
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
     public required TournamentDto Tournament { get; set; }
     public required MatchTypeDto MatchType { get; set; }
 }
