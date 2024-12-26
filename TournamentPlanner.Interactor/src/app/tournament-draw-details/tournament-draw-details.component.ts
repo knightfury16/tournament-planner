f386d67e2965b0b38d144e6f0f3245febf007c07
knightfury16 <suhaibknight@gmail.com>
2024-12-27 01:53:11 +0600

    Implement match type retrieval in TournamentDrawDetailsComponent and enhance state management with signals

--- TournamentPlanner.Interactor/src/app/tournament-draw-details/tournament-draw-details.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-draw-details/tournament-draw-details.component.ts
@@ -1,21 +1,61 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  inject,
+  Input,
+  input,
+  OnInit,
+  Output,
+  signal,
+} from '@angular/core';
 import { DrawTabViewType } from '../tournament-details-homepage/tournament-details-homepage.component';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabGroup, MatTabsModule } from '@angular/material/tabs';
 import { CommonModule } from '@angular/common';
+import { MatchTypeDto } from '../tp-model/TpModel';
+import { TournamentPlannerService } from '../tournament-planner.service';
 
 @Component({
   selector: 'app-tournament-draw-details',
   standalone: true,
-  imports: [MatIconModule,MatTabsModule,CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    MatIconModule,
+    MatTabsModule,
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   templateUrl: './tournament-draw-details.component.html',
   styleUrl: './tournament-draw-details.component.scss',
 })
 export class TournamentDrawDetailsComponent implements OnInit {
   @Input({ required: true }) public matchTypeId?: number;
   @Output() drawTabChangeEvent = new EventEmitter<DrawTabViewType>();
+  private _tpService = inject(TournamentPlannerService);
+
+  public matchType = signal<MatchTypeDto | undefined>(undefined);
+  public players = computed(() => this.matchType()?.players ?? undefined);
+  public rounds = computed(() => this.matchType()?.rounds ?? undefined);
+
+  async ngOnInit() {
+    try {
+      var response = await this._tpService.getMatchType(
+        this.matchTypeId!.toString()
+      );
+      console.log('MATCHTYPE', response);
+      this.matchType.set(response);
+    } catch (error) {
+      console.log(error);
+      console.log(
+        (error as any).error ??
+          (error as any).error?.Error ??
+          'An unknown error occurred.'
+      );
+    }
+  }
 
   public emitDrawTabChangeEvent() {
     this.drawTabChangeEvent.emit(DrawTabViewType.ListView);
