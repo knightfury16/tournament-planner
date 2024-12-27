922ab7af824ab7929c388fc2a4bca23ece8d34b1
knightfury16 <suhaibknight@gmail.com>
2024-12-27 20:54:59 +0600

    Add RoundRobinTableComponent to display players and matchups in a round-robin format

---
+++ TournamentPlanner.Interactor/src/app/round-robin-table/round-robin-table.component.ts
@@ -0,0 +1,39 @@
+import { Component, input, Input } from '@angular/core';
+import { GameTypeDto, MatchDto, PlayerDto } from '../tp-model/TpModel';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-round-robin-table',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './round-robin-table.component.html',
+  styleUrl: './round-robin-table.component.scss',
+})
+export class RoundRobinTableComponent {
+  @Input({ required: true }) players?: PlayerDto[];
+  @Input({ required: true }) matches?: MatchDto[];
+
+  @Input({ required: true }) gameType?: GameTypeDto | null;
+
+
+
+  public getTableRowData(
+    rowIndex: number,
+    columnIndex: number
+  ): string | undefined {
+    if (this.players == undefined) return;
+    if (rowIndex == columnIndex) return '_';
+    var firstPlayer = this.players[rowIndex];
+    var secondPlayer = this.players[columnIndex];
+    var match = this.getMatch(firstPlayer, secondPlayer);
+    return `${secondPlayer.name} vs. ${firstPlayer.name}`;
+  }
+  getMatch(firstPlayer: PlayerDto, secondPlayer: PlayerDto) {
+    return this.matches?.find((match) => 
+      (match.firstPlayer.id == firstPlayer.id &&
+        match.secondPlayer.id == secondPlayer.id) ||
+      (match.firstPlayer.id == secondPlayer.id &&
+        match.secondPlayer.id == firstPlayer.id)
+    );
+  }
+}
