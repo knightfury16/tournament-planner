04766c8db9407621740c050eb29c48a29d79eb95
knightfury16 <suhaibknight@gmail.com>
2024-12-28 11:48:21 +0600

    Refactor RoundRobinTableComponent to integrate GameTypeService for score display and improve match data handling

--- TournamentPlanner.Interactor/src/app/round-robin-table/round-robin-table.component.ts
+++ TournamentPlanner.Interactor/src/app/round-robin-table/round-robin-table.component.ts
@@ -1,6 +1,7 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, inject, input, Input } from '@angular/core';
 import { GameTypeDto, MatchDto, PlayerDto } from '../tp-model/TpModel';
 import { MatCardModule } from '@angular/material/card';
+import { GameTypeService } from '../../Shared/game-type.service';
 
 @Component({
   selector: 'app-round-robin-table',
@@ -15,6 +16,8 @@ export class RoundRobinTableComponent {
 
   @Input({ required: true }) gameType?: GameTypeDto | null;
 
+  private _gameTypeService = inject(GameTypeService);
+
 
 
   public getTableRowData(
@@ -26,7 +29,18 @@ export class RoundRobinTableComponent {
     var firstPlayer = this.players[columnIndex];
     var secondPlayer = this.players[rowIndex];
     var match = this.getMatch(firstPlayer, secondPlayer);
-    return `${secondPlayer.name} vs. ${firstPlayer.name}`;
+    var shouldIFlipScore = this.GetShuoulIFlip(match, firstPlayer, secondPlayer);
+    var displayData = this.getDisplayData(match,shouldIFlipScore);
+    return displayData;
+  }
+  GetShuoulIFlip(match: MatchDto | undefined, firstPlayer: PlayerDto, secondPlayer: PlayerDto) {
+    if(match?.firstPlayer.id == firstPlayer.id)return false;
+    return true;
+  }
+   
+  getDisplayData(match: MatchDto | undefined, flipScore: boolean) {
+    if(this.gameType == null || match?.winner == null || match.winner == undefined) return "Not Played"
+    return this._gameTypeService.getDisplayScore(this.gameType,match.scoreJson!,flipScore)
   }
   getMatch(firstPlayer: PlayerDto, secondPlayer: PlayerDto) {
     return this.matches?.find((match) => 
