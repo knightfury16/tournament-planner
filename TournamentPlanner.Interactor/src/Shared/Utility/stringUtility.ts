8bdbd3535040429a7bfbb5e73387d8c24e769586
knightfury16 <suhaibknight@gmail.com>
2024-12-28 11:47:37 +0600

    Add trimAllSpace utility function to remove all whitespace from a string

--- TournamentPlanner.Interactor/src/Shared/Utility/stringUtility.ts
+++ TournamentPlanner.Interactor/src/Shared/Utility/stringUtility.ts
@@ -1,3 +1,7 @@
 export function mapStringToEnum<T>(enumObj: T, value: string): T[keyof T] {
     return (enumObj as any)[value];
 }
+
+export function trimAllSpace(input: string): string {
+  return input.replace(/\s+/g, '');
+}
