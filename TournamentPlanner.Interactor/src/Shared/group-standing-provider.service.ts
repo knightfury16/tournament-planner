5bb2675c6beb11024649748fe7f94b818d759108
knightfury16 <suhaibknight@gmail.com>
2024-12-28 13:35:57 +0600

    group standing provider service provide me component based on gametype at runtime

---
+++ TournamentPlanner.Interactor/src/Shared/group-standing-provider.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Type } from '@angular/core';
+import { GameTypeDto, GameTypeSupported } from '../app/tp-model/TpModel';
+import { trimAllSpace } from './Utility/stringUtility';
+import { TableTennisGroupStandingComponent } from '../app/GroupStandingComponents/table-tennis-group-standing/table-tennis-group-standing.component';
+import { EightBallPoolGroupStandingComponent } from '../app/GroupStandingComponents/eight-ball-pool-group-standing/eight-ball-pool-group-standing.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GroupStandingProviderService {
+  constructor() {}
+
+  public getGroupStaindingComponent(gameType: GameTypeDto): Type<any> {
+    if (gameType.name == trimAllSpace(GameTypeSupported.TableTennis))
+      return TableTennisGroupStandingComponent;
+    if(gameType.name == trimAllSpace(GameTypeSupported.EightBallPool)){
+      return EightBallPoolGroupStandingComponent;
+    }
+    throw new Error('Unsupported game type');
+  }
+}
