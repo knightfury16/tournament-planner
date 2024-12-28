627fb921327d1029991ba4ec71e03b75a63a99d8
knightfury16 <suhaibknight@gmail.com>
2024-12-28 13:36:51 +0600

    added tabletennis group standing component

---
+++ TournamentPlanner.Interactor/src/app/GroupStandingComponents/table-tennis-group-standing/table-tennis-group-standing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-tennis-group-standing',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-tennis-group-standing.component.html',
+  styleUrl: './table-tennis-group-standing.component.scss'
+})
+export class TableTennisGroupStandingComponent {
+
+}
