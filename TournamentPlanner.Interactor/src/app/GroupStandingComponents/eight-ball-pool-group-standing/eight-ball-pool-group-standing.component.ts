9626ffc97dd8601deaaab4495b66964d1b27113c
knightfury16 <suhaibknight@gmail.com>
2024-12-28 13:36:29 +0600

    added eightballpool group standing component

---
+++ TournamentPlanner.Interactor/src/app/GroupStandingComponents/eight-ball-pool-group-standing/eight-ball-pool-group-standing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eight-ball-pool-group-standing',
+  standalone: true,
+  imports: [],
+  templateUrl: './eight-ball-pool-group-standing.component.html',
+  styleUrl: './eight-ball-pool-group-standing.component.scss'
+})
+export class EightBallPoolGroupStandingComponent {
+
+}
