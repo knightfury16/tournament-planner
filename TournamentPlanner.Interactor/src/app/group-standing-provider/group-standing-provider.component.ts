6477e309341d2813e9c6e653ff889ce06ccbd775
knightfury16 <suhaibknight@gmail.com>
2024-12-28 14:32:19 +0600

    added player standings retrieval and integration into group standing provider component

--- TournamentPlanner.Interactor/src/app/group-standing-provider/group-standing-provider.component.ts
+++ TournamentPlanner.Interactor/src/app/group-standing-provider/group-standing-provider.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, Input, OnInit, Signal, viewChild, ViewContainerRef } from '@angular/core';
-import { GameTypeDto } from '../tp-model/TpModel';
+import { Component, ComponentRef, inject, Input, OnInit, Signal, viewChild, ViewContainerRef } from '@angular/core';
+import { GameTypeDto, PlayerStandingDto } from '../tp-model/TpModel';
 import { MatCardModule } from '@angular/material/card';
 import { GroupStandingProviderService } from '../../Shared/group-standing-provider.service';
+import { BaseGroupStandingComponent } from '../GroupStandingComponents/base-group-standing/base-group-standing.component';
+import { TournamentPlannerService } from '../tournament-planner.service';
 
 @Component({
   selector: 'app-group-standing-provider',
@@ -14,11 +16,23 @@ export class GroupStandingProviderComponent implements OnInit {
   @Input({required: true}) matchTypeId?: number;
   @Input({ required: true }) gameType?: GameTypeDto | null;
   vcr: Signal<ViewContainerRef | undefined> = viewChild('gameSpecificGroupStandingContainer', {read: ViewContainerRef});
-
+   #groupStandingComponentRef: ComponentRef<BaseGroupStandingComponent> | undefined ;
   private _gsProviderService = inject(GroupStandingProviderService);
+  private _tpService = inject(TournamentPlannerService);
 
-  ngOnInit(): void {
+  async ngOnInit(){
     var component = this._gsProviderService.getGroupStaindingComponent(this.gameType!);
-    this.vcr()?.createComponent(component);
+    this.#groupStandingComponentRef = this.vcr()?.createComponent(component);
+    var playerStandings = await this.getPlayerStandings();
+    this.#groupStandingComponentRef?.setInput("playerStandings",playerStandings);
+  }
+  async getPlayerStandings():Promise<PlayerStandingDto[] | undefined> {
+    try {
+      return await this._tpService.getGroupStandingOfMatchType(this.matchTypeId!.toString());
+    } catch (error) {
+      console.log(error);
+      console.log((error as any).error ?? (error as any).error?.Error ?? 'An unknown erro occurred.');
+      return;
+    }
   }
 }
