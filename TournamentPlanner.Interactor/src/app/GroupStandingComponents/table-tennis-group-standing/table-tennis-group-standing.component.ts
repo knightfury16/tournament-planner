9ac80fcf9213fe5f5e7c70db2e46162fe878af12
knightfury16 <suhaibknight@gmail.com>
2024-12-28 18:38:07 +0600

    refactor table tennis group standing component to use Angular Material table for enhanced layout and styling

--- TournamentPlanner.Interactor/src/app/GroupStandingComponents/table-tennis-group-standing/table-tennis-group-standing.component.ts
+++ TournamentPlanner.Interactor/src/app/GroupStandingComponents/table-tennis-group-standing/table-tennis-group-standing.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { BaseGroupStandingComponent } from '../base-group-standing/base-group-standing.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-table-tennis-group-standing',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './table-tennis-group-standing.component.html',
-  styleUrl: './table-tennis-group-standing.component.scss'
+  styleUrl: './table-tennis-group-standing.component.scss',
 })
 export class TableTennisGroupStandingComponent extends BaseGroupStandingComponent {
-
+  displayedColumns = [
+    'ranking',
+    'name',
+    'points',
+    'matchesPlayed',
+    'matchRecord',
+    'gamesRecord',
+    'pointsRecord',
+  ];
 }
