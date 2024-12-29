90e68857a0471f47a2d89a65139a0b2f08c02df3
knightfury16 <suhaibknight@gmail.com>
2024-12-29 20:58:05 +0600

    Add TournamentMatchComponent with HTML template and styles
    - Introduced a new TournamentMatchComponent to display match details including player names, scores, scheduled and played dates, and court information.
    - Implemented methods to retrieve player names, match scores, and formatted dates.
    - Created associated HTML and SCSS files for the component's layout and styling.

---
+++ TournamentPlanner.Interactor/src/app/tournament-match/tournament-match.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject, Input } from '@angular/core';
+import { MatchModel } from '../tournament-matches-list/tournament-matches-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { GameTypeDto, NotAvailable } from '../tp-model/TpModel';
+import { GameTypeService } from '../../Shared/game-type.service';
+import { getDateAndTimeStringInFormat } from '../../Shared/Utility/dateTimeUtility';
+
+@Component({
+  selector: 'app-tournament-match',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './tournament-match.component.html',
+  styleUrl: './tournament-match.component.scss'
+})
+export class TournamentMatchComponent {
+  @Input({ required: true }) public match?: MatchModel;
+  @Input({ required: true }) public gameType?: GameTypeDto | null;
+
+  private _gameTypeService = inject(GameTypeService);
+
+  public getFirstPlayerName() {
+    if (this.match?.winner != null && this.match.winner.id == this.match.firstPlayer?.id) return this.getPlayerNameWithWinnerIcon(this.match.firstPlayer.name);
+    return this.match?.firstPlayer?.name;
+  }
+
+  public getSecondPlayerName() {
+    if (this.match?.winner != null && this.match.winner.id == this.match.secondPlayer?.id) return this.getPlayerNameWithWinnerIcon(this.match.secondPlayer.name);
+    return this.match?.secondPlayer?.name;
+  }
+
+  getPlayerNameWithWinnerIcon(name: string) {
+    return `Winner - name`
+  }
+
+  public getMatchScore(): string {
+    if (this.match?.winner == null || this.gameType == null || this.match.scoreJson == null) return NotAvailable;
+    var scoreString = this._gameTypeService.getFullDisplayeScore(this.gameType, this.match.scoreJson);
+    return scoreString;
+  }
+
+  getMatchCourtName(): string {
+    if(this.match?.court == null)return NotAvailable;
+    return this.match.court;
+  }
+  getMatchPlayedDate() {
+    if(this.match?.matchPlayed == null)return NotAvailable;
+    return getDateAndTimeStringInFormat(new Date(this.match.matchPlayed));
+  }
+
+  getMatchScheduledDate() {
+    if(this.match?.matchScheduled == null)return NotAvailable;
+    return getDateAndTimeStringInFormat(new Date(this.match.matchScheduled));
+  }
+}
