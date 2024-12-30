be9f476c8391de53209dfd6e91362d81612369f5
knightfury16 <suhaibknight@gmail.com>
2024-12-30 17:52:54 +0600

    Add base components for score addition in tournament planner
    - Introduced BaseAddScoreComponent
    - Created TableTennisAddScoreComponent that extends BaseAddScoreComponent
    
    This commit lays the groundwork for score management in the tournament planner application.

---
+++ TournamentPlanner.Interactor/src/app/AddScoreComponents/base-add-score/base-add-score.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatchDto } from '../../tp-model/TpModel';
+//- just add the form will contain the logic of the view in add score component
+@Component({
+  selector: 'app-base-add-score',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './base-add-score.component.html',
+  styleUrl: './base-add-score.component.scss'
+})
+export class BaseAddScoreComponent {
+
+  @Input({required: true}) match?: MatchDto;
+
+  public addScoreForm = new FormGroup({});
+
+}
---
+++ TournamentPlanner.Interactor/src/app/AddScoreComponents/table-tennis-add-score/table-tennis-add-score.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BaseAddScoreComponent } from '../base-add-score/base-add-score.component';
+
+@Component({
+  selector: 'app-table-tennis-add-score',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-tennis-add-score.component.html',
+  styleUrl: './table-tennis-add-score.component.scss'
+})
+export class TableTennisAddScoreComponent extends BaseAddScoreComponent {
+
+}
