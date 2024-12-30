286ff374a3aea9a48a73c532a703c7231343621b
knightfury16 <suhaibknight@gmail.com>
2024-12-30 22:00:02 +0600

    injected add score component dynamically

--- TournamentPlanner.Interactor/src/app/add-match-score-provider/add-match-score-provider.component.ts
+++ TournamentPlanner.Interactor/src/app/add-match-score-provider/add-match-score-provider.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ComponentRef, effect, inject, Input, signal, Signal, viewChild, ViewContainerRef } from '@angular/core';
 import { GameTypeDto, MatchDto } from '../tp-model/TpModel';
+import { BaseAddScoreComponent } from '../AddScoreComponents/base-add-score/base-add-score.component';
+import { ScoreComponentProviderService } from '../../Shared/score-component-provider.service';
 
 @Component({
   selector: 'app-add-match-score-provider',
@@ -9,7 +11,21 @@ import { GameTypeDto, MatchDto } from '../tp-model/TpModel';
   styleUrl: './add-match-score-provider.component.scss'
 })
 export class AddMatchScoreProviderComponent {
-  @Input({ required: true }) public match?: MatchDto;
+  @Input({ required: true }) public match = signal<MatchDto | undefined>(undefined);
   @Input({ required: true }) public gameType?: GameTypeDto | null;
 
+  vcr: Signal<ViewContainerRef | undefined> = viewChild('gameSpecificAddScoreForm', { read: ViewContainerRef });
+  #scoreFormComponentRef: ComponentRef<BaseAddScoreComponent> | undefined;
+  private _addScoreProviderService = inject(ScoreComponentProviderService);
+
+  constructor(){
+    effect(() =>{
+      this.#scoreFormComponentRef?.setInput("match", this.match());
+    })
+  }
+  
+  ngOnInit() {
+    var component = this._addScoreProviderService.porvideGameScoreComponentBasedOnGameType(this.gameType!);
+    this.#scoreFormComponentRef = this.vcr()?.createComponent(component);
+  }
 }
