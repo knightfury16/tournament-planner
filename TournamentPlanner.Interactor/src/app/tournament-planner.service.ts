2bc0be24f352296dbbb52968a0030ab8ff3119cd
knightfury16 <suhaibknight@gmail.com>
2024-12-31 23:07:19 +0600

    added param on getting tournament method

--- TournamentPlanner.Interactor/src/app/tournament-planner.service.ts
+++ TournamentPlanner.Interactor/src/app/tournament-planner.service.ts
@@ -13,11 +13,23 @@ export class TournamentPlannerService {
     @Inject(TP_BASE_URL) private baseUrl: string
   ) { }
 
-  public getTournament(name: string): Observable<TournamentDto[]> {
+  public getTournament(
+    name: string = '',
+    searchCategory?: string,
+    status?: string,
+    gameType?: string,
+    startDate?: string,
+    endDate?: string
+  ): Observable<TournamentDto[]> {
     let params = new HttpParams();
-    if (name) {
-      params = params.set('name', name);
-    }
+
+    if (name) params = params.set('name', name);
+    if (searchCategory) params = params.set('searchCategory', searchCategory);
+    if (status) params = params.set('status', status);
+    if (gameType) params = params.set('gameType', gameType);
+    if (startDate) params = params.set('startDate', startDate);
+    if (endDate) params = params.set('endDate', endDate);
+
     return this.httpClient.get<TournamentDto[]>(`${this.baseUrl}/tournament`, { params });
   }
   public getTournamentById(id: string): Promise<TournamentDto> {
@@ -29,7 +41,7 @@ export class TournamentPlannerService {
     return firstValueFrom(this.httpClient.get<DrawDto[]>(`${this.baseUrl}/tournament/${tournamentId}/matches`));
   }
 
-  public getMatchById(matchId: string):Promise<MatchDto>{
+  public getMatchById(matchId: string): Promise<MatchDto> {
     return firstValueFrom(this.httpClient.get<MatchDto>(`${this.baseUrl}/matches/${matchId}`))
   }
 
