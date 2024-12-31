0d7b13f5219c04c67b5e1913ba653bc308540e3c
knightfury16 <suhaibknight@gmail.com>
2024-12-31 13:23:47 +0600

    added CanIMakeDraw request and request handler

---
+++ TournamentPlanner.Application/Request/CanIMakeDrawRequest.cs
@@ -0,0 +1,13 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class CanIMakeDrawRequest : IRequest<bool>
+{
+    public int TournamentId { get; set; }
+    public CanIMakeDrawRequest(int tournamentId)
+    {
+        TournamentId = tournamentId;
+    }
+
+}
---
+++ TournamentPlanner.Application/RequestHandler/CanIMakeDrawRequestHandler.cs
@@ -0,0 +1,49 @@
+﻿using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.Helpers;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Domain.Enum;
+using TournamentPlanner.Domain.Exceptions;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class CanIMakeDrawRequestHandler : IRequestHandler<CanIMakeDrawRequest, bool>
+{
+
+    private readonly IRepository<Tournament> _tournamentRepository;
+
+    private readonly ITournamentService _tournamentService;
+
+    public CanIMakeDrawRequestHandler(IRepository<Tournament> tournamentRepository, ITournamentService tournamentService)
+    {
+        _tournamentRepository = tournamentRepository;
+        _tournamentService = tournamentService;
+    }
+
+    public async Task<bool> Handle(CanIMakeDrawRequest request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        //does the tournament exists?
+        var navigationProperty = Utility.NavigationPrpertyCreator(nameof(Tournament.Draws), nameof(Draw.MatchType));
+        var tournament = await _tournamentRepository.GetByIdAsync(request.TournamentId, [nameof(Tournament.Participants), navigationProperty]);
+
+        if (tournament == null)
+        {
+            throw new NotFoundException(nameof(tournament), request.TournamentId);
+        }
+
+        if (!_tournamentService.AmITheCreator(tournament)) throw new AdminOwnershipException();
+
+        //TODO: Need to check the tournament status
+        //only allow if the registration is complete>> if status is completed will not allow to make the draw.
+        if (tournament.Status <= TournamentStatus.RegistrationOpen)
+        {
+            return false;
+        }
+        //can i make draw? 
+        var canIDarw = await _tournamentService.CanIMakeDraw(tournament);
+
+        return canIDarw;
+    }
+}
