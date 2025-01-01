56646dc35ebe64545a183dc1005f6d88140456a2
knightfury16 <suhaibknight@gmail.com>
2025-01-01 16:27:31 +0600

    created knockout mstchtype component

---
+++ TournamentPlanner.Interactor/src/app/knockout-matchtype-details/knockout-matchtype-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { GameTypeDto } from '../tp-model/TpModel';
+import { MatchDto } from '../tp-model/TpModel';
+import { PlayerDto } from '../tp-model/TpModel';
+
+@Component({
+  selector: 'app-knockout-matchtype-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './knockout-matchtype-details.component.html',
+  styleUrl: './knockout-matchtype-details.component.scss'
+})
+export class KnockoutMatchtypeDetailsComponent {
+
+
+
+}
