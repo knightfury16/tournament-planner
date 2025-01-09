aad44190c8311d4a8c9a420b650b52afa686c7a3
knightfury16 <suhaibknight@gmail.com>
2025-01-09 18:28:41 +0600

    can i schedule request and request handler added

---
+++ TournamentPlanner.Application/Request/CanIScheduleRequest.cs
@@ -0,0 +1,13 @@
+﻿using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class CanIScheduleRequest : IRequest<CanIScheduleDto>
+{
+    public int TournamentId { get; set; }
+
+    public CanIScheduleRequest(int tournamentId)
+    {
+        TournamentId = tournamentId;
+    }
+}
---
+++ TournamentPlanner.Application/RequestHandler/CanIScheduleRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using TournamentPlanner.Application.Common.Interfaces;
+using TournamentPlanner.Application.Helpers;
+using TournamentPlanner.Domain.Entities;
+using TournamentPlanner.Mediator;
+
+namespace TournamentPlanner.Application;
+
+public class CanIScheduleRequestHandler : IRequestHandler<CanIScheduleRequest, CanIScheduleDto>
+{
+    private readonly IRepository<Tournament> _tournamentRepository;
+    private readonly ITournamentService _tournamentService;
+
+    public CanIScheduleRequestHandler(IRepository<Tournament> tournamentRepository, ITournamentService tournamentService)
+    {
+        _tournamentRepository = tournamentRepository;
+        _tournamentService = tournamentService;
+    }
+
+    public async Task<CanIScheduleDto?> Handle(CanIScheduleRequest request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var navigationProperty = Utility.NavigationPrpertyCreator(nameof(Tournament.Draws));
+        var tournament = await _tournamentRepository.GetByIdAsync(request.TournamentId, [navigationProperty]);
+        if (tournament == null) return new CanIScheduleDto { Success = false, Message = "Toutnament with the given Id not found" };
+        try
+        {
+            var canISchedule = await _tournamentService.CanISchedule(tournament);
+            return new CanIScheduleDto { Success = canISchedule };
+        }
+        catch (System.Exception ex)
+        {
+            return new CanIScheduleDto { Success = false, Message = ex.Message };
+        }
+    }
+}
