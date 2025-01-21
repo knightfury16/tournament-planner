2dde038fc3e7c43a61ca4f67c31419328ed6921b
knightfury16 <suhaibknight@gmail.com>
2025-01-21 23:54:53 +0600

    added default match duration in scheduling info

--- TournamentPlanner.Application/DTOs/SchedulingInfo.cs
+++ TournamentPlanner.Application/DTOs/SchedulingInfo.cs
@@ -5,7 +5,8 @@ public class SchedulingInfo
     //TODO: make scheduling info non nullable
     public static TimeOnly DefaultStartTime => new TimeOnly(10, 0); //10am
     public static TimeOnly DefaultEndTime => new TimeOnly(17, 0); //5pm
-    public string? EachMatchTime { get; set; }
+    public static TimeSpan DefaultMatchDuration => TimeSpan.FromMinutes(30); //30min
+    public string? EachMatchTime { get; set; } = DefaultMatchDuration.ToString();
     public string? StartTime { get; set; } = DefaultStartTime.ToString(); //will default to 10am
     public string? EndTime { get; set; } = DefaultEndTime.ToString();//will default to 5pm
     public DateTime? StartDate { get; set; }
