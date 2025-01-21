925f2ef1cf8452c9584af502dc2d55311f3a81b2
knightfury16 <suhaibknight@gmail.com>
2025-01-21 23:41:28 +0600

    added default start time and default end time in scheduling info

--- TournamentPlanner.Application/DTOs/SchedulingInfo.cs
+++ TournamentPlanner.Application/DTOs/SchedulingInfo.cs
@@ -3,8 +3,11 @@ namespace TournamentPlanner.Application.DTOs;
 public class SchedulingInfo
 {
     //TODO: make scheduling info non nullable
+    public static TimeOnly DefaultStartTime => new TimeOnly(10, 0); //10am
+    public static TimeOnly DefaultEndTime => new TimeOnly(17, 0); //5pm
     public string? EachMatchTime { get; set; }
-    public string? StartTime { get; set; }
+    public string? StartTime { get; set; } = DefaultStartTime.ToString(); //will default to 10am
+    public string? EndTime { get; set; } = DefaultEndTime.ToString();//will default to 5pm
     public DateTime? StartDate { get; set; }
     public int MatchPerDay { get; set; } = 15; //each with default 30min match
     public int ParallelMatchesPossible { get; set; } = 1;
