79fdb8919d67d169ef7e9c6f092fe128c4e4c2f6
knightfury16 <suhaibknight@gmail.com>
2025-01-27 18:49:52 +0600

    added manage tournament view tab index constant

---
+++ TournamentPlanner.Interactor/src/app/tp-model/ManageTournamentVIewTabIndex.ts
@@ -0,0 +1,8 @@
+export class ManageTournamentViewTabIndex {
+  public static readonly DefaultTabIndex = 2;
+  public static readonly DetailsTabIndex = 0;
+  public static readonly DrawTabIndex = 1;
+  public static readonly ScheduleTabIndex = 2;
+  public static readonly MatchesTabIndex = 3;
+  public static readonly PlayersTabIndex = 4;
+}
