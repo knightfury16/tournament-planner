b59079a94f8a6ba2742c810df0d937fc7f782844
knightfury16 <suhaibknight@gmail.com>
2025-02-03 22:27:18 +0600

    added some constants.

--- TournamentPlanner.Application/Helpers/Utility.cs
+++ TournamentPlanner.Application/Helpers/Utility.cs
@@ -4,6 +4,15 @@ public static class Utility
 {
     public static string ByePlayerName => "ByePlayer";
     public static string ByePlayerEmail => "byeplayer@gmail.com";
+    public static readonly string Knockout = "Knockout";
+    public static readonly string QuaterFinal = "Quater Final";
+    public static readonly string SemiFinal = "Semi Final";
+    public static readonly string PlayOff = "Playoff 3/4";
+    public static readonly string Final = "Final";
+    public const int PlayOffPlayerNumber = -1;
+    public const int QuaterFinalPlayerNumber = 8;
+    public const int SemiFinalPlayerNumber = 4;
+    public const int FinalPlayerNumber = 2;
 
     public static string NavigationPrpertyCreator(params string [] propertyNames){
         string navigationString = string.Empty;
