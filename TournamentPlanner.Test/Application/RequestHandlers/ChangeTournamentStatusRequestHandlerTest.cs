a3fbde0891e8e6a67ebfea759ed8bdc6c248913b
knightfury16 <suhaibknight@gmail.com>
2025-02-15 18:05:38 +0600

    fixed ChangedTournamentStatusRequestHandler tests

--- TournamentPlanner.Test/Application/RequestHandlers/ChangeTournamentStatusRequestHandlerTest.cs
+++ TournamentPlanner.Test/Application/RequestHandlers/ChangeTournamentStatusRequestHandlerTest.cs
@@ -10,13 +10,16 @@ namespace TournamentPlanner.Test.Application.RequestHandlers;
 
 public class ChangeTournamentStatusRequestHandlerTest
 {
-    private readonly Mock<IRepository<Tournament>> _mockRepository;
+    private readonly Mock<IRepository<Tournament>> _mockTournamentRepository;
+    private readonly Mock<ITournamentService> _mockTournamentService;
     private readonly ChangeTournamentStatusRequestHandler _handler;
+
 
     public ChangeTournamentStatusRequestHandlerTest()
     {
-        _mockRepository = new Mock<IRepository<Tournament>>();
-        _handler = new ChangeTournamentStatusRequestHandler(_mockRepository.Object);
+        _mockTournamentRepository = new Mock<IRepository<Tournament>>();
+        _mockTournamentService = new Mock<ITournamentService>();
+        _handler = new ChangeTournamentStatusRequestHandler(_mockTournamentRepository.Object, _mockTournamentService.Object);
     }
 
     [Fact]
@@ -31,8 +34,8 @@ public class ChangeTournamentStatusRequestHandlerTest
     {
         // Arrange
         var tournamentId = 1;
-        var request = new ChangeTournamentStatusRequest(tournamentId, TournamentStatus.Draft);
-        _mockRepository.Setup(r => r.GetByIdAsync(tournamentId)).ReturnsAsync((Tournament)null!);
+        var request = new ChangeTournamentStatusRequest(tournamentId, TournamentStatus.Draft.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournamentId)).ReturnsAsync((Tournament)null!);
 
         // Act and Assert
         await Assert.ThrowsAsync<NotFoundException>(() => _handler.Handle(request, CancellationToken.None));
@@ -44,15 +47,17 @@ public class ChangeTournamentStatusRequestHandlerTest
         // Arrange
         var tournament = TournamentFixtures.GetTournament();
         tournament.Status = TournamentStatus.RegistrationClosed;
-        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Ongoing);
-        _mockRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Ongoing.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        _mockTournamentService.Setup(s => s.AmITheCreator(tournament)).Returns(true);
 
         // Act
         var result = await _handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.True(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Once);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Once);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.Once);
     }
 
     [Fact]
@@ -61,15 +66,17 @@ public class ChangeTournamentStatusRequestHandlerTest
         // Arrange
         var tournament = TournamentFixtures.GetTournament();
         tournament.Status = TournamentStatus.Ongoing;
-        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Completed);
-        _mockRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Completed.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        _mockTournamentService.Setup(s => s.AmITheCreator(tournament)).Returns(true);
 
         // Act
         var result = await _handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.True(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Once);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Once);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.Once);
     }
 
     [Fact]
@@ -79,36 +86,41 @@ public class ChangeTournamentStatusRequestHandlerTest
         var tournament = TournamentFixtures.GetTournament();
         tournament.Status = TournamentStatus.Ongoing;
         //request lower to draft
-        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Draft);
-        _mockRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Draft.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        _mockTournamentService.Setup(s => s.AmITheCreator(tournament)).Returns(true);
 
         // Act
         var result = await _handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.Once);
 
         // Arrange requst lower to RegistrationOpen
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationOpen);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationOpen.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
         // Arrange requst lower to RegistrationClosed
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationClosed);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationClosed.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
         // Arrange requst lower and equal to Ongoing
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Ongoing);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Ongoing.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
     }
     [Fact]
@@ -118,14 +130,16 @@ public class ChangeTournamentStatusRequestHandlerTest
         var tournament = TournamentFixtures.GetTournament();
         tournament.Status = TournamentStatus.Ongoing;
         //request higer to complete
-        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Completed);
-        _mockRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Completed.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        _mockTournamentService.Setup(s => s.AmITheCreator(tournament)).Returns(true);
 
         // Act
         var result = await _handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.True(result);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.Once);
 
     }
 
@@ -136,43 +150,64 @@ public class ChangeTournamentStatusRequestHandlerTest
         var tournament = TournamentFixtures.GetTournament();
         tournament.Status = TournamentStatus.Completed;
         //request lower to draft
-        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Draft);
-        _mockRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Draft.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        _mockTournamentService.Setup(s => s.AmITheCreator(tournament)).Returns(true);
 
         // Act
         var result = await _handler.Handle(request, CancellationToken.None);
 
         // Assert
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.Once);
 
         // Arrange requst lower to RegistrationOpen
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationOpen);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationOpen.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
         // Arrange requst lower to RegistrationClosed
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationClosed);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationClosed.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
         // Arrange requst lower to Ongoing
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Ongoing);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Ongoing.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
         // Arrange requst lower and equal to  completed
-        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Completed);
+        request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.Completed.ToString());
         //Act and Assert
         result = await _handler.Handle(request, CancellationToken.None);
         Assert.False(result);
-        _mockRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
+        _mockTournamentService.Verify(r => r.AmITheCreator(tournament), Times.AtLeastOnce);
 
+    }
+    [Fact]
+    public async Task Handle_NotTournamentCreator_ThrowsAdminOwnershipException()
+    {
+        // Arrange
+        var tournament = TournamentFixtures.GetTournament();
+        var request = new ChangeTournamentStatusRequest(tournament.Id, TournamentStatus.RegistrationOpen.ToString());
+        _mockTournamentRepository.Setup(r => r.GetByIdAsync(tournament.Id)).ReturnsAsync(tournament);
+        _mockTournamentService.Setup(s => s.AmITheCreator(tournament)).Returns(false);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<AdminOwnershipException>(() => 
+            _handler.Handle(request, CancellationToken.None));
+
+        _mockTournamentRepository.Verify(r => r.SaveAsync(), Times.Never);
     }
 }
