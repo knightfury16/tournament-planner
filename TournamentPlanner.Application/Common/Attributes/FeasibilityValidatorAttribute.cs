dd9e88ac3add178c469d253396bed0f9ef98ac58
knightfury16 <suhaibknight@gmail.com>
2025-02-24 23:55:56 +0600

    Added FeasibilityValidatorAttribute

---
+++ TournamentPlanner.Application/Common/Attributes/FeasibilityValidatorAttribute.cs
@@ -0,0 +1,116 @@
+using System.ComponentModel.DataAnnotations;
+using TournamentPlanner.Application.Helpers;
+using TournamentPlanner.Domain.Enum;
+
+namespace TournamentPlanner.Application.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FeasibilityValidatorAttribute : ValidationAttribute
+    {
+        //matchtype, knockoutstartnumber, maxparticipant
+        private readonly string _tournamentTypePropName;
+        private readonly string _maxParticipantPropName;
+        private readonly string _knockoutstartnumberPropName;
+
+        public FeasibilityValidatorAttribute(
+            string tournamentTypePropName,
+            string maxparticipantPropName,
+            string knockoutstartnumberPropName
+        )
+        {
+            _tournamentTypePropName = tournamentTypePropName;
+            _maxParticipantPropName = maxparticipantPropName;
+            _knockoutstartnumberPropName = knockoutstartnumberPropName;
+        }
+
+        public FeasibilityValidatorAttribute()
+        {
+            _tournamentTypePropName = "TournamentType";
+            _maxParticipantPropName = "MaxParticipant";
+            _knockoutstartnumberPropName = "KnockOutStartNumber";
+        }
+
+        protected override ValidationResult? IsValid(
+            object? value,
+            ValidationContext validationContext
+        )
+        {
+            Console.WriteLine(
+                "Validation context performing on instance of : ",
+                validationContext.DisplayName
+            );
+            var tournamentTypeProp = validationContext.ObjectType.GetProperty(
+                _tournamentTypePropName
+            );
+            var maxParticipantProp = validationContext.ObjectType.GetProperty(
+                _maxParticipantPropName
+            );
+            var knockoutStartProp = validationContext.ObjectType.GetProperty(
+                _knockoutstartnumberPropName
+            );
+
+            if (
+                tournamentTypeProp == null
+                || maxParticipantProp == null
+                || knockoutStartProp == null
+            )
+            {
+                return new ValidationResult(
+                    $"Unkown property: {_tournamentTypePropName}, {_maxParticipantPropName}, {_knockoutstartnumberPropName}"
+                );
+            }
+            var tournamentTypeValue =
+                tournamentTypeProp.GetValue(validationContext.ObjectInstance) as TournamentType?;
+            var maxparticipantValue =
+                maxParticipantProp.GetValue(validationContext.ObjectInstance) as int?;
+            var knockoutStartNumberValue =
+                knockoutStartProp.GetValue(validationContext.ObjectInstance) as int?;
+
+            if (tournamentTypeValue == null)
+            {
+                return new ValidationResult(
+                    "Feasibility Validation Fail: Tournament Type of the Tournament is not given."
+                );
+            }
+
+            if (!maxparticipantValue.HasValue || !knockoutStartNumberValue.HasValue)
+            {
+                return new ValidationResult(
+                    "Feasibility Validation Fail: MaxParticipant or KnockoutStartNumber is not given."
+                );
+            }
+
+            if (
+                tournamentTypeValue == TournamentType.Knockout
+                && maxparticipantValue > Utility.KnocoutMatchTypeMaxParticipant
+            )
+            {
+                return new ValidationResult(
+                    "Feasibility Validation Fail: Knockout match type max participants count exceeded."
+                );
+            }
+
+            if (tournamentTypeValue == TournamentType.GroupStage)
+            {
+                var ppg = GetPPG(maxparticipantValue, knockoutStartNumberValue);
+                if (ppg > Utility.GroupMatchTypePPG)
+                {
+                    return new ValidationResult(
+                        "Feasibility Validation Fail: Player per group max count exceeded."
+                    );
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private int GetPPG(int? maxparticipantValue, int? knockoutStartNumberValue)
+        {
+            return (int)
+                Math.Ceiling(
+                    2 * ((double)maxparticipantValue! / (double)knockoutStartNumberValue!)
+                ); //I have checked for null before
+        }
+    }
+}
+
