e210b6294de860267cd6dc90c4f31e1d937257ca
knightfury16 <suhaibknight@gmail.com>
2025-03-07 23:53:38 +0600

    Refactored change tournament status handler to use tournament service method to change the status

--- TournamentPlanner.Application/RequestHandler/ChangeTournamentStatusRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/ChangeTournamentStatusRequestHandler.cs
@@ -1,58 +1,44 @@
 ﻿using TournamentPlanner.Application.Common.Interfaces;
 using TournamentPlanner.Domain.Entities;
-using TournamentPlanner.Domain.Enum;
 using TournamentPlanner.Domain.Exceptions;
 using TournamentPlanner.Mediator;
 
 namespace TournamentPlanner.Application;
 
-public class ChangeTournamentStatusRequestHandler : IRequestHandler<ChangeTournamentStatusRequest, bool>
+public class ChangeTournamentStatusRequestHandler
+    : IRequestHandler<ChangeTournamentStatusRequest, bool>
 {
     private readonly IRepository<Tournament> _tournamentRepository;
     private readonly ITournamentService _tournamentService;
 
-
-    public ChangeTournamentStatusRequestHandler(IRepository<Tournament> tournamentRepository, ITournamentService tournamentService)
+    public ChangeTournamentStatusRequestHandler(
+        IRepository<Tournament> tournamentRepository,
+        ITournamentService tournamentService
+    )
     {
         _tournamentRepository = tournamentRepository;
         _tournamentService = tournamentService;
     }
 
-    public async Task<bool> Handle(ChangeTournamentStatusRequest request, CancellationToken cancellationToken = default)
+    public async Task<bool> Handle(
+        ChangeTournamentStatusRequest request,
+        CancellationToken cancellationToken = default
+    )
     {
         ArgumentNullException.ThrowIfNull(request);
 
         var tournament = await _tournamentRepository.GetByIdAsync(request.TournamentId);
 
-        if (tournament == null) throw new NotFoundException(nameof(tournament));
-        if (!_tournamentService.AmITheCreator(tournament)) throw new AdminOwnershipException();
-        if (tournament.Status == null) throw new NotFoundException(nameof(tournament.Status));
+        if (tournament == null)
+            throw new NotFoundException(nameof(tournament));
+        if (!_tournamentService.AmITheCreator(tournament))
+            throw new AdminOwnershipException();
+        if (tournament.Status == null)
+            throw new NotFoundException(nameof(tournament.Status));
 
-        var currentStatus = tournament.Status;
-        var requestedChangedStatus = request.TournamentStatus;
-
-        if (CanIChangeStatus(requestedChangedStatus, currentStatus))
-        {
-            tournament.Status = requestedChangedStatus;
-            await _tournamentRepository.SaveAsync();
-            return true;
-        }
-
-        return false;
-    }
-
-    private bool CanIChangeStatus(TournamentStatus requestedChangedStatus, TournamentStatus? currentStatus)
-    {
-        //I can change between Draft, RegistrationOpen, RegistrationClosed back and forth as much as i want
-        //but once status is ongoing i cant go back
-
-        if (currentStatus == TournamentStatus.Completed) return false;
-
-        if (currentStatus >= TournamentStatus.Ongoing && requestedChangedStatus <= TournamentStatus.Ongoing)
-        {
-            return false;
-        }
-
-        return true;
+        return await _tournamentService.ChangeTournamentStatus(
+            tournament,
+            request.TournamentStatus
+        );
     }
 }
