47f3565acd8af34f0efcdf0ec6006cd3179e732c
knightfury16 <suhaibknight@gmail.com>
2025-03-08 00:58:06 +0600

    making default tournament status to registration closed in the fixture

--- TournamentPlanner.Test/Fixtures/TournamentFixtures.cs
+++ TournamentPlanner.Test/Fixtures/TournamentFixtures.cs
@@ -10,7 +10,7 @@ namespace TournamentPlanner.Test.Fixtures
             Id = 1,
             Name = "Test Admin",
             Email = "test@example.com",
-            PhoneNumber = "1234567890"
+            PhoneNumber = "1234567890",
         };
 
         public static Tournament GetTournament(int id = 1)
@@ -20,12 +20,12 @@ namespace TournamentPlanner.Test.Fixtures
                 Id = id,
                 Name = $"Test Tournament {id}",
                 CreatedBy = TestAdmin,
-                Status = TournamentStatus.Ongoing,
+                Status = TournamentStatus.RegistrationClosed,
                 GameType = new GameType { Name = GameTypeSupported.Chess },
                 StartDate = DateTime.UtcNow.AddDays(2),
                 EndDate = DateTime.UtcNow.AddDays(3),
                 TournamentType = TournamentType.GroupStage, //default
-                MaxParticipant = 64
+                MaxParticipant = 64,
             };
         }
 
@@ -46,30 +46,30 @@ namespace TournamentPlanner.Test.Fixtures
         {
             return new List<Tournament>
             {
-                new Tournament 
-                { 
-                    Id = 1, 
-                    Name = "Test Tournament 1", 
+                new Tournament
+                {
+                    Id = 1,
+                    Name = "Test Tournament 1",
                     CreatedBy = TestAdmin,
                     Status = TournamentStatus.Ongoing,
                     GameType = new GameType { Name = GameTypeSupported.Chess },
                     StartDate = DateTime.UtcNow.AddDays(2),
                     EndDate = DateTime.UtcNow.AddDays(3),
-                    TournamentType = TournamentType.GroupStage
+                    TournamentType = TournamentType.GroupStage,
                 },
-                new Tournament 
-                { 
-                    Id = 2, 
+                new Tournament
+                {
+                    Id = 2,
                     Name = "Another Tournament",
                     CreatedBy = TestAdmin,
                     Status = TournamentStatus.Draft,
                     GameType = new GameType { Name = GameTypeSupported.TableTennis },
                     StartDate = DateTime.UtcNow.AddDays(-1),
                     EndDate = DateTime.UtcNow.AddDays(2),
-                    TournamentType = TournamentType.Knockout
+                    TournamentType = TournamentType.Knockout,
                 },
-                new Tournament 
-                { 
+                new Tournament
+                {
                     Id = 3,
                     Name = "Test Tournament 2",
                     CreatedBy = TestAdmin,
@@ -77,10 +77,10 @@ namespace TournamentPlanner.Test.Fixtures
                     GameType = new GameType { Name = GameTypeSupported.Chess },
                     StartDate = DateTime.UtcNow.AddDays(-5),
                     EndDate = DateTime.UtcNow.AddDays(-2),
-                    TournamentType = TournamentType.GroupStage
+                    TournamentType = TournamentType.GroupStage,
                 },
-                new Tournament 
-                { 
+                new Tournament
+                {
                     Id = 4,
                     Name = "Upcoming Test",
                     CreatedBy = TestAdmin,
@@ -88,10 +88,10 @@ namespace TournamentPlanner.Test.Fixtures
                     GameType = new GameType { Name = GameTypeSupported.TableTennis },
                     StartDate = DateTime.UtcNow.AddDays(5),
                     EndDate = DateTime.UtcNow.AddDays(10),
-                    TournamentType = TournamentType.GroupStage
+                    TournamentType = TournamentType.GroupStage,
                 },
-                new Tournament 
-                { 
+                new Tournament
+                {
                     Id = 5,
                     Name = "Upcoming knockout Test",
                     CreatedBy = TestAdmin,
@@ -99,8 +99,8 @@ namespace TournamentPlanner.Test.Fixtures
                     GameType = new GameType { Name = GameTypeSupported.TableTennis },
                     StartDate = DateTime.UtcNow.AddDays(5),
                     EndDate = DateTime.UtcNow.AddDays(10),
-                    TournamentType = TournamentType.Knockout
-                }
+                    TournamentType = TournamentType.Knockout,
+                },
             };
         }
 
