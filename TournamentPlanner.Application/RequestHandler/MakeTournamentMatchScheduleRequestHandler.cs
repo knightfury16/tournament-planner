4d8d628dcaa4d804a2e880dfa4805b88c6b6816a
knightfury16 <suhaibknight@gmail.com>
2025-03-07 23:58:30 +0600

    on test removed and format

--- TournamentPlanner.Application/RequestHandler/MakeTournamentMatchScheduleRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/MakeTournamentMatchScheduleRequestHandler.cs
@@ -11,7 +11,8 @@ using MatchType = TournamentPlanner.Domain.Entities.MatchType;
 
 namespace TournamentPlanner.Application;
 
-public class MakeTournamentMatchScheduleRequestHandler : IRequestHandler<MakeTournamentMatchScheduleRequest, IEnumerable<MatchDto>>
+public class MakeTournamentMatchScheduleRequestHandler
+    : IRequestHandler<MakeTournamentMatchScheduleRequest, IEnumerable<MatchDto>>
 {
     private readonly IRepository<Tournament> _tournamentRepository;
 
@@ -21,8 +22,14 @@ public class MakeTournamentMatchScheduleRequestHandler : IRequestHandler<MakeTou
     private readonly ITournamentService _tournamentService;
     private readonly IMatchScheduler _matchScheduler;
 
-    public MakeTournamentMatchScheduleRequestHandler(IRepository<Tournament> tournamentRepository, IMapper mapper,
-                                                    IDrawService drawService, IMatchService matchService, ITournamentService tournamentService, IMatchScheduler matchScheduler)
+    public MakeTournamentMatchScheduleRequestHandler(
+        IRepository<Tournament> tournamentRepository,
+        IMapper mapper,
+        IDrawService drawService,
+        IMatchService matchService,
+        ITournamentService tournamentService,
+        IMatchScheduler matchScheduler
+    )
     {
         _tournamentRepository = tournamentRepository;
         _mapper = mapper;
@@ -31,34 +38,54 @@ public class MakeTournamentMatchScheduleRequestHandler : IRequestHandler<MakeTou
         _tournamentService = tournamentService;
         _matchScheduler = matchScheduler;
     }
-    public async Task<IEnumerable<MatchDto>?> Handle(MakeTournamentMatchScheduleRequest request, CancellationToken cancellationToken = default)
+
+    public async Task<IEnumerable<MatchDto>?> Handle(
+        MakeTournamentMatchScheduleRequest request,
+        CancellationToken cancellationToken = default
+    )
     {
-        if(request == null)throw new ArgumentNullException(nameof(request));
+        if (request == null)
+            throw new ArgumentNullException(nameof(request));
 
         //get the tournament with all the draw
-        var nagivationProperty = Utility.NavigationPrpertyCreator(nameof(Tournament.Draws), nameof(Draw.MatchType), nameof(MatchType.Players));
-        var tournament = await _tournamentRepository.GetByIdAsync(request.TournamentId, [nagivationProperty, nameof(Tournament.Matches)]);
-        if(tournament == null)throw new NotFoundException(nameof(tournament));
-        if(!_tournamentService.AmITheCreator(tournament))throw new AdminOwnershipException();
+        var nagivationProperty = Utility.NavigationPrpertyCreator(
+            nameof(Tournament.Draws),
+            nameof(Draw.MatchType),
+            nameof(MatchType.Players)
+        );
+        var tournament = await _tournamentRepository.GetByIdAsync(
+            request.TournamentId,
+            [nagivationProperty, nameof(Tournament.Matches)]
+        );
+        if (tournament == null)
+            throw new NotFoundException(nameof(tournament));
+        if (!_tournamentService.AmITheCreator(tournament))
+            throw new AdminOwnershipException();
 
-        if(tournament.Status == TournamentStatus.Completed)throw new BadRequestException("Tournament already completed");
+        if (tournament.Status == TournamentStatus.Completed)
+            throw new BadRequestException("Tournament already completed");
 
         //go to draw service and see if im able to make schedule
-            //ON-TEST: Test this please 29/09/24
         var canISchedule = await _tournamentService.CanISchedule(tournament);
-        if(canISchedule == false)throw new BadRequestException("Draws not made or previous draws not complete. Can not make schedule");
+        if (canISchedule == false)
+            throw new BadRequestException(
+                "Draws not made or previous draws not complete. Can not make schedule"
+            );
 
         //go to match service with all the draws
         var matches = await _matchService.CreateMatches(tournament, request.SchedulingInfo);
 
-        var scheduledMatches = await _matchScheduler.DefaultMatchScheduler(matches.ToList(), request.SchedulingInfo);
+        var scheduledMatches = await _matchScheduler.DefaultMatchScheduler(
+            matches.ToList(),
+            request.SchedulingInfo
+        );
 
-        //ON-TEST 
+        //ON-TEST
         //TODO: Recalculate this match count logic. This does not seem right
-        //! at worst case will have 20 group with 10 match each, with total 200 matches between them. so cant send all the info. remember it. 
+        //! at worst case will have 20 group with 10 match each, with total 200 matches between them. so cant send all the info. remember it.
 
         tournament.Matches.AddRange(scheduledMatches);
-         await _tournamentRepository.SaveAsync();
+        await _tournamentRepository.SaveAsync();
         return _mapper.Map<IEnumerable<MatchDto>>(scheduledMatches);
     }
 }
