e2642a2398df25ffd4081fe7c257205e9f905ae9
knightfury16 <suhaibknight@gmail.com>
2025-03-10 21:11:31 +0600

    Migrating Db to cloud: Added user secret in datamodeling project

--- TournamentPlanner.DataModeling/TournamentPlannerDataContextFactory.cs
+++ TournamentPlanner.DataModeling/TournamentPlannerDataContextFactory.cs
@@ -1,14 +1,19 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
 namespace TournamentPlanner.DataModeling
 {
-    public class TournamentPlannerDataContextFactory : IDesignTimeDbContextFactory<TournamentPlannerDataContext>
+    public class TournamentPlannerDataContextFactory
+        : IDesignTimeDbContextFactory<TournamentPlannerDataContext>
     {
         public TournamentPlannerDataContext CreateDbContext(string[]? args)
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .Build();
             var environment = Environment.GetEnvironmentVariable("DOTNET_ENV");
             string connectionString;
             if (environment == "test")
@@ -21,9 +26,11 @@ namespace TournamentPlanner.DataModeling
             }
 
             var optionsBuilder = new DbContextOptionsBuilder<TournamentPlannerDataContext>();
+            Console.WriteLine("Connection string: " + connectionString);
             optionsBuilder.UseNpgsql(connectionString);
 
             return new TournamentPlannerDataContext(optionsBuilder.Options);
         }
     }
 }
+
