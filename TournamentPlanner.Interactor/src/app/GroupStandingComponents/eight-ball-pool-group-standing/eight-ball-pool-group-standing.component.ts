a6ceb5c8a1b5ff65bfb2cb32b5cbb7a7b8fb6c68
knightfury16 <suhaibknight@gmail.com>
2025-03-12 20:47:34 +0600

    Eight Ball pool group standing functional

--- TournamentPlanner.Interactor/src/app/GroupStandingComponents/eight-ball-pool-group-standing/eight-ball-pool-group-standing.component.ts
+++ TournamentPlanner.Interactor/src/app/GroupStandingComponents/eight-ball-pool-group-standing/eight-ball-pool-group-standing.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { BaseGroupStandingComponent } from '../base-group-standing/base-group-standing.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-eight-ball-pool-group-standing',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './eight-ball-pool-group-standing.component.html',
   styleUrl: './eight-ball-pool-group-standing.component.scss'
 })
 export class EightBallPoolGroupStandingComponent extends BaseGroupStandingComponent {
+  displayedColumns = [
+    'ranking',
+    'name',
+    'points',
+    'matchesPlayed',
+    'matchRecord',
+    'gamesRecord',
+  ];
 
 }
