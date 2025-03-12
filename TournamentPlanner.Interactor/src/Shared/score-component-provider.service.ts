158828c253b0702ab025c4eef6cb123327d48090
knightfury16 <suhaibknight@gmail.com>
2025-03-12 13:30:23 +0600

    Added eight ball pool score form in score component provider

--- TournamentPlanner.Interactor/src/Shared/score-component-provider.service.ts
+++ TournamentPlanner.Interactor/src/Shared/score-component-provider.service.ts
@@ -3,6 +3,7 @@ import { GameTypeDto, GameTypeSupported } from '../app/tp-model/TpModel';
 import { BaseAddScoreComponent } from '../app/AddScoreComponents/base-add-score/base-add-score.component';
 import { TableTennisAddScoreComponent } from '../app/AddScoreComponents/table-tennis-add-score/table-tennis-add-score.component';
 import { trimAllSpace } from './Utility/stringUtility';
+import { EightBallPoolAddScoreComponent } from '../app/AddScoreComponents/eight-ball-pool-add-score/eight-ball-pool-add-score.component';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,9 @@ export class ScoreComponentProviderService {
 
   constructor() { }
 
-  public porvideGameScoreComponentBasedOnGameType(gameType: GameTypeDto): Type<BaseAddScoreComponent>{
-    if(gameType.name == trimAllSpace(GameTypeSupported.TableTennis))return TableTennisAddScoreComponent;
+  public porvideGameScoreComponentBasedOnGameType(gameType: GameTypeDto): Type<BaseAddScoreComponent> {
+    if (gameType.name == trimAllSpace(GameTypeSupported.TableTennis)) return TableTennisAddScoreComponent;
+    if (gameType.name == trimAllSpace(GameTypeSupported.EightBallPool)) return EightBallPoolAddScoreComponent;
     return BaseAddScoreComponent;
   }
 }
