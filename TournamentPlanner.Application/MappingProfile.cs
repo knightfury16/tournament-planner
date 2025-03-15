f5cffc869bf3fd04fbe238b762a87eae0e496177
knightfury16 <suhaibknight@gmail.com>
2025-03-16 00:42:30 +0600

    format

--- TournamentPlanner.Application/MappingProfile.cs
+++ TournamentPlanner.Application/MappingProfile.cs
@@ -12,16 +12,16 @@ namespace TournamentPlanner.Application
             CreateMap<Player, PlayerDto>().ReverseMap();
             CreateMap<Player, FullPlayerDto>().ReverseMap();
             CreateMap<GameType, GameTypeDto>()
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToString()))
-            .ReverseMap();
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToString()))
+                .ReverseMap();
             CreateMap<Tournament, TournamentDto>()
-            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
-            .ForMember(dest => dest.GameTypeDto, opt => opt.MapFrom(src => src.GameType))
-            .ReverseMap();
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.GameTypeDto, opt => opt.MapFrom(src => src.GameType))
+                .ReverseMap();
             CreateMap<Tournament, FullTournamentDto>()
-            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
-            .ForMember(dest => dest.GameTypeDto, opt => opt.MapFrom(src => src.GameType))
-            .ReverseMap();
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.GameTypeDto, opt => opt.MapFrom(src => src.GameType))
+                .ReverseMap();
             CreateMap<Player, AddPlayerDto>().ReverseMap();
             CreateMap<Admin, AdminDto>().ReverseMap();
             CreateMap<Admin, AddAdminDto>().ReverseMap();
@@ -29,12 +29,19 @@ namespace TournamentPlanner.Application
             CreateMap<Match, MatchDto>().ReverseMap();
             CreateMap<Round, RoundDto>().ReverseMap();
             CreateMap<Domain.Entities.MatchType, MatchTypeDto>()
-            .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src is Group ? "Group" : src is KnockOut ? "KnockOut" : "Unknown"))
-            .ReverseMap();
+                .ForMember(
+                    dest => dest.Type,
+                    opt =>
+                        opt.MapFrom(src =>
+                            src is Group ? "Group"
+                            : src is KnockOut ? "KnockOut"
+                            : "Unknown"
+                        )
+                )
+                .ReverseMap();
             CreateMap<Draw, DrawDto>().ReverseMap();
             CreateMap<PlayerStanding, PlayerStandingDto>().ReverseMap();
-
         }
-
     }
 }
+
