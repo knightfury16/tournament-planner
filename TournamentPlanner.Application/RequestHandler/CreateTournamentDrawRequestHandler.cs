60f9867b7b9c82b977b572d937b8d8dfd94986fd
knightfury16 <suhaibknight@gmail.com>
2025-03-18 22:54:57 +0600

    format

--- TournamentPlanner.Application/RequestHandler/CreateTournamentDrawRequestHandler.cs
+++ TournamentPlanner.Application/RequestHandler/CreateTournamentDrawRequestHandler.cs
@@ -8,49 +8,70 @@ using TournamentPlanner.Mediator;
 
 namespace TournamentPlanner.Application;
 
-public class CreateTournamentDrawRequestHandler : IRequestHandler<CreateTournamentDrawRequest, IEnumerable<DrawDto>>
+public class CreateTournamentDrawRequestHandler
+    : IRequestHandler<CreateTournamentDrawRequest, IEnumerable<DrawDto>>
 {
     private readonly IRepository<Tournament> _tournamentRepository;
     private readonly IMapper _mapper;
     private readonly ITournamentService _tournamentService;
 
-    public CreateTournamentDrawRequestHandler(IRepository<Tournament> tournamentRepository, IMapper mapper, ITournamentService tournamentService)
+    public CreateTournamentDrawRequestHandler(
+        IRepository<Tournament> tournamentRepository,
+        IMapper mapper,
+        ITournamentService tournamentService
+    )
     {
         _tournamentRepository = tournamentRepository;
         _mapper = mapper;
         _tournamentService = tournamentService;
     }
 
-
-    public async Task<IEnumerable<DrawDto>?> Handle(CreateTournamentDrawRequest request, CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<DrawDto>?> Handle(
+        CreateTournamentDrawRequest request,
+        CancellationToken cancellationToken = default
+    )
     {
         //!! Axiom:: in TP I can only make two draw at most if the type is Group
         //does the tournament exists?
-        var navigationProperty = Utility.NavigationPrpertyCreator(nameof(Tournament.Draws), nameof(Draw.MatchType));
-        var tournament = (await _tournamentRepository.GetAllAsync(t => t.Id == request.TournamentId, [nameof(Tournament.Participants),
-                            navigationProperty]))
-                            .FirstOrDefault();
+        var navigationProperty = Utility.NavigationPrpertyCreator(
+            nameof(Tournament.Draws),
+            nameof(Draw.MatchType)
+        );
+        var tournament = (
+            await _tournamentRepository.GetAllAsync(
+                t => t.Id == request.TournamentId,
+                [nameof(Tournament.Participants), navigationProperty]
+            )
+        ).FirstOrDefault();
 
         if (tournament == null)
         {
             throw new NotFoundException(nameof(tournament), request.TournamentId);
         }
 
-        if (!_tournamentService.AmITheCreator(tournament)) throw new AdminOwnershipException();
+        if (!_tournamentService.AmITheCreator(tournament))
+            throw new AdminOwnershipException();
 
         //TODO: Need to check the tournament status
         //only allow if the registration is complete>> if status is completed will not allow to make the draw.
         if (tournament.Status <= TournamentStatus.RegistrationOpen)
         {
-            throw new BadRequestException("Please close the Tournament Registration in order to make draw");
+            throw new BadRequestException(
+                "Please close the Tournament Registration in order to make draw"
+            );
         }
-        //can i make draw? 
+        //can i make draw?
         var canIDarw = await _tournamentService.CanIMakeDraw(tournament);
 
-        if (!canIDarw) throw new BadRequestException("Previous draws are not completed.Can not make new draw");
+        if (!canIDarw)
+            throw new BadRequestException("Previous draws are not completed.Can not make new draw");
 
         //make draw -> make matchType
-        var draws = await _tournamentService.MakeDraws(tournament, request.MatchTypePrefix, request.SeedersId);
+        var draws = await _tournamentService.MakeDraws(
+            tournament,
+            request.MatchTypePrefix,
+            request.SeedersId
+        );
         tournament.Draws.AddRange(draws);
         await _tournamentRepository.SaveAsync();
 
