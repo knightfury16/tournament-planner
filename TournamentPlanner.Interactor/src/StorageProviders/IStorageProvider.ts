0d1e81fe9fee4d16eeac91657e667e902c082bb4
knightfury16 <suhaibknight@gmail.com>
2025-03-20 13:47:26 +0600

    Created storage provider interface

---
+++ TournamentPlanner.Interactor/src/StorageProviders/IStorageProvider.ts
@@ -0,0 +1,7 @@
+export interface IStorageProvider
+{
+    get(key: string): Promise<string | null>;
+    set(key: string, value: string): Promise<void>;
+    remove(key: string): Promise<void>;
+    clear(): Promise<void>;
+}
