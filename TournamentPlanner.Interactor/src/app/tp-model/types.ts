49601642ec2d958d0583b7a7701d303fc6c7455d
knightfury16 <suhaibknight@gmail.com>
2025-03-20 18:25:27 +0600

    defined persistence types and constants

--- TournamentPlanner.Interactor/src/app/tp-model/types.ts
+++ TournamentPlanner.Interactor/src/app/tp-model/types.ts
@@ -3,3 +3,13 @@ export enum MatchTabViewType {
   MatchView = "MatchView",
   AddScoreView = "AddScoreView"
 }
+
+//Persistance stored item type
+export type StoredItem<T>  = {
+  value: T;
+  expiry: Date | undefined | null;
+}
+
+
+export const DefaultExpiryMintures = 120;
+export const DefaultApplicationPrefix = "TP"
