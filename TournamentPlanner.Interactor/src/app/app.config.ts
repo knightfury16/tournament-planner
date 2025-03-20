980007335b25e7fc66826b8e4308d57b88d20e8f
knightfury16 <suhaibknight@gmail.com>
2025-03-20 13:48:58 +0600

    Injected local storage provider as the storage provider for the application

--- TournamentPlanner.Interactor/src/app/app.config.ts
+++ TournamentPlanner.Interactor/src/app/app.config.ts
@@ -4,9 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { IStorageProvider } from '../StorageProviders/IStorageProvider';
+import { LocalStorageProvider } from '../StorageProviders/LocalStorageProvider';
 
 export const TRIPPIN_BASE_URL = new InjectionToken<string>('TRIPPIN_BASE_URL');
 export const TP_BASE_URL = new InjectionToken<string>("TP_BASE_URL");
+export const STORAGE_PROVIDER = new InjectionToken<IStorageProvider>("STORAGE_PROVIDER");
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +19,10 @@ export const appConfig: ApplicationConfig = {
       provide: TRIPPIN_BASE_URL,
       useValue:
         'https://services.odata.org/TripPinRESTierService/(S(dwkum2ychcndvz35fd2eivpa))',
+    },
+    {
+      provide: STORAGE_PROVIDER,
+      useClass: LocalStorageProvider
     },
     {
       provide: TP_BASE_URL,
