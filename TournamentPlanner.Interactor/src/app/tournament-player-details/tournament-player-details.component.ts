48666d04f8b4b2f50b11b1ce9f8afa1b9fc9a330
knightfury16 <suhaibknight@gmail.com>
2025-03-25 20:44:12 +0600

    removed tournamentId input

--- TournamentPlanner.Interactor/src/app/tournament-player-details/tournament-player-details.component.ts
+++ TournamentPlanner.Interactor/src/app/tournament-player-details/tournament-player-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PlayerTabViewType } from '../tournament-details-homepage/tournament-details-homepage.component';
 import { MatButtonModule } from '@angular/material/button';
+import { PlayerDto } from '../tp-model/TpModel';
 
 @Component({
   selector: 'app-tournament-player-details',
@@ -10,11 +11,11 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './tournament-player-details.component.scss'
 })
 export class TournamentPlayerDetailsComponent {
-  @Input({ required: true }) tournamentId?: string;
+  // @Input({ required: true }) tournamentId?: string;
+  @Input({ required: false }) player: PlayerDto | undefined;
   @Output() playerTabChangeEvent = new EventEmitter<PlayerTabViewType>();
 
-  public emitTabViewChangeEvent()
-  {
+  public emitTabViewChangeEvent() {
     this.playerTabChangeEvent.emit(PlayerTabViewType.ListView)
   }
 
