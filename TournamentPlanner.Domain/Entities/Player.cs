72269c6effd11d43729640a3ecc51c13a5718afe
knightfury16 <suhaibknight@gmail.com>
2025-03-26 22:21:39 +0600

    Used GameStatistic in Player entity

--- TournamentPlanner.Domain/Entities/Player.cs
+++ TournamentPlanner.Domain/Entities/Player.cs
@@ -4,10 +4,7 @@ public class Player : User
 {
     public int Age { get; set; }
     public int Weight { get; set; }
-    public int GamePlayed { get; set; }
-    public int GameWon { get; set; }
-    public double WinRatio => GamePlayed > 0 ? (double)GameWon / GamePlayed : 0;
-
+    public List<GameStatistic>? GameStatistics { get; set; }
     public List<Tournament>? Tournaments { get; set; }
     public List<MatchType>? MatchTypes { get; set; }
     public List<SeededPlayer>? SeededEntries { get; set; }
